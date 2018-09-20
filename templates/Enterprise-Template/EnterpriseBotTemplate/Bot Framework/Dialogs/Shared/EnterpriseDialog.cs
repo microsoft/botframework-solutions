07499ed5bd97f961c43035f74fe548209e14b745
Lauren Mills <lamil@microsoft.com>
2018-09-20 10:55:04 -0700

    Added enterprise template and vsix

---
+++ templates/Enterprise-Template/EnterpriseBotTemplate/Bot Framework/Dialogs/Shared/EnterpriseDialog.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Luis;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace $safeprojectname$
+{
+    public class EnterpriseDialog : InterruptableDialog
+    {
+        protected const string LuisResultKey = "LuisResult";
+
+        // Fields
+        private readonly BotServices _services;
+        private readonly CancelResponses _responder = new CancelResponses();
+
+        public EnterpriseDialog(BotServices botServices, string dialogId)
+            : base(dialogId)
+        {
+            _services = botServices;
+
+            AddDialog(new CancelDialog());
+        }
+
+        protected override async Task<InterruptionStatus> OnDialogInterruptionAsync(DialogContext dc, CancellationToken cancellationToken)
+        {
+            // check luis intent
+            var luisService = _services.LuisServices["$safeprojectname$_General"];
+            var luisResult = await luisService.RecognizeAsync<General>(dc.Context, cancellationToken);
+            var intent = luisResult.TopIntent().intent;
+
+            // Add the luis result (intent and entities) for further processing in the derived dialog
+            dc.Context.TurnState.Add(LuisResultKey, luisResult);
+
+            switch (intent)
+            {
+                case General.Intent.Cancel:
+                    {
+                        return await OnCancel(dc);
+                    }
+
+                case General.Intent.Help:
+                    {
+                        return await OnHelp(dc);
+                    }
+            }
+
+            return InterruptionStatus.NoAction;
+        }
+
+        protected virtual async Task<InterruptionStatus> OnCancel(DialogContext dc)
+        {
+            if (dc.ActiveDialog.Id != nameof(CancelDialog))
+            {
+                // Don't start restart cancel dialog
+                await dc.BeginDialogAsync(nameof(CancelDialog));
+
+                // Signal that the dialog is waiting on user response
+                return InterruptionStatus.Waiting;
+            }
+
+            // Else, continue
+            return InterruptionStatus.NoAction;
+        }
+
+        protected virtual async Task<InterruptionStatus> OnHelp(DialogContext dc)
+        {
+            var view = new MainResponses();
+            await view.ReplyWith(dc.Context, MainResponses.Help);
+
+            // Signal the conversation was interrupted and should immediately continue
+            return InterruptionStatus.Interrupted;
+        }
+    }
+}
