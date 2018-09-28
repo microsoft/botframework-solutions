b24f82348120796e4e622f92fa1e71b70bc80c97
Lauren Mills <lamil@microsoft.com>
2018-09-28 16:19:10 -0700

    added news skill

---
+++ solutions/Virtual-Assistant/src/csharp/skills/NewsSkill/Dialogs/FindArticles/FindArticlesDialog.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Solutions.Skills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NewsSkill
+{
+    public class FindArticlesDialog : EnterpriseDialog
+    {
+        private NewsClient _client;
+        private FindArticlesResponses _responder = new FindArticlesResponses();
+
+        public FindArticlesDialog(SkillConfiguration botServices) 
+            : base(botServices, nameof(FindArticlesDialog))
+        {
+            var key = botServices.Properties["BingNewsKey"] ?? throw new Exception("The BingNewsKey must be provided to use this dialog. Please provide this key in your Skill Configuration.");
+
+            _client = new NewsClient((string)key);
+
+            var findArticles = new WaterfallStep[]
+            {
+                GetQuery,
+                ShowArticles,
+            };
+
+            AddDialog(new WaterfallDialog(nameof(FindArticlesDialog), findArticles));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+        }
+
+        private async Task<DialogTurnResult> GetQuery(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions()
+            {
+                Prompt = await _responder.RenderTemplate(stepContext.Context, "en", FindArticlesResponses.TopicPrompt)
+            });
+        }
+
+        private async Task<DialogTurnResult> ShowArticles(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var query = (string)stepContext.Result;
+            var articles = await _client.GetNewsForTopic(query);
+            await _responder.ReplyWith(stepContext.Context, FindArticlesResponses.ShowArticles, articles);
+
+            return await stepContext.EndDialogAsync();
+        }
+    }
+}
---
+++ solutions/Virtual-Assistant/src/csharp/skills/NewsSkill/Dialogs/FindArticles/FindArticlesResponses.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Azure.CognitiveServices.Search.NewsSearch.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.TemplateManager;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsSkill
+{
+    public class FindArticlesResponses : TemplateManager
+    {
+        public const string TopicPrompt = "topicPrompt";
+        public const string ShowArticles = "showArticles";
+
+        private static LanguageTemplateDictionary _responseTemplates = new LanguageTemplateDictionary
+        {
+            ["default"] = new TemplateIdMap
+            {
+                { TopicPrompt, (context, data) => "What topic are you interested in?" },
+                { ShowArticles, (context, data) => ShowArticleCards(context, data) }
+            },
+            ["en"] = new TemplateIdMap { },
+            ["fr"] = new TemplateIdMap { },
+        };
+
+        public FindArticlesResponses()
+        {
+            Register(new DictionaryRenderer(_responseTemplates));
+        }
+
+        private static object ShowArticleCards(ITurnContext context, dynamic data)
+        {
+            var response = context.Activity.CreateReply();
+            var articles = data as List<NewsArticle>;
+
+            if (articles.Any())
+            {
+                response.Text = "Here's what I found:";
+                response.Attachments = new List<Attachment>();
+                response.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+                foreach (var item in articles)
+                {
+                    var card = new ThumbnailCard()
+                    {
+                        Title = item.Name,
+                        Subtitle = item.DatePublished,
+                        Text = item.Description,
+                        Images = new List<CardImage>()
+                        {
+                            new CardImage(item.Image.Thumbnail.ContentUrl),
+                        },
+                            Buttons = new List<CardAction>()
+                        {
+                            new CardAction(ActionTypes.OpenUrl, title: "Read more", value: item.Url)
+                        },
+                    }.ToAttachment();
+
+                    response.Attachments.Add(card);
+                }
+            }
+            else
+            {
+                response.Text = "Sorry, I couldn't find any articles on that topic.";
+            }          
+
+            return response;
+        }
+    }
+}
