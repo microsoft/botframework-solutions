f6b37523001ada5d7dcea10631cc1836204e7e85
Tianyu You <tiyou@microsoft.com>
2018-10-09 16:31:21 +0800

    fix build issue

--- solutions/Virtual-Assistant/src/csharp/skills/EmailSkill/Dialogs/DeleteEmail/DeleteEmailDialog.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/EmailSkill/Dialogs/DeleteEmail/DeleteEmailDialog.cs
@@ -10,6 +10,7 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using EmailSkill.Dialogs.DeleteEmail.Resources;
 
 namespace EmailSkill
 {
@@ -20,53 +21,56 @@ namespace EmailSkill
             IStatePropertyAccessor<EmailSkillState> emailStateAccessor,
             IStatePropertyAccessor<DialogState> dialogStateAccessor,
             IMailSkillServiceManager serviceManager)
-            : base(nameof(ShowEmailDialog), services, emailStateAccessor, dialogStateAccessor, serviceManager)
+            : base(nameof(DeleteEmailDialog), services, emailStateAccessor, dialogStateAccessor, serviceManager)
         {
-            var showEmail = new WaterfallStep[]
+            var deleteEmail = new WaterfallStep[]
             {
-                IfClearContextStep,
                 GetAuthToken,
                 AfterGetAuthToken,
-                ShowEmailsWithoutEnd,
-                PromptToRead,
-                CallReadEmailDialog,
+                PromptToDelete,
+                DeleteEmail,
             };
 
-            var readEmail = new WaterfallStep[]
+            var showEmail = new WaterfallStep[]
             {
-                ReadEmail,
-                AfterReadOutEmail,
+                PromptCollectMessage,
+                ShowEmails
             };
 
             // Define the conversation flow using a waterfall model.
+            AddDialog(new WaterfallDialog(Action.Delete, deleteEmail));
             AddDialog(new WaterfallDialog(Action.Show, showEmail));
-            AddDialog(new WaterfallDialog(Action.Read, readEmail));
-            InitialDialogId = Action.Show;
+            InitialDialogId = Action.Delete;
         }
 
-        public async Task<DialogTurnResult> IfClearContextStep(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<DialogTurnResult> PromptToDelete(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
         {
             try
             {
-                // clear context before show emails, and extract it from luis result again.
                 var state = await _emailStateAccessor.GetAsync(sc.Context);
-                var luisResult = state.LuisResult;
-
-                var topIntent = luisResult?.TopIntent().intent;
-                if (topIntent == Email.Intent.CheckMessages)
+                var focusedMessage = state.Message.FirstOrDefault();
+                if (focusedMessage != null)
                 {
-                    await ClearConversationState(sc);
-                    await DigestEmailLuisResult(sc, luisResult);
+                    return await sc.PromptAsync(Action.ConfirmRecipient, new PromptOptions { Prompt = sc.Context.Activity.CreateReply(DeleteEmailResponses.DeleteConfirm) });
                 }
 
-                if (topIntent == Email.Intent.ShowNext)
-                {
-                    state.ShowEmailIndex++;
-                }
+                return await sc.BeginDialogAsync(Action.Show);
+            }
+            catch (Exception ex)
+            {
+                throw await HandleDialogExceptions(sc, ex);
+            }
+        }
 
-                if (topIntent == Email.Intent.ShowPrevious && state.ShowEmailIndex > 0)
+        public async Task<DialogTurnResult> PromptCollectMessage(WaterfallStepContext sc, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var state = await _emailStateAccessor.GetAsync(sc.Context);
+                var focusedMessage = state.Message.FirstOrDefault();
+                if (focusedMessage != null)
                 {
-                    state.ShowEmailIndex--;
+                    return await sc.PromptAsync(Action.Prompt, new PromptOptions { Prompt = sc.Context.Activity.CreateReply(DeleteEmailResponses.DeletePrompt) });
                 }
 
                 return await sc.NextAsync();
@@ -77,137 +81,15 @@ namespace EmailSkill
             }
         }
 
-        public async Task<DialogTurnResult> ShowEmailsWithoutEnd(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<DialogTurnResult> DeleteEmail(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
         {
             try
             {
                 var state = await _emailStateAccessor.GetAsync(sc.Context);
-
-                var messages = await GetMessagesAsync(sc);
-                if (messages.Count > 0)
-                {
-                    await ShowMailList(sc, messages);
-                    state.MessageList = messages;
-                    return await sc.NextAsync();
-                }
-                else
-                {
-                    await sc.Context.SendActivityAsync(sc.Context.Activity.CreateReply(EmailSharedResponses.EmailNotFound, _responseBuilder));
-                    return await sc.EndDialogAsync(true);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw await HandleDialogExceptions(sc, ex);
-            }
-        }
-
-        public async Task<DialogTurnResult> PromptToRead(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            try
-            {
-                return await sc.PromptAsync(Action.Prompt, new PromptOptions { Prompt = sc.Context.Activity.CreateReply(ShowEmailResponses.ReadOutPrompt) });
-            }
-            catch (Exception ex)
-            {
-                throw await HandleDialogExceptions(sc, ex);
-            }
-        }
-
-        public async Task<DialogTurnResult> CallReadEmailDialog(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            try
-            {
-                return await sc.BeginDialogAsync(Action.Read);
-            }
-            catch (Exception ex)
-            {
-                throw await HandleDialogExceptions(sc, ex);
-            }
-        }
-
-        public async Task<DialogTurnResult> ReadEmail(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            try
-            {
-                var state = await _emailStateAccessor.GetAsync(sc.Context);
-                var luisResult = state.LuisResult;
-
-                var topIntent = luisResult?.TopIntent().intent;
-                if (topIntent == null)
-                {
-                    return await sc.EndDialogAsync(true);
-                }
-
-                var message = state.Message.FirstOrDefault();
-                if (topIntent == Email.Intent.ConfirmNo)
-                {
-                    await sc.Context.SendActivityAsync(
-                        sc.Context.Activity.CreateReply(EmailSharedResponses.CancellingMessage));
-                    return await sc.EndDialogAsync(true);
-                }
-                else if (topIntent == Email.Intent.ReadAloud && message == null)
-                {
-                    return await sc.PromptAsync(Action.Prompt, new PromptOptions { Prompt = sc.Context.Activity.CreateReply(ShowEmailResponses.ReadOutPrompt), });
-                }
-                else if (topIntent == Email.Intent.SelectItem || (topIntent == Email.Intent.ReadAloud && message != null))
-                {
-                    var nameListString = $"To: {message?.ToRecipients.FirstOrDefault()?.EmailAddress.Name}";
-                    if (message?.ToRecipients.Count() > 1)
-                    {
-                        nameListString += $" + {message.ToRecipients.Count() - 1} more";
-                    }
-
-                    var emailCard = new EmailCardData
-                    {
-                        Subject = message?.Subject,
-                        Sender = message?.Sender.EmailAddress.Name,
-                        NameList = nameListString,
-                        EmailContent = message?.BodyPreview,
-                        EmailLink = message?.WebLink,
-                        ReceivedDateTime = message?.ReceivedDateTime == null
-                            ? "Not available"
-                            : message.ReceivedDateTime.Value.UtcDateTime.ToRelativeString(state.GetUserTimeZone()),
-                        Speak = message?.Subject + " From " + message?.Sender.EmailAddress.Name + " " + message?.BodyPreview,
-                    };
-                    var replyMessage = sc.Context.Activity.CreateAdaptiveCardReply(ShowEmailResponses.ReadOutMessage, "Dialogs/Shared/Resources/Cards/EmailDetailCard.json", emailCard);
-                    await sc.Context.SendActivityAsync(replyMessage);
-
-                    return await sc.PromptAsync(Action.Prompt, new PromptOptions { Prompt = sc.Context.Activity.CreateReply(ShowEmailResponses.ReadOutMorePrompt) });
-                }
-                else
-                {
-                    return await sc.NextAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                throw await HandleDialogExceptions(sc, ex);
-            }
-        }
-
-        public async Task<DialogTurnResult> AfterReadOutEmail(WaterfallStepContext sc, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            try
-            {
-                var state = await _emailStateAccessor.GetAsync(sc.Context);
-                var luisResult = state.LuisResult;
-
-                var topIntent = luisResult?.TopIntent().intent;
-                if (topIntent == null)
-                {
-                    return await sc.EndDialogAsync(true);
-                }
-
-                if (topIntent == Email.Intent.ReadAloud || topIntent == Email.Intent.SelectItem)
-                {
-                    return await sc.BeginDialogAsync(Action.Read);
-                }
-                else
-                {
-                    // return a signal for main flow need to start a new ComponentDialog.
-                    return await sc.EndDialogAsync(true);
-                }
+                var mailService = this._serviceManager.InitMailService(state.MsGraphToken, state.GetUserTimeZone());
+                var focusMessage = state.Message.FirstOrDefault();
+                await mailService.DeleteMessage(focusMessage.Id);
+                return await sc.EndDialogAsync();
             }
             catch (Exception ex)
             {
