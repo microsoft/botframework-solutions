bd4dc731797ab5fa0fa9fa15b039ab59acc089bf
Darren Jefford <33519520+darrenj@users.noreply.github.com>
2018-10-25 20:51:57 +0100

    Overhaul of LU management and further localisation (#170)
    * Further Localisation
    Intoduction of IPA.ResetUser event to tear down state and remove tokens
    Add (commented out) translator support for deployments that need it
    Fixed CreateEventDialog bug around error messages
    
    * Changed PoI directions event to add address
    Added country detection based on locale (localisation)
    Added language parameter to azure maps call to enable localisation of responses
    
    * tweaks to enable full zh
    
    * Move to LU files, remove overlapping duplicate intents to general, translation of LU files including Chinese.
    
    * added in general luis intents where needed
    
    * updated LU files to remove last common pieces
    recovered deleteintent from json file (only changed there by STC team
    new technique for calling confirm recognizer within dialogs (not calling LUIS)
    
    * new dispatch model
    
    * workaround over zealous interruption (very low score)

---
+++ solutions/Virtual-Assistant/src/csharp/microsoft.bot.solutions/Dialogs/ConfirmRecognizerHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Recognizers.Text.Choice;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Solutions.Dialogs
+{
+    public static class ConfirmRecognizerHelper
+    {
+        public static PromptRecognizerResult<bool> ConfirmYesOrNo(string utterance, string locale = null)
+        {
+            var result = new PromptRecognizerResult<bool>();
+            if (!string.IsNullOrEmpty(utterance))
+            {
+                // Recognize utterance
+                var results = ChoiceRecognizer.RecognizeBoolean(utterance, locale);
+                if (results.Count > 0)
+                {
+                    var first = results[0];
+                    if (bool.TryParse(first.Resolution["value"].ToString(), out var value))
+                    {
+                        result.Succeeded = true;
+                        result.Value = value;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- solutions/Virtual-Assistant/src/csharp/skills/todoskill/Dialogs/Main/MainDialog.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/todoskill/Dialogs/Main/MainDialog.cs
@@ -100,8 +100,6 @@ namespace ToDoSkill
                         }
 
                     case ToDo.Intent.ShowToDo:
-                    case ToDo.Intent.Previous:
-                    case ToDo.Intent.Next:
                         {
                             await dc.BeginDialogAsync(nameof(ShowToDoItemDialog), skillOptions);
                             break;
@@ -197,6 +195,7 @@ namespace ToDoSkill
                 else
                 {
                     var luisResult = await luisService.RecognizeAsync<General>(dc.Context, cancellationToken);
+                    state.GeneralLuisResult = luisResult;
                     var topIntent = luisResult.TopIntent().intent;
 
                     // check intent
