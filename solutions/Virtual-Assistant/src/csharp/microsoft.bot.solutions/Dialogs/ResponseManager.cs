524a554909e764901faae0c45540b3f032f929a7
Gabo Gilabert <ggilaber@microsoft.com>
2018-10-26 15:44:44 -0400

    Moved response management logic into ResponseManager. Moved resusable tt code into a t4 include file. Created several tests.

---
+++ solutions/Virtual-Assistant/src/csharp/microsoft.bot.solutions/Dialogs/ResponseManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Solutions.Dialogs
+{
+    public class ResponseManager
+    {
+        private readonly string _jsonFilePath;
+        private readonly string _jsonFileSearchPattern;
+        private Dictionary<string, Dictionary<string, BotResponse>> _jsonResponses;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResponseManager"/> class.
+        /// </summary>
+        /// <param name="resourcePath">The full path to the resource files.</param>
+        /// <param name="resourceName">The name of the resources (e.g: MyResponses).</param>
+        public ResponseManager(string resourcePath, string resourceName)
+        {
+            _jsonFileSearchPattern = resourceName + ".*.json";
+            _jsonFilePath = resourcePath;
+        }
+
+        private Dictionary<string, Dictionary<string, BotResponse>> JsonResponses
+        {
+            get
+            {
+                if (_jsonResponses != null)
+                {
+                    return _jsonResponses;
+                }
+
+                _jsonResponses = new Dictionary<string, Dictionary<string, BotResponse>>();
+                var jsonFiles = Directory.GetFiles(_jsonFilePath, _jsonFileSearchPattern);
+                if (jsonFiles.Length == 0)
+                {
+                    throw new FileNotFoundException($"Unable to find resource files for \"{_jsonFileSearchPattern}\" under \"{_jsonFilePath}\".", Path.Combine(_jsonFilePath, _jsonFileSearchPattern));
+                }
+
+                foreach (var file in jsonFiles)
+                {
+                    try
+                    {
+                        string jsonData;
+                        using (var sr = new StreamReader(file, Encoding.GetEncoding("iso-8859-1")))
+                        {
+                            jsonData = sr.ReadToEnd();
+                        }
+
+                        var responses = JsonConvert.DeserializeObject<Dictionary<string, BotResponse>>(jsonData);
+                        var localeKey = new FileInfo(file).Name.Split(".")[1].ToLower();
+                        _jsonResponses.Add(localeKey, responses);
+                    }
+                    catch (JsonSerializationException ex)
+                    {
+                        throw new JsonSerializationException($"Error deserializing {file}. {ex.Message}", ex);
+                    }
+                }
+
+                return _jsonResponses;
+            }
+        }
+
+        public BotResponse GetBotResponse([CallerMemberName] string propertyName = null)
+        {
+            var locale = CultureInfo.CurrentUICulture.Name;
+            var key = GetJsonResponseKeyForLocale(locale, propertyName);
+
+            // fall back to parent language
+            if (key == null)
+            {
+                locale = CultureInfo.CurrentUICulture.Name.Split("-")[0].ToLower();
+                key = GetJsonResponseKeyForLocale(locale, propertyName);
+
+                // fall back to en
+                if (key == null)
+                {
+                    locale = "en";
+                    key = GetJsonResponseKeyForLocale(locale, propertyName, true);
+                }
+            }
+
+            var botResponse = JsonResponses[locale][key ?? throw new KeyNotFoundException($"Unable to find response \"{propertyName}\".")];
+            return JsonConvert.DeserializeObject<BotResponse>(JsonConvert.SerializeObject(botResponse));
+        }
+
+        private string GetJsonResponseKeyForLocale(string locale, string propertyName, bool isFallbackLanguage = false)
+        {
+            try
+            {
+                if (JsonResponses.ContainsKey(locale))
+                {
+                    return JsonResponses[locale].ContainsKey(propertyName) ? JsonResponses[locale].Keys.FirstOrDefault(k => string.Compare(k, propertyName, StringComparison.CurrentCultureIgnoreCase) == 0) : null;
+                }
+
+                return null;
+            }
+            catch (KeyNotFoundException)
+            {
+                if (isFallbackLanguage)
+                {
+                    throw;
+                }
+
+                return null;
+            }
+        }
+    }
+}
--- solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Dialogs/CreateEvent/Resources/CreateEventResponses.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Dialogs/CreateEvent/Resources/CreateEventResponses.cs
@@ -1,14 +1,11 @@
 ﻿  
+// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using Microsoft.Bot.Solutions.Dialogs;
-using Newtonsoft.Json;
 
 namespace CalendarSkill.Dialogs.CreateEvent.Resources
 {
@@ -17,11 +14,16 @@ namespace CalendarSkill.Dialogs.CreateEvent.Resources
     /// </summary>
     public static class CreateEventResponses
     {
-        private const string _jsonFileName = "CreateEventResponses.*.json";
+        private static readonly ResponseManager _responseManager;
 
-        private static Dictionary<string, Dictionary<string, BotResponse>> _jsonResponses;
+		static CreateEventResponses()
+        {
+            var dir = Path.GetDirectoryName(typeof(CreateEventResponses).Assembly.Location);
+            var resDir = Path.Combine(dir, @"Dialogs\CreateEvent\Resources");
+            _responseManager = new ResponseManager(resDir, "CreateEventResponses");
+        }
 
-        // Generated code:  
+        // Generated accessors  
         public static BotResponse NoTitle => GetBotResponse();
           
         public static BotResponse NoContent => GetBotResponse();
@@ -56,73 +58,9 @@ namespace CalendarSkill.Dialogs.CreateEvent.Resources
           
         public static BotResponse ConfirmRecipient => GetBotResponse();
                 
-        private static Dictionary<string, Dictionary<string, BotResponse>> JsonResponses
-        {
-            get
-            {
-                if (_jsonResponses != null)
-                {
-                    return _jsonResponses;
-                }
-
-                _jsonResponses = new Dictionary<string, Dictionary<string, BotResponse>>();
-                var dir = Path.GetDirectoryName(typeof(CreateEventResponses).Assembly.Location);
-                var resDir = Path.Combine(dir, @"Dialogs\CreateEvent\Resources");
-
-                var jsonFiles = Directory.GetFiles(resDir, _jsonFileName);
-                foreach (var file in jsonFiles)
-                {
-                    var jsonData = File.ReadAllText(file);
-                    var jsonResponses = JsonConvert.DeserializeObject<Dictionary<string, BotResponse>>(jsonData);
-                    var key = new FileInfo(file).Name.Split(".")[1].ToLower();
-
-                    _jsonResponses.Add(key, jsonResponses);
-                }
-
-                return _jsonResponses;
-            }
-        }
-
         private static BotResponse GetBotResponse([CallerMemberName] string propertyName = null)
         {
-            var locale = CultureInfo.CurrentUICulture.Name;
-            var theK = GetJsonResponseKeyForLocale(locale, propertyName);
-
-            // fall back to parent language
-            if (theK == null)
-            {
-                locale = CultureInfo.CurrentUICulture.Name.Split("-")[0].ToLower();
-                theK = GetJsonResponseKeyForLocale(locale, propertyName);
-
-                // fall back to en
-                if (theK == null)
-                {
-                    locale = "en";
-                    theK = GetJsonResponseKeyForLocale(locale, propertyName);
-                }
-            }
-
-            var botResponse = JsonResponses[locale][theK ?? throw new ArgumentNullException(nameof(propertyName))];
-            return JsonConvert.DeserializeObject<BotResponse>(JsonConvert.SerializeObject(botResponse));
-        }
-
-        private static string GetJsonResponseKeyForLocale(string locale, string propertyName)
-        {
-            try
-            {
-                if (JsonResponses.ContainsKey(locale))
-                {
-                    return JsonResponses[locale].ContainsKey(propertyName) ? 
-                        JsonResponses[locale].Keys.FirstOrDefault(k => string.Compare(k, propertyName, StringComparison.CurrentCultureIgnoreCase) == 0) : 
-                        null;
-                }
-
-                return null;
-            }
-            catch (KeyNotFoundException)
-            {
-                return null;
-            }
+            return _responseManager.GetBotResponse(propertyName);
         }
     }
 }
