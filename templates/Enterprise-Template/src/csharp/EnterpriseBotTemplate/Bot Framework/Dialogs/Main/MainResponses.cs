c79c7dacebaf8ae551c46f2fc94dd96ef57437e9
Ryan Lengel <rylengel@microsoft.com>
2018-11-06 11:20:31 -0800

    Fixed formatting

--- templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Main/MainResponses.cs
+++ templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Main/MainResponses.cs
@@ -12,18 +12,18 @@ using Newtonsoft.Json;
 namespace $safeprojectname$
 {
     public class MainResponses : TemplateManager
-    {
-        // Constants
-        public const string Cancelled = "cancelled";
-        public const string Completed = "completed";
-        public const string Confused = "confused";
-        public const string Greeting = "greeting";
-        public const string Help = "help";
-        public const string Intro = "intro";
+{
+    // Constants
+    public const string Cancelled = "cancelled";
+    public const string Completed = "completed";
+    public const string Confused = "confused";
+    public const string Greeting = "greeting";
+    public const string Help = "help";
+    public const string Intro = "intro";
 
-        private static LanguageTemplateDictionary _responseTemplates = new LanguageTemplateDictionary
-        {
-            ["default"] = new TemplateIdMap
+    private static LanguageTemplateDictionary _responseTemplates = new LanguageTemplateDictionary
+    {
+        ["default"] = new TemplateIdMap
             {
                 { Cancelled, (context, data) => MainStrings.CANCELLED },
                 { Completed, (context, data) => MainStrings.COMPLETED },
@@ -32,59 +32,59 @@ namespace $safeprojectname$
                 { Help, (context, data) => SendHelpCard(context, data) },
                 { Intro, (context, data) => SendIntroCard(context, data) },
             },
-            ["en"] = new TemplateIdMap { },
-            ["fr"] = new TemplateIdMap { },
+        ["en"] = new TemplateIdMap { },
+        ["fr"] = new TemplateIdMap { },
+    };
+
+    public MainResponses()
+    {
+        Register(new DictionaryRenderer(_responseTemplates));
+    }
+
+    public static IMessageActivity SendIntroCard(ITurnContext turnContext, dynamic data)
+    {
+        var response = turnContext.Activity.CreateReply();
+
+        var introCard = File.ReadAllText(@".\Dialogs\Main\Resources\Intro.json");
+
+        response.Attachments = new List<Attachment>();
+        response.Attachments.Add(new Attachment()
+        {
+            ContentType = "application/vnd.microsoft.card.adaptive",
+            Content = JsonConvert.DeserializeObject(introCard),
+        });
+
+        response.SuggestedActions = new SuggestedActions()
+        {
+            Actions = new List<CardAction>()
+                {
+                    new CardAction(type: ActionTypes.ImBack, title: "Test LUIS", value: "Hi there!"),
+                    new CardAction(type: ActionTypes.ImBack, title: "Test QnAMaker", value: "Why did Microsoft develop the Bot Framework?"),
+                    new CardAction(type: ActionTypes.OpenUrl, title: "Learn More", value: "https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-enterprise-template-overview?view=azure-bot-service-4.0")
+                }
         };
 
-        public MainResponses()
+        return response;
+    }
+
+    public static IMessageActivity SendHelpCard(ITurnContext turnContext, dynamic data)
+    {
+        var response = turnContext.Activity.CreateReply();
+        response.Attachments = new List<Attachment>();
+
+        response.Attachments.Add(new HeroCard()
         {
-            Register(new DictionaryRenderer(_responseTemplates));
-        }
-
-        public static IMessageActivity SendIntroCard(ITurnContext turnContext, dynamic data)
-        {
-            var response = turnContext.Activity.CreateReply();
-
-            var introCard = File.ReadAllText(@".\Dialogs\Main\Resources\Intro.json");
-
-            response.Attachments = new List<Attachment>();
-            response.Attachments.Add(new Attachment()
-            {
-                ContentType = "application/vnd.microsoft.card.adaptive",
-                Content = JsonConvert.DeserializeObject(introCard),
-            });
-
-            response.SuggestedActions = new SuggestedActions()
-            {
-                Actions = new List<CardAction>()
-                    {
-                        new CardAction(){ Type=ActionTypes.ImBack, Title = "Test LUIS", Value="Hi there!" },
-                        new CardAction(){ Type=ActionTypes.ImBack, Title = "Test QnAMaker", Value="Why did Microsoft develop the Bot Framework?" },
-                        new CardAction(){ Type=ActionTypes.OpenUrl, Title = "Learn More", Value="https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-enterprise-template-overview?view=azure-bot-service-4.0" }
-                    }
-            };
-
-            return response;
-        }
-
-        public static IMessageActivity SendHelpCard(ITurnContext turnContext, dynamic data)
-        {
-            var response = turnContext.Activity.CreateReply();
-            response.Attachments = new List<Attachment>();
-
-            response.Attachments.Add(new HeroCard()
-            {
-                Title = MainStrings.HELP_TITLE,
-                Text = MainStrings.HELP_TEXT,
-                Buttons = new List<CardAction>()
+            Title = MainStrings.HELP_TITLE,
+            Text = MainStrings.HELP_TEXT,
+            Buttons = new List<CardAction>()
                 {
                     new CardAction(type: ActionTypes.ImBack, title: "Test LUIS", value: "Hello"),
                     new CardAction(type: ActionTypes.ImBack, title: "Test QnAMaker", value: "What is the sdk v4?"),
                     new CardAction(type: ActionTypes.OpenUrl, title: "Learn More", value: "https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0"),
                 },
-            }.ToAttachment());
+        }.ToAttachment());
 
-            return response;
-        }
+        return response;
     }
+}
 }
