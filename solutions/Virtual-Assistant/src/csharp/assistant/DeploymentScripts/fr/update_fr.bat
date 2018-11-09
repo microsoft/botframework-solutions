c50d78907112e3d9b6b6ff78905e4322e9b859ce
Lauren Mills <lamil@microsoft.com>
2018-11-08 21:45:37 -0800

    updated .lu parsing scripts to use file location rather than current directory

--- solutions/Virtual-Assistant/src/csharp/assistant/DeploymentScripts/fr/update_fr.bat
+++ solutions/Virtual-Assistant/src/csharp/assistant/DeploymentScripts/fr/update_fr.bat
@@ -1,12 +1,12 @@
 @echo off
 
 ECHO Generating fr-fr LUIS and QnA Maker models from .lu files ..
-call ludown parse toluis --in CognitiveModels/LUIS/fr/general.lu -o DeploymentScripts/fr --out general.luis -n General
-call ludown parse toluis --in ../skills/calendarskill/CognitiveModels/LUIS/fr/calendar.lu -o DeploymentScripts/fr --out calendar.luis -n Calendar
-call ludown parse toluis --in ../skills/emailskill/CognitiveModels/LUIS/fr/email.lu -o DeploymentScripts/fr --out email.luis -n Email
-call ludown parse toluis --in ../skills/todoskill/CognitiveModels/LUIS/fr/todo.lu -o DeploymentScripts/fr --out todo.luis -n ToDo
-call ludown parse toluis --in ../skills/pointofinterestskill/CognitiveModels/LUIS/fr/pointofinterest.lu -o DeploymentScripts/fr --out pointofinterest.luis -n PointOfInterest
-call ludown parse toqna --in CognitiveModels/QnA/fr/faq.lu -o DeploymentScripts/fr -n faq.qna
-call ludown parse toluis --in CognitiveModels/LUIS/fr/dispatch.lu -o DeploymentScripts/fr --out dispatch.luis -n Dispatch -i Dispatch -c fr-fr
+call ludown parse toluis --in %0\..\..\..\CognitiveModels\LUIS\fr\general.lu -o %0\..\..\..\DeploymentScripts\fr --out general.luis -n General
+call ludown parse toluis --in %0\..\..\..\..\skills\calendarskill\CognitiveModels\LUIS\fr\calendar.lu -o %0\..\..\..\DeploymentScripts\fr --out calendar.luis -n Calendar
+call ludown parse toluis --in %0\..\..\..\..\skills\emailskill\CognitiveModels\LUIS\fr\email.lu -o %0\..\..\..\DeploymentScripts\fr --out email.luis -n Email
+call ludown parse toluis --in %0\..\..\..\..\skills\todoskill\CognitiveModels\LUIS\fr\todo.lu -o %0\..\..\..\DeploymentScripts\fr --out todo.luis -n ToDo
+call ludown parse toluis --in %0\..\..\..\..\skills\pointofinterestskill\CognitiveModels\LUIS\fr\pointofinterest.lu -o %0\..\..\..\DeploymentScripts\fr --out pointofinterest.luis -n PointOfInterest
+call ludown parse toqna --in %0\..\..\..\CognitiveModels\QnA\fr\faq.lu -o %0\..\..\..\DeploymentScripts\fr -n faq.qna
+call ludown parse toluis --in %0\..\..\..\CognitiveModels\LUIS\fr\dispatch.lu -o %0\..\..\..\DeploymentScripts\fr --out dispatch.luis -n Dispatch -i Dispatch -c fr-fr
 
 @echo on
