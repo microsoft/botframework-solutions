78d0a6d1b5eefcc09cc199845cff0451285016e5
Ryan Lengel Isgrig <rylengel@microsoft.com>
2018-11-09 14:49:55 -0800

    Integrated Web Test and Linked Accounts into a singular test harness Updated web chat to latest, using user's intials for avatar Adding all launchsettings.json,appsettings.Development.json to .gitignore to maintain consistency across repo Updated documentation for web test, direct line usage, tasks/todo usage

--- solutions/Virtual-Assistant/src/csharp/testharnesses/assistant-WebTest/Assistant-WebTest/Controllers/HomeController.cs
+++ solutions/Virtual-Assistant/src/csharp/testharnesses/assistant-WebTest/Assistant-WebTest/Controllers/HomeController.cs
@@ -10,6 +10,12 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Assistant_WebTest.Models;
 using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;using System.Security.Claims;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Connector.Authentication;
 
 namespace Assistant_WebTest.Controllers
 {
@@ -17,65 +23,185 @@ namespace Assistant_WebTest.Controllers
     public class HomeController : Controller
     {        
         public const string AadObjectidentifierClaim = "http://schemas.microsoft.com/identity/claims/objectidentifier";
-        public HomeController(IConfiguration configuration)
+        public HomeController(ICredentialProvider provider, IConfiguration configuration)
         {
             // Retrieve the Bot configuration
             directLineSecret = configuration.GetSection("DirectLineSecret").Value;
+            directLineEndpoint = configuration.GetSection("DirectLineEndpoint").Value;
             speechKey = configuration.GetSection("SpeechKey").Value;
             voiceName = configuration.GetSection("VoiceName").Value;
+            credentialProvider = provider;
         }
 
         public IActionResult Index()
         {
-            // Pass the DirectLine Secret, Speech Key and Voice Name
+            return View();
+        }
+
+        public IActionResult WebChat()
+        {
+            // Get DirectLine Token
+            // Pass the DirectLine Token, Speech Key and Voice Name
             // Note this approach will require magic code validation
-            ViewData["DirectLineSecret"] = directLineSecret;
-            ViewData["SpeechKey"] = speechKey;
-            ViewData["VoiceName"] = voiceName;
+            var directLineToken = string.Empty;
+            HttpResponseMessage response = GetDirectLineTokenResponse();
 
-            // Retrieve the object idenitifer for the user which will be the userID (fromID) passed to the Bot
-            var claimsIdentity = this.User?.Identity as System.Security.Claims.ClaimsIdentity;
-
-            if (claimsIdentity == null)
+            if (response.IsSuccessStatusCode)
             {
-                throw new InvalidOperationException("User is not logged in and needs to be.");
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                var directLineResponse = JsonConvert.DeserializeObject<DirectLineResponse>(responseString);
+                directLineToken = directLineResponse.token;
+
+                // Update as appropriate for your scenario to the unique identifier claim
+                return View(new WebChatViewModel()
+                {
+                    DirectLineToken = directLineToken,
+                    SpeechKey = speechKey,
+                    VoiceName = voiceName,
+                    UserID = this.GetUserId(),
+                    UserName = this.GetUserName()
+                });
             }
-
-            // Update as appropriate for your scenario to the unique identifier claim
-            var useriD = claimsIdentity.Claims?.SingleOrDefault(c => c.Type == HomeController.AadObjectidentifierClaim)?.Value;
-            ViewData["UserId"] = useriD;           
-
-            return View();
+            else
+            {
+                throw new InvalidOperationException($"Exchanging a DirectLine Secret for a Token failed, check your configuration settings. Error: {response.ReasonPhrase}");
+            }
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> LinkedAccounts()
         {
-            ViewData["Message"] = "Your application description page.";
+            var directLineToken = string.Empty;
+            HttpResponseMessage response = GetDirectLineTokenResponse();
 
-            return View();
-        }
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                var directLineResponse = JsonConvert.DeserializeObject<DirectLineResponse>(responseString);
+                directLineToken = directLineResponse.token;
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+                // Retrieve the object identifier for the user which will be the userID (fromID) passed to the Bot
+                var userId = this.GetUserId();
 
-            return View();
-        }
+                // Retrieve the status
+                TokenStatus[] tokenStatuses = await repository.GetTokenStatusAsync(userId, credentialProvider);
 
-        public IActionResult Privacy()
-        {
-            return View();
+                // Pass the User Id, Direct Line Token, Endpoint and Token Status to the View model
+                return View(new LinkedAccountsViewModel()
+                {
+                    UserId = userId,
+                    DirectLineToken = directLineToken,
+                    Endpoint = directLineEndpoint,
+                    Status = tokenStatuses
+                });
+            }
+            else
+            {
+                throw new InvalidOperationException($"Exchanging a DirectLine Secret for a Token failed, check your configuration settings. Error: {response.ReasonPhrase}");
+            }
         }
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private string GetUserId()
+        {
+            // Retrieve the object idenitifer for the user which will be the userID (fromID) passed to the Bot
+            var claimsIdentity = this.User?.Identity as ClaimsIdentity;
+
+            if (claimsIdentity == null)
+            {
+                throw new InvalidOperationException("User is not logged in and needs to be.");
+            }
+
+            var objectId = claimsIdentity.Claims?.SingleOrDefault(c => c.Type == AadObjectidentifierClaim)?.Value;
+
+            if (objectId == null)
+            {
+                throw new InvalidOperationException("User does not have a valid AAD ObjectId claim.");
+            }
+
+            return objectId;
+        }
+
+        private string GetUserName()
+        {
+            // Retrieve the object idenitifer for the user which will be the userID (fromID) passed to the Bot
+            var claimsIdentity = this.User?.Identity as ClaimsIdentity;
+
+            if (claimsIdentity == null)
+            {
+                throw new InvalidOperationException("User is not logged in and needs to be.");
+            }
+
+            var objectName = claimsIdentity.Claims?.SingleOrDefault(c => c.Type == "name")?.Value;
+
+            if (objectName == null)
+            {
+                throw new InvalidOperationException("User does not have a valid AAD ObjectId claim.");
+            }
+
+            return objectName;
+        }
+
+        private HttpResponseMessage GetDirectLineTokenResponse()
+        {
+            var directLineClient = new HttpClient();
+            directLineClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", directLineSecret);
+
+            // In order to avoid magic code prompts we need to set a TrustedOrigin, therefore requests using the token can be validated
+            // as coming from this web-site and protecting against scenarios where a URL is shared with someone else
+            string trustedOrigin = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+
+            var response = directLineClient.PostAsync($"{directLineEndpoint}/tokens/generate", new StringContent(JsonConvert.SerializeObject(new { TrustedOrigins = new string[] { trustedOrigin } }), Encoding.UTF8, "application/json")).Result;
+            return response;
+        }
+
+        /// <summary>
+        /// Retrieve a URL for the user to link a given connection name to their Bot
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> SignIn(TokenStatus account)
+        {
+            var userId = GetUserId();
+
+            string link = await repository.GetSignInLinkAsync(userId, credentialProvider, account.ConnectionName, $"{this.Request.Scheme}://{this.Request.Host.Value}/Home/LinkedAccounts");
+
+            return Redirect(link);
+        }
+
+        /// <summary>
+        /// Sign a user out of a given connection name previously linked to their Bot
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> SignOut(TokenStatus account)
+        {
+            var userId = GetUserId();
+
+            await this.repository.SignOutAsync(userId, credentialProvider, account.ConnectionName);
+
+            return RedirectToAction("LinkedAccounts");
+        }
+
+        public async Task<IActionResult> SignOutAll()
+        {
+            var userId = GetUserId();
+
+            await this.repository.SignOutAsync(userId, credentialProvider);
+
+            return RedirectToAction("LinkedAccounts");
         }
 
         private string directLineSecret;
+        private string directLineEndpoint;
         private string speechKey;
         private string voiceName;
+        private ICredentialProvider credentialProvider;
+        private ILinkedAccountRepository repository = new LinkedAccountRepository();
     }
 }
