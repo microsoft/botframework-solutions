db773ca3a9d7da27178702aa9f63baa15256647d
Lauren Mills <lamil@microsoft.com>
2018-11-09 10:25:38 -0800

    updated formatting

--- solutions/Virtual-Assistant/src/csharp/assistant/DeploymentScripts/en/update_en.bat
+++ solutions/Virtual-Assistant/src/csharp/assistant/DeploymentScripts/en/update_en.bat
@@ -6,7 +6,7 @@ call ludown parse toluis --in %0\..\..\..\..\skills\calendarskill\CognitiveModel
 call ludown parse toluis --in %0\..\..\..\..\skills\emailskill\CognitiveModels\LUIS\en\email.lu -o %0\..\..\..\DeploymentScripts\en -n Email --out email.luis -n Email
 call ludown parse toluis --in %0\..\..\..\..\skills\pointofinterestskill\CognitiveModels\LUIS\en\pointofinterest.lu -o %0\..\..\..\DeploymentScripts\en -n PointOfInterest --out pointofinterest.luis
 call ludown parse toluis --in %0\..\..\..\..\skills\todoskill\CognitiveModels\LUIS\en\todo.lu -o %0\..\..\..\DeploymentScripts\en -n ToDo --out todo.luis
-call ludown parse toqna	 --in %0\..\..\..\CognitiveModels\QnA\en\faq.lu -o %0\..\..\..\DeploymentScripts\en -n faq.qna
+call ludown parse toqna  --in %0\..\..\..\CognitiveModels\QnA\en\faq.lu -o %0\..\..\..\DeploymentScripts\en -n faq.qna
 call ludown parse toluis --in %0\..\..\..\CognitiveModels\LUIS\en\dispatch.lu -o %0\..\..\..\DeploymentScripts\en --out dispatch.luis -n Dispatch -i Dispatch
 
 @echo on
