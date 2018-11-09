5a94dc6f476a3b7ad2d731a678a997110b30cf8b
yuesu <yuesu@microsoft.com>
2018-11-09 19:31:35 +0800

    show meetings from now on only when ask for today meeting

--- solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Models/EventModel.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Models/EventModel.cs
@@ -694,7 +694,7 @@ namespace CalendarSkill
             };
         }
 
-        public bool IsSameDate(DateTime dateTime1, DateTime dateTime2)
+        public static bool IsSameDate(DateTime dateTime1, DateTime dateTime2)
         {
             return dateTime1.Year == dateTime2.Year && dateTime1.Month == dateTime2.Month && dateTime1.Day == dateTime2.Day;
         }
