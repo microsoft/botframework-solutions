da63764f8ca0fb642830932f2bda868d35954a12
lauren-mills <lamil@microsoft.com>
2018-11-14 09:46:28 -0800

    updated enteprise bot to use latest telemetry + other small formattin… (#290)
    * updated enteprise bot to use latest telemetry + other small formatting changes
    
    * updated enterprise template with new language files
    
    * updated intro card with diff languages

--- templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Onboarding/OnboardingResponses.cs
+++ templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Onboarding/OnboardingResponses.cs
@@ -4,7 +4,7 @@
 using $safeprojectname$.Dialogs.Onboarding.Resources;
 using Microsoft.Bot.Builder.TemplateManager;
 
-namespace $safeprojectname$
+namespace $safeprojectname$.Dialogs.Onboarding
 {
     public class OnboardingResponses : TemplateManager
     {
