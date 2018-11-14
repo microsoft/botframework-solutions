d632b56d60a8d7d6d36e975f88ff1044714e5d5b
lauren-mills <lamil@microsoft.com>
2018-11-14 09:46:28 -0800

    updated enteprise bot to use latest telemetry + other small formattin… (#290)
    * updated enteprise bot to use latest telemetry + other small formatting changes
    
    * updated enterprise template with new language files
    
    * updated intro card with diff languages

--- templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Onboarding/OnboardingState.cs
+++ templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Onboarding/OnboardingState.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-namespace $safeprojectname$
+namespace $safeprojectname$.Dialogs.Onboarding
 {
     public class OnboardingState
     {
