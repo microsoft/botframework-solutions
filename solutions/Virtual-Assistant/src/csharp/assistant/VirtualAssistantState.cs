cc2864f5034d2cb28eca4263ac485e2e54bbbfe7
Fei Chen <feich@microsoft.com>
2018-11-16 16:40:28 +0800

    Enable Next/Previous intents in skill mode

---
+++ solutions/Virtual-Assistant/src/csharp/assistant/VirtualAssistantState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace VirtualAssistant
+{
+    public class VirtualAssistantState
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirtualAssistantState"/> class.
+        /// </summary>
+        public VirtualAssistantState()
+        {
+            ExecutedIntents = new List<string>();
+        }
+
+        /// <summary>
+        /// Gets or sets ExecutedIntents.
+        /// </summary>
+        /// <value>
+        /// ToDoTaskContent.
+        /// </value>
+        public List<string> ExecutedIntents { get; set; }
+    }
+}
