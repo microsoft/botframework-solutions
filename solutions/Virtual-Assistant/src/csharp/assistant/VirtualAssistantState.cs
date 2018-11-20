ce42115aeac437c2be34f6cef90ed229156797f8
Fei Chen <feich@microsoft.com>
2018-11-20 09:51:55 +0800

    fix comments

--- solutions/Virtual-Assistant/src/csharp/assistant/VirtualAssistantState.cs
+++ solutions/Virtual-Assistant/src/csharp/assistant/VirtualAssistantState.cs
@@ -3,14 +3,6 @@
     public class VirtualAssistantState
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="VirtualAssistantState"/> class.
-        /// </summary>
-        public VirtualAssistantState()
-        {
-            LastIntent = null;
-        }
-
-        /// <summary>
         /// Gets or sets LastIntent.
         /// </summary>
         /// <value>
