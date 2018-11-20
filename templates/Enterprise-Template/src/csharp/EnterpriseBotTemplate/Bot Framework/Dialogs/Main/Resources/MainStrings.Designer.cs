49be1f906064dfc9663413cfb99344be56d81bd1
Lauren Mills <lamil@microsoft.com>
2018-11-20 15:32:29 -0800

    fixed template bug

--- templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Main/Resources/MainStrings.Designer.cs
+++ templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Main/Resources/MainStrings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EnterpriseBot.Dialogs.Main.Resources {
+namespace $safeprojectname$.Dialogs.Main.Resources {
     using System;
     
     
