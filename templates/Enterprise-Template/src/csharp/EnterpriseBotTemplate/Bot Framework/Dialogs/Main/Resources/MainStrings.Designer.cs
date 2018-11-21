a8c338bfa6c1403bd1ef7496bfd0dac1797263da
Lauren Mills <lamil@microsoft.com>
2018-11-20 16:07:07 -0800

    removed azure resource references

--- templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Main/Resources/MainStrings.Designer.cs
+++ templates/Enterprise-Template/src/csharp/EnterpriseBotTemplate/Bot Framework/Dialogs/Main/Resources/MainStrings.Designer.cs
@@ -39,7 +39,7 @@ namespace $safeprojectname$.Dialogs.Main.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EnterpriseBot.Dialogs.Main.Resources.MainStrings", typeof(MainStrings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("$safeprojectname$.Dialogs.Main.Resources.MainStrings", typeof(MainStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
