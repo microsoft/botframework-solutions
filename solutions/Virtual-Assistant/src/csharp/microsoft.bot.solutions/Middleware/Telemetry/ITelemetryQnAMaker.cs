283068d1fe90e58883ea60c1e34929930ebbf84d
Gabo Gilabert <gabog@users.noreply.github.com>
2018-11-28 09:15:04 -0500

    Added interfaces for TelemetryQnaMaker and TelemetryLuisRecognizer to be able to creat fakes for unit tests. (#362)

---
+++ solutions/Virtual-Assistant/src/csharp/microsoft.bot.solutions/Middleware/Telemetry/ITelemetryQnAMaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.QnA;
+
+namespace Microsoft.Bot.Solutions
+{
+    public interface ITelemetryQnAMaker
+    {
+        bool LogOriginalMessage { get; }
+        bool LogUserName { get; }
+
+        Task<QueryResult[]> GetAnswersAsync(ITurnContext context);
+    }
+}
