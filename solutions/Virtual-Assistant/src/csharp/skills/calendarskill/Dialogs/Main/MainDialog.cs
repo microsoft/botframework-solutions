96f6f1422de86cf5e0b1ded9248881484b6d17ca
Fei Chen <43032123+feich-ms@users.noreply.github.com>
2018-11-29 11:01:55 +0800

    Remove cancel confirmation for ToDo/Email/Calendar skill (#352)
    * remove cancel confirmation for ToDo/Email/Calendar skill
    
    * remove cancel dialogs from skills and add cancel login in main dialog of each skill
    
    * add dc.CancelAllDialogsAsync to make sure all the dialogs in the stack are cleared out

--- solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Dialogs/Main/MainDialog.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Dialogs/Main/MainDialog.cs
@@ -258,7 +258,9 @@ namespace CalendarSkill
 
         private async Task<InterruptionAction> OnCancel(DialogContext dc)
         {
-            await dc.BeginDialogAsync(nameof(CancelDialog));
+            await dc.Context.SendActivityAsync(dc.Context.Activity.CreateReply(CalendarMainResponses.CancelMessage));
+            await CompleteAsync(dc);
+            await dc.CancelAllDialogsAsync();
             return InterruptionAction.StartedDialog;
         }
 
@@ -302,7 +304,6 @@ namespace CalendarSkill
             AddDialog(new NextMeetingDialog(_services, _stateAccessor, _serviceManager));
             AddDialog(new SummaryDialog(_services, _stateAccessor, _serviceManager));
             AddDialog(new UpdateEventDialog(_services, _stateAccessor, _serviceManager));
-            AddDialog(new CancelDialog(_stateAccessor));
         }
 
         private class Events
