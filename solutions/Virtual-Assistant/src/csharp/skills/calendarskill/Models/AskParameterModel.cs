ff90078ae99e94df191ea09b4ae0750b59776939
Chengxian Mo <chenmo@microsoft.com>
2018-11-29 14:40:38 +0800

    add parameter model

---
+++ solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Models/AskParameterModel.cs
@@ -0,0 +1,123 @@
+﻿using CalendarSkill.Models.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarSkill.Models
+{
+    public enum AskParameterType
+    {
+        /// <summary>
+        /// defaut type, ask for details about event
+        /// </summary>
+        AskForDetail = 0,
+
+        /// <summary>
+        /// ask for start time
+        /// </summary>
+        AskForStartTime = 1,
+
+        /// <summary>
+        /// ask for end time
+        /// </summary>
+        AskForEndTime = 2,
+
+        /// <summary>
+        /// ask for start and end time
+        /// </summary>
+        AskForTime = 3,
+
+        /// <summary>
+        /// ask for duration
+        /// </summary>http://tianqi.sogou.com/beijing/
+        AskForDuration = 4,
+
+        /// <summary>
+        /// ask for location
+        /// </summary>
+        AskForLocation = 5,
+
+        /// <summary>
+        /// ask for attenddees
+        /// </summary>
+        AskForAttendee = 6,
+
+        /// <summary>
+        /// ask for title
+        /// </summary>
+        AskForTitle = 7,
+
+        /// <summary>
+        /// ask for content
+        /// </summary>
+        AskForContent = 8,
+    }
+
+    public class AskParameterModel
+    {
+        public bool NeedDetail { get; set; } = false;
+
+        public bool NeedTime { get; set; } = false;
+
+        public bool NeedDuration { get; set; } = false;
+
+        public bool NeedLocation { get; set; } = false;
+
+        public AskParameterModel(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                return;
+            }
+
+            content = content.ToLower();
+            List<AskParameterType> askParameterTypes = AskParameterTemplate.GetAskParameterTypes(content);
+            foreach (AskParameterType type in askParameterTypes)
+            {
+                switch (type)
+                {
+                    case AskParameterType.AskForStartTime:
+                        {
+                            NeedTime = true;
+                            NeedDetail = true;
+                            break;
+                        }
+
+                    case AskParameterType.AskForEndTime:
+                        {
+                            NeedTime = true;
+                            NeedDetail = true;
+                            break;
+                        }
+
+                    case AskParameterType.AskForTime:
+                        {
+                            NeedTime = true;
+                            NeedDetail = true;
+                            break;
+                        }
+
+                    case AskParameterType.AskForDuration:
+                        {
+                            NeedDuration = true;
+                            NeedDetail = true;
+                            break;
+                        }
+
+                    case AskParameterType.AskForLocation:
+                        {
+                            NeedLocation = true;
+                            NeedDetail = true;
+                            break;
+                        }
+
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ solutions/Virtual-Assistant/src/csharp/skills/calendarskill/Models/Resources/AskParameterTemplate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CalendarSkill.Models.Resources
+{
+    public static class AskParameterTemplate
+    {
+        private static Dictionary<string, AskParameterType> templateMapping;
+
+        static AskParameterTemplate()
+        {
+            templateMapping = new Dictionary<string, AskParameterType>();
+            var dir = Path.GetDirectoryName(typeof(AskParameterTemplate).Assembly.Location);
+            var resDir = Path.Combine(dir, @"Models\Resources\AskParameterTemplate.txt");
+            StreamReader sr = new StreamReader(resDir, Encoding.Default);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                string[] parts = line.Split("\t");
+                if (parts[0].Length > 0 && parts[1].Length > 0)
+                {
+                    AskParameterType askParameterType = Enum.Parse<AskParameterType>(parts[0], true);
+                    templateMapping.Add(parts[1], askParameterType);
+                }
+            }
+        }
+
+        public static List<AskParameterType> GetAskParameterTypes(string content)
+        {
+            List<AskParameterType> types = new List<AskParameterType>();
+            if (string.IsNullOrEmpty(content))
+            {
+                return types;
+            }
+            foreach (string key in templateMapping.Keys)
+            {
+                Regex regex = new Regex(key);
+                if (regex.IsMatch(content))
+                {
+                    types.Add(templateMapping[key]);
+                }
+            }
+
+            return types;
+        }
+    }
+}
