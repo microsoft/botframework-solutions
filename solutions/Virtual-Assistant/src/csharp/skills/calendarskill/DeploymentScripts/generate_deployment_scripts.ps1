59373884107b94e6b849f8c6145426b1374caaea
Lauren Mills <lamil@microsoft.com>
2018-12-05 09:55:53 -0600

    duplicate utterances don't break deployment

--- solutions/Virtual-Assistant/src/csharp/skills/calendarskill/DeploymentScripts/generate_deployment_scripts.ps1
+++ solutions/Virtual-Assistant/src/csharp/skills/calendarskill/DeploymentScripts/generate_deployment_scripts.ps1
@@ -11,25 +11,18 @@ $calendarLUPath = "$($basePath)\CognitiveModels\LUIS\$($langCode)\calendar.lu"
 $generalLUPath = "$($basePath)\..\..\assistant\CognitiveModels\LUIS\$($langCode)\general.lu"
 
 $luArr = @($calendarLUPath, $generalLUPath)
-$hasDuplicates = 0
 
 Write-Host "Updating $($locale) deployment scripts ..."
-
 foreach ($lu in $luArr) 
 {
 	$duplicates = Get-Content $lu | Group-Object | Where-Object { $_.Count -gt 1 } | Select -ExpandProperty Name
 
 	if ($duplicates.Count -gt 1) 
 	{
-		Write-Host "$($duplicates.Count - 1) duplicate utterances found in $($lu):"
-		Write-Host $duplicates 
-		$hasDuplicates = 1
+		Write-Error "$($duplicates.Count - 1) duplicate utterances found in $($lu). This could cause issues in your model accuracy."
 	}
 }
 
-if ($hasDuplicates -eq 0) 
-{
-	Write-Host "Generating $($locale) LUIS and QnA Maker models from .lu files ..."
-	ludown parse toluis -c $($locale) -o $outputPath --in $calendarLUPath --out calendar.luis -n Calendar
-	ludown parse toluis -c $($locale) -o $outputPath --in $generalLUPath --out general.luis -n General
-}
+Write-Host "Generating $($locale) LUIS and QnA Maker models from .lu files ..."
+ludown parse toluis -c $($locale) -o $outputPath --in $calendarLUPath --out calendar.luis -n Calendar
+ludown parse toluis -c $($locale) -o $outputPath --in $generalLUPath --out general.luis -n General
