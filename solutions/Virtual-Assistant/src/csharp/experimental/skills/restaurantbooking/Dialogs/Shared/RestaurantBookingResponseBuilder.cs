950f762e57a2f05fb0f37155fa86d0340b140d1c
Darren Jefford <darrenj@microsoft.com>
2018-12-07 16:54:12 +0000

    Base skill project

---
+++ solutions/Virtual-Assistant/src/csharp/experimental/skills/restaurantbooking/Dialogs/Shared/RestaurantBookingResponseBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Bot.Solutions.Dialogs;
+using Microsoft.Bot.Solutions.Dialogs.BotResponseFormatters;
+
+namespace RestaurantBooking
+{
+    public class RestaurantBookingResponseBuilder : BotResponseBuilder
+    {
+        public RestaurantBookingResponseBuilder()
+           : base()
+        {
+            AddFormatter(new TextBotResponseFormatter());
+        }
+    }
+}
---
+++ solutions/Virtual-Assistant/src/csharp/experimental/skills/restaurantbooking/RestaurantBookingState.cs
@@ -0,0 +1,19 @@
+﻿using Luis;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+
+namespace RestaurantBooking
+{
+    public class RestaurantBookingState
+    {
+        public RestaurantBookingState()
+        {
+
+        }
+
+        public Skill LuisResult { get; set; }
+    }
+}
