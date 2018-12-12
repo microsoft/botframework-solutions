aac5c63a99ded15953843c9ff9cca870ddfe9c7e
Lauren Mills <lamil@microsoft.com>
2018-12-12 15:05:01 -0600

    added news skill

---
+++ solutions/Virtual-Assistant/src/csharp/experimental/skills/newsskill/Dialogs/Main/MainDialog.cs
@@ -0,0 +1,164 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Luis;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Solutions;
+using Microsoft.Bot.Solutions.Dialogs;
+using Microsoft.Bot.Solutions.Skills;
+using NewsSkill.Dialogs.Main.Resources;
+
+namespace NewsSkill
+{
+    public class MainDialog : RouterDialog
+    {
+        private bool _skillMode;
+        private SkillConfiguration _services;
+        private UserState _userState;
+        private ConversationState _conversationState;
+        private MainResponses _responder = new MainResponses();
+
+        public MainDialog(SkillConfiguration services, ConversationState conversationState, UserState userState, bool skillMode)
+            : base(nameof(MainDialog))
+        {
+            _skillMode = skillMode;
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+            _conversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+            _userState = userState ?? throw new ArgumentNullException(nameof(userState));
+
+            RegisterDialogs();
+        }
+
+        protected override async Task OnStartAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (!_skillMode)
+            {
+                // send a greeting if we're in local mode
+                await _responder.ReplyWith(dc.Context, MainResponses.Intro);
+            }
+        }
+
+        protected override async Task RouteAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // If dispatch result is general luis model
+            _services.LuisServices.TryGetValue("news", out var luisService);
+
+            if (luisService == null)
+            {
+                throw new Exception("The specified LUIS Model could not be found in your Bot Services configuration.");
+            }
+            else
+            {
+                var result = await luisService.RecognizeAsync<News>(dc.Context, CancellationToken.None);
+
+                var intent = result?.TopIntent().intent;
+
+                // switch on general intents
+                switch (intent)
+                {
+                    case News.Intent.FindNews:
+                        {
+                            // send greeting response
+                            await dc.BeginDialogAsync(nameof(FindArticlesDialog));
+                            break;
+                        }
+
+                    case News.Intent.None:
+                        {
+                            // No intent was identified, send confused message
+                            await _responder.ReplyWith(dc.Context, MainResponses.Confused);
+                            break;
+                        }
+
+                    default:
+                        {
+                            // intent was identified but not yet implemented
+                            await dc.Context.SendActivityAsync("This feature is not yet implemented in this skill.");
+                            break;
+                        }
+                }
+            }
+        }
+
+        protected override async Task CompleteAsync(DialogContext dc, DialogTurnResult result = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (_skillMode)
+            {
+                var response = dc.Context.Activity.CreateReply();
+                response.Type = ActivityTypes.EndOfConversation;
+
+                await dc.Context.SendActivityAsync(response);
+            }
+            else
+            {
+                await _responder.ReplyWith(dc.Context, MainResponses.Completed);
+            }
+
+            // End active dialog
+            await dc.EndDialogAsync(result);
+        }
+
+        protected override async Task<InterruptionAction> OnInterruptDialogAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var result = InterruptionAction.NoAction;
+
+            if (dc.Context.Activity.Type == ActivityTypes.Message)
+            {
+                // check luis intent
+                _services.LuisServices.TryGetValue("general", out var luisService);
+
+                if (luisService == null)
+                {
+                    throw new Exception("The specified LUIS Model could not be found in your Skill configuration.");
+                }
+                else
+                {
+                    var luisResult = await luisService.RecognizeAsync<General>(dc.Context, cancellationToken);
+                    var topIntent = luisResult.TopIntent().intent;
+
+                    switch (topIntent)
+                    {
+                        case General.Intent.Cancel:
+                            {
+                                result = await OnCancel(dc);
+                                break;
+                            }
+
+                        case General.Intent.Help:
+                            {
+                                // result = await OnHelp(dc);
+                                break;
+                            }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private async Task<InterruptionAction> OnCancel(DialogContext dc)
+        {
+            await _responder.ReplyWith(dc.Context, MainResponses.Cancelled);
+            await CompleteAsync(dc);
+            await dc.CancelAllDialogsAsync();
+            return InterruptionAction.StartedDialog;
+        }
+
+        private async Task<InterruptionAction> OnHelp(DialogContext dc)
+        {
+            await _responder.ReplyWith(dc.Context, MainResponses.Help);
+            return InterruptionAction.MessageSentToUser;
+        }
+
+        private void RegisterDialogs()
+        {
+            AddDialog(new FindArticlesDialog(_services));
+        }
+    }
+}
---
+++ solutions/Virtual-Assistant/src/csharp/experimental/skills/newsskill/Dialogs/Shared/EnterpriseDialog.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Luis;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Solutions.Dialogs;
+using Microsoft.Bot.Solutions.Skills;
+
+namespace NewsSkill
+{
+    public class EnterpriseDialog : InterruptableDialog
+    {
+        protected const string LuisResultKey = "LuisResult";
+
+        // Fields
+        private readonly SkillConfiguration _services;
+
+        public EnterpriseDialog(SkillConfiguration botServices, string dialogId)
+            : base(dialogId)
+        {
+            _services = botServices;
+        }
+
+        protected override async Task<InterruptionAction> OnInterruptDialogAsync(DialogContext dc, CancellationToken cancellationToken)
+        {
+            // check luis intent
+            _services.LuisServices.TryGetValue("general", out var luisService);
+
+            if (luisService == null)
+            {
+                throw new Exception("The specified LUIS Model could not be found in your Bot Services configuration.");
+            }
+            else
+            {
+                var luisResult = await luisService.RecognizeAsync<General>(dc.Context, cancellationToken);
+                var intent = luisResult.TopIntent().intent;
+
+                // Add the luis result (intent and entities) for further processing in the derived dialog
+                dc.Context.TurnState.Add(LuisResultKey, luisResult);
+
+                switch (intent)
+                {
+                    case General.Intent.Cancel:
+                        {
+                            return await OnCancel(dc);
+                        }
+
+                    case General.Intent.Help:
+                        {
+                            return await OnHelp(dc);
+                        }
+                }
+            }
+
+            return InterruptionAction.NoAction;
+        }
+
+        protected virtual async Task<InterruptionAction> OnCancel(DialogContext dc)
+        {
+            //if (dc.ActiveDialog.Id != nameof(CancelDialog))
+            //{
+            //    // Don't start restart cancel dialog
+            //    await dc.BeginDialogAsync(nameof(CancelDialog));
+
+            //    // Signal that the dialog is waiting on user response
+            //    return InterruptionAction.StartedDialog;
+            //}
+
+            // Else, continue
+            return InterruptionAction.NoAction;
+        }
+
+        protected virtual async Task<InterruptionAction> OnHelp(DialogContext dc)
+        {
+            var view = new MainResponses();
+            await view.ReplyWith(dc.Context, MainResponses.Help);
+
+            // Signal the conversation was interrupted and should immediately continue
+            return InterruptionAction.MessageSentToUser;
+        }
+    }
+}
