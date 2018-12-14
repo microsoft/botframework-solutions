1a01b16fdf07366af12575a154fc63cca11c5cd9
Darren Jefford <darrenj@microsoft.com>
2018-12-14 10:45:06 +0000

    updated deployment

--- solutions/Virtual-Assistant/src/csharp/skills/automotiveskill/DeploymentScripts/update_deployment_scripts.bat
+++
@@ -1,6 +0,0 @@
-﻿ECHO Updating deployment files in Automotive Skill
-call ludown parse toluis --in %0\..\..\..\..\assistant\CognitiveModels\LUIS\en\general.lu -o %0\..\en -n General --out general.luis
-call ludown parse toluis --in %0\..\..\CognitiveModels\LUIS\en\settings.lu -o %0\..\en -n settings --out settings.luis
-call ludown parse toluis --in %0\..\..\CognitiveModels\LUIS\en\settings_name.lu -o %0\..\en -n settings_name --out settings_name.luis
-call ludown parse toluis --in %0\..\..\CognitiveModels\LUIS\en\settings_value.lu -o %0\..\en -n settings_value --out settings_value.luis
-
