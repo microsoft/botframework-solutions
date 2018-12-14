ddbd4fe89a584f8db44182cc7b18f2bcba97a5e4
Darren Jefford <33519520+darrenj@users.noreply.github.com>
2018-12-14 12:16:06 +0000

    Automotive Skill - VehicleSettings support (english only) (#428)
    * initial port from example
    
    * Simpification, confirmation prompts
    
    * directory layout change
    
    * Major refactor to typed LUIS models (incomplete)
    Unit Test framework and first unit test
    Removed custom confirmation mechanism proposed in earlier codebase
    
    * Refactoring
    Tests
    Further simplification
    ChoicePrompts for Names and Settings
    
    * refactored settingfilter
    declarative support
    remembers settings values even after disambiguating settingname - e.g. increase bass
    updated deployment
    
    * Setting fixes for declarative
    New Tests and mocks
    
    * Updated deployment
    Refined LU
    CSM to Audio
    Skill Documentation
    
    * StyleCop changes
    
    * Updates for new LocaleConfiguration
    SLN merge fix
    
    * update stylecopy analyzers package
    
    * update to not use .bot file for tests as per new test flows
    
    * PR review changes
    
    * update to 4.2
    
    * updated deployment
    
    * solution file merge fixes
    setting choice prompt formatting
    telemetry updates to reflect 4.2
    
    * docs update
    
    * updated readme

---
+++ solutions/Virtual-Assistant/src/csharp/skills/automotiveskill/Models/SelectableSettingValue.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomotiveSkill.Models
+{
+    /// <summary>
+    /// A setting value that can be selected from a list.
+    /// </summary>
+    public class SelectableSettingValue
+    {
+        /// <summary>
+        /// Gets or sets the canonical name of the setting this value belongs to.
+        /// </summary>
+        /// <value>The canonical name of the setting.</value>
+        public string CanonicalSettingName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the setting value.
+        /// </summary>
+        /// <value>The Available Setting value.</value>
+        public AvailableSettingValue Value { get; set; }
+    }
+}
---
+++ solutions/Virtual-Assistant/src/csharp/skills/tests/automotiveskilltest/Flow/Fakes/MockVehicleSettingsValueIntent.cs
@@ -0,0 +1,37 @@
+﻿using AutomotiveSkill;
+using Luis;
+using Microsoft.Bot.Builder;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AutomotiveSkillTest.Flow.Fakes
+{
+    public class MockVehicleSettingsValueIntent : VehicleSettingsValueSelection
+    {
+        public string userInput;
+        private Intent intent;
+        private double score;     
+
+        public MockVehicleSettingsValueIntent(string userInput)
+        {
+            if (string.IsNullOrEmpty(userInput))
+            {
+                throw new ArgumentNullException(nameof(userInput));
+            }
+
+            this.Entities = new VehicleSettingsValueSelection._Entities();
+            this.Intents = new Dictionary<Intent, IntentScore>();
+
+            var intentScore = new Microsoft.Bot.Builder.IntentScore();
+            intentScore.Score = 0.9909704;
+            intentScore.Properties = new Dictionary<string, object>();
+
+            // This LUIS model is for followup setting clarification so we assume input for test purposes is the setting name
+            this.Intents.Add(VehicleSettingsValueSelection.Intent.SETTING_VALUE_SELECTION, intentScore);
+            this.Entities.VALUE = new string[] { userInput.ToLower() };
+
+            (intent, score) = this.TopIntent();
+        }            
+    }
+}
