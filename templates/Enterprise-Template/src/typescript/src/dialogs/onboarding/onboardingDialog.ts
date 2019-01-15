64667971a25a301893a77f993cf93050feece525
Martin Battaglino <martinbatta32@gmail.com>
2019-01-15 10:23:17 -0300

    Update OnBoarding Dialog and Responses

--- templates/Enterprise-Template/src/typescript/src/dialogs/onboarding/onboardingDialog.ts
+++ templates/Enterprise-Template/src/typescript/src/dialogs/onboarding/onboardingDialog.ts
@@ -9,30 +9,30 @@ import { OnboardingResponses } from "./onboardingResponses";
 import { OnboardingState } from "./onboardingState";
 
 export class OnboardingDialog extends EnterpriseDialog {
-    private readonly NamePrompt: string = "namePrompt";
-    private readonly EmailPrompt: string = "emailPrompt";
-    private readonly LocationPrompt: string = "locationPrompt";
+
+    // Fields
     private readonly _responder: OnboardingResponses;
     private readonly _accessor: StatePropertyAccessor<OnboardingState>;
     private _state!: OnboardingState;
 
     constructor(botServices: BotServices, accessor: StatePropertyAccessor<OnboardingState>) {
-        super(botServices, "OnboardingDialog");
+        super(botServices, OnboardingDialog.name);
 
         this._accessor = accessor;
-        this.initialDialogId = "OnboardingDialog";
+        this.initialDialogId = OnboardingDialog.name;
         this._responder = new OnboardingResponses();
 
-        this.addDialog(new WaterfallDialog<OnboardingState>(this.initialDialogId, [
+        const onboarding = [
             this.askForName.bind(this),
             this.askForEmail.bind(this),
             this.askForLocation.bind(this),
             this.finishOnboardingDialog.bind(this),
-        ]));
-        this.addDialog(new TextPrompt(this.NamePrompt));
-        this.addDialog(new TextPrompt(this.EmailPrompt));
-        this.addDialog(new TextPrompt(this.LocationPrompt));
+        ];
 
+        this.addDialog(new WaterfallDialog<OnboardingState>(this.initialDialogId, onboarding));
+        this.addDialog(new TextPrompt(DialogIds.NamePrompt));
+        this.addDialog(new TextPrompt(DialogIds.EmailPrompt));
+        this.addDialog(new TextPrompt(DialogIds.LocationPrompt));
     }
 
     public async askForName(sc: WaterfallStepContext<OnboardingState>): Promise<DialogTurnResult> {
@@ -41,8 +41,8 @@ export class OnboardingDialog extends EnterpriseDialog {
         if (this._state.name) {
             return await sc.next(this._state.name);
         } else {
-            return await sc.prompt(this.NamePrompt, {
-                prompt: await this._responder.renderTemplate(sc.context, OnboardingResponses.NamePrompt, "en"),
+            return await sc.prompt(DialogIds.NamePrompt, {
+                prompt: await this._responder.renderTemplate(sc.context, OnboardingResponses.ResponseIds.NamePrompt, sc.context.activity.locale as string),
             });
         }
     }
@@ -51,37 +51,29 @@ export class OnboardingDialog extends EnterpriseDialog {
         this._state = await this.getStateFromAccessor(sc.context);
         this._state.name = sc.result;
 
-        await this._responder.replyWith(sc.context, OnboardingResponses.HaveName, { name: this._state.name });
+        await this._responder.replyWith(sc.context, OnboardingResponses.ResponseIds.HaveNameMessage, { name: this._state.name });
 
-        if (this._state.email) {
-            return await sc.next(this._state.email);
-        } else {
-            return await sc.prompt(this.EmailPrompt, {
-                prompt: await this._responder.renderTemplate(sc.context, OnboardingResponses.EmailPrompt, "en"),
-            });
-        }
+        return await sc.prompt(DialogIds.EmailPrompt, {
+            prompt: await this._responder.renderTemplate(sc.context, OnboardingResponses.ResponseIds.EmailPrompt, sc.context.activity.locale as string),
+        });
     }
 
     public async askForLocation(sc: WaterfallStepContext<OnboardingState>): Promise<DialogTurnResult> {
         this._state = await this.getStateFromAccessor(sc.context);
         this._state.email = sc.result;
 
-        await this._responder.replyWith(sc.context, OnboardingResponses.HaveEmail, { email: this._state.email });
+        await this._responder.replyWith(sc.context, OnboardingResponses.ResponseIds.HaveEmailMessage, { email: this._state.email });
 
-        if (this._state.location) {
-            return await sc.next(this._state.location);
-        } else {
-            return await sc.prompt(this.LocationPrompt, {
-                prompt: await this._responder.renderTemplate(sc.context, OnboardingResponses.LocationPrompt, "en"),
-            });
-        }
+        return await sc.prompt(DialogIds.LocationPrompt, {
+            prompt: await this._responder.renderTemplate(sc.context, OnboardingResponses.ResponseIds.LocationPrompt, sc.context.activity.locale as string),
+        });     
     }
 
     public async finishOnboardingDialog(sc: WaterfallStepContext<OnboardingState>): Promise<DialogTurnResult> {
         this._state = await this.getStateFromAccessor(sc.context);
-        this._state.location = sc.result;
+        this._state.location = sc.result as string;
 
-        await this._responder.replyWith(sc.context, OnboardingResponses.HaveLocation, { name: this._state.name, location: this._state.location });
+        await this._responder.replyWith(sc.context, OnboardingResponses.ResponseIds.HaveLocationMessage, { name: this._state.name, location: this._state.location });
 
         return await sc.endDialog();
     }
@@ -99,4 +91,10 @@ export class OnboardingDialog extends EnterpriseDialog {
         }
         return state;
     }
+}
+
+class DialogIds {
+    public static NamePrompt: string = "namePrompt";
+    public static EmailPrompt: string = "emailPrompt";
+    public static LocationPrompt: string =  "locationPrompt";
 }
--- templates/Enterprise-Template/src/typescript/src/dialogs/onboarding/onboardingResponses.ts
+++ templates/Enterprise-Template/src/typescript/src/dialogs/onboarding/onboardingResponses.ts
@@ -2,33 +2,36 @@
 // Licensed under the MIT License
 
 import { TurnContext } from "botbuilder";
-import { DictionaryRenderer, LanguageTemplateDictionary, TemplateFunction } from "../templateManager/dictionaryRenderer";
+import { LanguageTemplateDictionary, TemplateFunction } from "../templateManager/dictionaryRenderer";
 import { TemplateManager } from "../templateManager/templateManager";
 import * as i18n from "i18n";
 
 export class OnboardingResponses extends TemplateManager {
-    // Constants
-    public static readonly NamePrompt: string = "namePrompt";
-    public static readonly HaveName: string = "haveName";
-    public static readonly EmailPrompt: string = "emailPrompt";
-    public static readonly HaveEmail: string = "haveEmail";
-    public static readonly LocationPrompt: string = "locationPrompt";
-    public static readonly HaveLocation: string = "haveLocation";
+
+   // Fields
+   public static ResponseIds = {
+        EmailPrompt:  "emailPrompt",
+        HaveEmailMessage: "haveEmail",
+        HaveNameMessage: "haveName",
+        HaveLocationMessage: "haveLocation",
+        LocationPrompt: "locationPrompt",
+        NamePrompt: "namePrompt",
+    }
 
     private static readonly _responseTemplates: LanguageTemplateDictionary = new Map([
         ["default", new Map([
-            [OnboardingResponses.NamePrompt, OnboardingResponses.fromResources("onBoarding.namePrompt")],
-            [OnboardingResponses.HaveName, async (context: TurnContext, data: any) => {
+            [OnboardingResponses.ResponseIds.NamePrompt, OnboardingResponses.fromResources("onBoarding.namePrompt")],
+            [OnboardingResponses.ResponseIds.HaveNameMessage, async (context: TurnContext, data: any) => {
                 const value = i18n.__("onBoarding.haveName");
                 return value.replace("{0}", data.name);
             }],
-            [OnboardingResponses.EmailPrompt, OnboardingResponses.fromResources("onBoarding.emailPrompt")],
-            [OnboardingResponses.HaveEmail, async (context: TurnContext, data: any) => {
+            [OnboardingResponses.ResponseIds.EmailPrompt, OnboardingResponses.fromResources("onBoarding.emailPrompt")],
+            [OnboardingResponses.ResponseIds.HaveEmailMessage, async (context: TurnContext, data: any) => {
                 const value = i18n.__("onBoarding.haveEmail");
                 return value.replace("{0}", data.email);
             }],
-            [OnboardingResponses.LocationPrompt, OnboardingResponses.fromResources("onBoarding.locationPrompt")],
-            [OnboardingResponses.HaveLocation, async (context: TurnContext, data: any) => {
+            [OnboardingResponses.ResponseIds.LocationPrompt, OnboardingResponses.fromResources("onBoarding.locationPrompt")],
+            [OnboardingResponses.ResponseIds.HaveLocationMessage, async (context: TurnContext, data: any) => {
                 const value = i18n.__("onBoarding.haveLocation");
                 return value.replace("{0}", data.name).replace("{1}", data.location);
             }]
@@ -37,10 +40,5 @@ export class OnboardingResponses extends TemplateManager {
 
     private static fromResources(name: string): TemplateFunction {
         return (context: TurnContext, data: any) => Promise.resolve(i18n.__(name));
-    }
-
-    constructor() {
-        super();
-        this.register(new DictionaryRenderer(OnboardingResponses._responseTemplates));
     }
 }
