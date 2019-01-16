38b63fca7d9688df1d59edf14eec731cc33381f7
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-01-16 14:24:26 -0800

    Direct Line Example/Speech DDK web socket bug fix (#565)
    * Updated the startup events to send startConversation used in the Virtual Assistant.
    Configured teh web socket client to use TLS 1.2 protocol
    
    * Added MIT license to sample code.
    Refactored and updated web socket connection to enable TLS 1.2.
    Added Virtual Assistant startConversation event to set locale and trigger introduction card.
    Removed user id in Direct Line sample.

--- solutions/Virtual-Assistant/src/csharp/testharnesses/assistant-SpeechDevicesSDK/virtualassistant/app/src/main/java/io/swagger/client/WebSocketServerConnection.java
+++ solutions/Virtual-Assistant/src/csharp/testharnesses/assistant-SpeechDevicesSDK/virtualassistant/app/src/main/java/io/swagger/client/WebSocketServerConnection.java
@@ -1,19 +1,41 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 package io.swagger.client;
-
 
 import android.os.Handler;
 import android.os.Message;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
-
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 
-
-public class ServerConnection {
+public class WebSocketServerConnection {
     public enum ConnectionStatus {
         DISCONNECTED,
         CONNECTED
@@ -53,20 +75,20 @@ public class ServerConnection {
 
         @Override
         public void onFailure(WebSocket webSocket, Throwable t, Response response) {
-            disconnect();
+            Disconnect();
         }
     }
 
-    public ServerConnection(String url) {
+    public WebSocketServerConnection(String url) {
         mClient = new OkHttpClient.Builder()
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS))
                 .readTimeout(3,  TimeUnit.SECONDS)
                 .retryOnConnectionFailure(true)
                 .build();
-
         mServerUrl = url;
     }
 
-    public void connect(ServerListener listener) {
+    public void Connect(ServerListener listener) {
         Request request = new Request.Builder()
                 .url(mServerUrl)
                 .build();
@@ -78,14 +100,10 @@ public class ServerConnection {
             return true;});
     }
 
-    public void disconnect() {
+    public void Disconnect() {
         mWebSocket.cancel();
         mListener = null;
         mMessageHandler.removeCallbacksAndMessages(null);
         mStatusHandler.removeCallbacksAndMessages(null);
-    }
-
-    public void sendMessage(String message) {
-        mWebSocket.send(message);
     }
 }
