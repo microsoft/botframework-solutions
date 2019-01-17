d63c0656f6e0a19aca3fa0797452097952df211c
Martin Battaglino <martinbatta32@gmail.com>
2019-01-17 06:26:11 -0300

    [Enterprise Bot Template][TS] Update Dialogs with Resources (#555)
    * Rename utils to extensions folder
    
    * Replace signin for authentication folder
    
    * Update Cancel Dialog and Responses
    
    * Update Escalate Dialog and Responses
    
    * Update Main Dialog and Responses
    
    * Update OnBoarding Dialog and Responses
    
    * Rename interruptableStatus to interruptionStatus file
    
    * Update Shared Dialog
    
    * Instantiate static Responses for Dialogs
    
    * Add new keys to locales
    
    * Fix rebase issues
    
    * Add new keys to locales
    
    * Sync resources with i18n
    
    * Update Main Dialog Resources

--- templates/Enterprise-Template/src/typescript/src/dialogs/onboarding/onboardingResponses.ts
+++ templates/Enterprise-Template/src/typescript/src/dialogs/onboarding/onboardingResponses.ts
@@ -7,28 +7,31 @@ import { TemplateManager } from "../templateManager/templateManager";
 import * as i18n from "i18n";
 
 export class OnboardingResponses extends TemplateManager {
-    // Constants
-    public static readonly NamePrompt: string = "namePrompt";
-    public static readonly HaveName: string = "haveName";
-    public static readonly EmailPrompt: string = "emailPrompt";
-    public static readonly HaveEmail: string = "haveEmail";
-    public static readonly LocationPrompt: string = "locationPrompt";
-    public static readonly HaveLocation: string = "haveLocation";
+
+   // Fields
+   public static ResponseIds = {
+        EmailPrompt:  "emailPrompt",
+        HaveEmailMessage: "haveEmail",
+        HaveNameMessage: "haveName",
+        HaveLocationMessage: "haveLocation",
+        LocationPrompt: "locationPrompt",
+        NamePrompt: "namePrompt",
+    }
 
     private static readonly _responseTemplates: LanguageTemplateDictionary = new Map([
         ["default", new Map([
-            [OnboardingResponses.NamePrompt, OnboardingResponses.fromResources("onBoarding.namePrompt")],
-            [OnboardingResponses.HaveName, async (context: TurnContext, data: any) => {
+            [OnboardingResponses.ResponseIds.NamePrompt, OnboardingResponses.fromResources("onBoarding.namePrompt")],
+            [OnboardingResponses.ResponseIds.HaveNameMessage, async (context: TurnContext, data: any) => {
                 const value = i18n.__("onBoarding.haveName");
                 return value.replace("{0}", data.name);
             }],
-            [OnboardingResponses.EmailPrompt, OnboardingResponses.fromResources("onBoarding.emailPrompt")],
-            [OnboardingResponses.HaveEmail, async (context: TurnContext, data: any) => {
+            [OnboardingResponses.ResponseIds.EmailPrompt, OnboardingResponses.fromResources("onBoarding.emailPrompt")],
+            [OnboardingResponses.ResponseIds.HaveEmailMessage, async (context: TurnContext, data: any) => {
                 const value = i18n.__("onBoarding.haveEmail");
                 return value.replace("{0}", data.email);
             }],
-            [OnboardingResponses.LocationPrompt, OnboardingResponses.fromResources("onBoarding.locationPrompt")],
-            [OnboardingResponses.HaveLocation, async (context: TurnContext, data: any) => {
+            [OnboardingResponses.ResponseIds.LocationPrompt, OnboardingResponses.fromResources("onBoarding.locationPrompt")],
+            [OnboardingResponses.ResponseIds.HaveLocationMessage, async (context: TurnContext, data: any) => {
                 const value = i18n.__("onBoarding.haveLocation");
                 return value.replace("{0}", data.name).replace("{1}", data.location);
             }]
