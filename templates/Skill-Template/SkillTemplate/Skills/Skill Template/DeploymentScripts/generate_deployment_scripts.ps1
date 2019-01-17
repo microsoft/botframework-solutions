e5b11b522426e5e8284f2ee4056e38bccbd0d6df
Lauren Mills <lamil@microsoft.com>
2019-01-17 13:54:36 -0800

    updated skill template local deployment scripts

--- templates/Skill-Template/SkillTemplate/Skills/Skill Template/DeploymentScripts/generate_deployment_scripts.ps1
+++ templates/Skill-Template/SkillTemplate/Skills/Skill Template/DeploymentScripts/generate_deployment_scripts.ps1
@@ -2,28 +2,39 @@
     [string] [Parameter(Mandatory=$true)]$locale
 )
 
-$locale = $locale.ToLower()
-$langCode = ($locale -split "-")[0]
-$basePath = "$($PSScriptRoot)\.."
-$outputPath = "$($PSScriptRoot)\$($langCode)"
-
-# lu file paths
-$skillLUPath = "$($basePath)\CognitiveModels\LUIS\$($langCode)\skill.lu"
-$generalLUPath = "$($basePath)\CognitiveModels\LUIS\$($langCode)\general.lu"
-
-$luArr = @($skillLUPath, $generalLUPath)
-
-Write-Host "Updating $($locale) deployment scripts ..."
-foreach ($lu in $luArr) 
-{
+function CheckForDuplicates($lu) {
 	$duplicates = Get-Content $lu | Group-Object | Where-Object { $_.Count -gt 1 } | Select -ExpandProperty Name
 
 	if ($duplicates.Count -gt 1) 
 	{
-		Write-Error "$($duplicates.Count - 1) duplicate utterances found in $($lu). This could cause issues in your model accuracy."
+		Write-Warning "$($duplicates.Count - 1) duplicate utterances found in $($lu). This could cause issues in your model accuracy."
 	}
 }
 
-Write-Host "Generating $($locale) LUIS models from .lu files ..."
-ludown parse toluis -c $($locale) -o $outputPath --in $skillLUPath --out skill.luis -n Skill
-ludown parse toluis -c $($locale) -o $outputPath --in $generalLUPath --out general.luis -n General
+$locale = $locale.ToLower()
+$langCode = ($locale -split "-")[0]
+$outputPath = "$($PSScriptRoot)\..\DeploymentScripts\$($langCode)"
+$recipePath = "$($PSScriptRoot)\..\DeploymentScripts\$($langCode)\bot.recipe"
+$recipe = Get-Content -Raw -Path $recipePath | ConvertFrom-Json
+
+foreach ($service in $recipe.resources)
+{
+	Write-Host "Generating $($locale) $($service.name) script ..."
+	$path = "$($PSScriptRoot)\$($service.luPath)"
+
+	if ($service.type -eq "luis")
+	{
+		CheckForDuplicates $path
+		ludown parse toluis -c $($locale) -o $outputPath --in $path --out "$($service.id).luis" -n $service.Name
+	}
+	elseif ($service.type -eq "qna")
+	{
+		CheckForDuplicates $path
+		ludown parse toqna -o $outputPath --in $path --out "$($service.id).qna"
+	}
+	elseif ($service.type -eq "dispatch")
+	{
+		CheckForDuplicates $path
+		ludown parse toluis -c $($locale) -o $outputPath --in $path --out "$($service.id).luis" -n $service.Name
+	}
+}
