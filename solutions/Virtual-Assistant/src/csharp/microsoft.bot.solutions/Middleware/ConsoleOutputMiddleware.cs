6a33103341e341a59245d42c6113c6d92aaffa7d
lauren-mills <lamil@microsoft.com>
2019-01-18 11:58:00 -0800

    Skill Template Updates (#578)
    * updated skill template
    
    * updated skill template and added TestSkill to VA
    
    * test updates
    
    * temp
    
    * removing extra projects
    
    * update dispatch.lu files with new folder structure
    
    * updated bot.recipe files
    
    * moved testframework project to microsoft.bot.solutions
    
    * updated package references with new folder structure
    
    * updated skill folder structure
    
    * added skill template
    
    * Added skill template and vsix
    
    * update template .lu file and using
    
    * updated skill template local deployment scripts
    
    * va updates
    
    * update to netcoreapp2.2
    
    * update names in skill template to $safeprojectname$
    
    * updated skill docs
    
    * updated skill template docs
    
    * update to folder structure
    
    * updates for folder structure
    
    * updated readme
    
    * changed projects back to 2.1
    
    * update sdk to 2.2 test
    
    * netcore2.1
    
    * small update
    
    * updated prebuild script
    
    * removed prebuild step

---
+++ solutions/Virtual-Assistant/src/csharp/microsoft.bot.solutions/Middleware/ConsoleOutputMiddleware.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Solutions.Testing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Schema;
+
+    public class ConsoleOutputMiddleware : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext context, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            this.LogActivity(string.Empty, context.Activity);
+            context.OnSendActivities(this.OnSendActivities);
+
+            await next(cancellationToken).ConfigureAwait(false);
+        }
+
+        private static string GetTextOrSpeak(IMessageActivity messageActivity)
+        {
+            return string.IsNullOrWhiteSpace(messageActivity.Text) ? messageActivity.Speak : messageActivity.Text;
+        }
+
+        private async Task<ResourceResponse[]> OnSendActivities(ITurnContext context, List<Activity> activities, Func<Task<ResourceResponse[]>> next)
+        {
+            foreach (var response in activities)
+            {
+                this.LogActivity(string.Empty, response);
+            }
+
+            return await next();
+        }
+
+        private void LogActivity(string prefix, Activity contextActivity)
+        {
+            Console.WriteLine(string.Empty);
+            if (contextActivity.Type == ActivityTypes.Message)
+            {
+                var messageActivity = contextActivity.AsMessageActivity();
+                Console.WriteLine($"{prefix} [{DateTime.Now:ss.fff}] {GetTextOrSpeak(messageActivity)}");
+            }
+            else if (contextActivity.Type == ActivityTypes.Event)
+            {
+                var eventActivity = contextActivity.AsEventActivity();
+                Console.WriteLine($"{prefix} Event: [{DateTime.Now:ss.fff}{DateTime.Now.Millisecond}] {eventActivity.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{prefix} {contextActivity.Type}: [{DateTime.Now:ss.fff}{DateTime.Now.Millisecond}]");
+            }
+        }
+    }
+}
---
+++ solutions/Virtual-Assistant/src/csharp/skills/automotiveskill/automotiveskilltest/Flow/AutomotiveSkillTestBase.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Autofac;
+using AutomotiveSkill;
+using AutomotiveSkillTest.Flow.Fakes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Configuration;
+using Microsoft.Bot.Solutions.Dialogs;
+using Microsoft.Bot.Solutions.Dialogs.BotResponseFormatters;
+using Microsoft.Bot.Solutions.Skills;
+using Microsoft.Bot.Solutions.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace AutomotiveSkillTest.Flow
+{
+    public class AutomotiveSkillTestBase : BotTestBase
+    {
+        public IStatePropertyAccessor<AutomotiveSkillState> AutomotiveSkillStateAccessor { get; set; }
+
+        public ConversationState ConversationState { get; set; }
+
+        public UserState UserState { get; set; }
+
+        public SkillConfigurationBase Services { get; set; }
+
+        public BotConfiguration Options { get; set; }
+
+        public HttpContext MockHttpContext { get; set; }
+
+        public IBotTelemetryClient TelemetryClient { get; set; }
+
+        public HttpContextAccessor MockHttpContextAcessor { get; set; }
+
+        public override void Initialize()
+        {
+           var builder = new ContainerBuilder();
+
+            this.ConversationState = new ConversationState(new MemoryStorage());
+            this.UserState = new UserState(new MemoryStorage());
+            this.AutomotiveSkillStateAccessor = this.ConversationState.CreateProperty<AutomotiveSkillState>(nameof(AutomotiveSkillState));
+            this.Services = new MockSkillConfiguration();
+
+            builder.RegisterInstance(new BotStateSet(this.UserState, this.ConversationState));
+
+            this.Container = builder.Build();
+            
+            this.BotResponseBuilder = new BotResponseBuilder();
+            this.BotResponseBuilder.AddFormatter(new TextBotResponseFormatter());
+
+            this.TelemetryClient = new NullBotTelemetryClient();
+
+            // Mock HttpContext for image path resolution
+            MockHttpContext = new DefaultHttpContext();            
+            MockHttpContext.Request.Scheme = "http";
+            MockHttpContext.Request.Host = new HostString("localhost",3980);
+
+            MockHttpContextAcessor = new HttpContextAccessor();
+            MockHttpContextAcessor.HttpContext = MockHttpContext;
+        }
+
+        public TestFlow GetTestFlow()
+        {
+            var adapter = new TestAdapter()
+                .Use(new AutoSaveStateMiddleware(this.ConversationState));
+
+            var testFlow = new TestFlow(adapter, async (context, token) =>
+            {
+                var bot = this.BuildBot() as AutomotiveSkill.AutomotiveSkill;
+                var state = await this.AutomotiveSkillStateAccessor.GetAsync(context, () => new AutomotiveSkillState());
+                await bot.OnTurnAsync(context, CancellationToken.None);
+            });
+
+            return testFlow;
+        }
+
+        public override IBot BuildBot()
+        {
+            return new AutomotiveSkill.AutomotiveSkill(this.Services, this.ConversationState, this.UserState, this.TelemetryClient,null, MockHttpContextAcessor, true);
+        }
+    }
+}
