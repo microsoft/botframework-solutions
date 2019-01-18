2b9f9ac91e9c70cd17279490a25a3d2dd69ef167
Martin Battaglino <martinbatta32@gmail.com>
2019-01-18 14:06:45 -0300

    Fix enterpriseBot tslint errors

--- templates/Enterprise-Template/src/typescript/src/enterpriseBot.ts
+++ templates/Enterprise-Template/src/typescript/src/enterpriseBot.ts
@@ -1,8 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ConversationState, TurnContext, UserState } from 'botbuilder';
-import { DialogSet, DialogState, DialogTurnStatus } from 'botbuilder-dialogs';
+import {
+    ConversationState,
+    TurnContext,
+    UserState } from 'botbuilder';
+import {
+    DialogContext,
+    DialogSet,
+    DialogState,
+    DialogTurnResult,
+    DialogTurnStatus } from 'botbuilder-dialogs';
 import { BotServices } from './botServices';
 import { MainDialog } from './dialogs/main/mainDialog';
 
@@ -10,38 +18,33 @@ import { MainDialog } from './dialogs/main/mainDialog';
  * Main entry point and orchestration for bot.
  */
 export class EnterpriseBot {
-    private readonly _botServices: BotServices;
-    private readonly _conversationState: ConversationState;
-    private readonly _userState: UserState;
-    private readonly _dialogs: DialogSet;
+    private readonly BOT_SERVICES: BotServices;
+    private readonly CONVERSATION_STATE: ConversationState;
+    private readonly USER_STATE: UserState;
+    private readonly DIALOGS: DialogSet;
 
     /**
      * Constructs the three pieces necessary for this bot to operate.
-     * @constructor
-     * @param {BotServices} botServices Services included in the bot (LUIS, QnA, Dispatcher, etc...).
-     * @param {ConversationState} conversationState Bot conversation state.
-     * @param {UserState} userState Bot user state.
      */
     constructor(botServices: BotServices, conversationState: ConversationState, userState: UserState) {
         if (!botServices) { throw new Error(('Missing parameter.  botServices is required')); }
         if (!conversationState) { throw new Error(('Missing parameter.  conversationState is required')); }
         if (!userState) { throw new Error(('Missing parameter.  userState is required')); }
 
-        this._botServices = botServices;
-        this._conversationState = conversationState;
-        this._userState = userState;
+        this.BOT_SERVICES = botServices;
+        this.CONVERSATION_STATE = conversationState;
+        this.USER_STATE = userState;
 
-        this._dialogs = new DialogSet(this._conversationState.createProperty<DialogState>('EnterpriseBot'));
-        this._dialogs.add(new MainDialog(this._botServices, this._conversationState, this._userState));
+        this.DIALOGS = new DialogSet(this.CONVERSATION_STATE.createProperty<DialogState>('EnterpriseBot'));
+        this.DIALOGS.add(new MainDialog(this.BOT_SERVICES, this.CONVERSATION_STATE, this.USER_STATE));
     }
 
     /**
      * Run every turn of the conversation. Handles orchestration of messages.
-     * @param {TurnContext} turnContext Turn context from the adapter.
      */
     public async onTurn(turnContext: TurnContext): Promise<void> {
-        const dc = await this._dialogs.createContext(turnContext);
-        const result = await dc.continueDialog();
+        const dc: DialogContext = await this.DIALOGS.createContext(turnContext);
+        const result: DialogTurnResult<any> = await dc.continueDialog();
 
         if (result.status === DialogTurnStatus.empty) {
             await dc.beginDialog('MainDialog');
