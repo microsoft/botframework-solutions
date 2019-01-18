5df0e3a8527fa394d334bf8c90ea3477e3496cd7
Ryan Lengel <rylengel@microsoft.com>
2019-01-18 14:10:04 -0800

    Added data models for Foursquare data and beginning refactoring of POI model that will use either Azure Maps or Foursquare as data source

---
+++ solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Models/AzureMaps/SearchResult.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Newtonsoft.Json;
+
+namespace PointOfInterestSkill.Models
+{
+    public class SearchResult
+    {
+        [JsonProperty(PropertyName = "type")]
+        public string ResultType { get; set; }
+
+        [JsonProperty(PropertyName = "address")]
+        public SearchAddress Address { get; set; }
+
+        [JsonProperty(PropertyName = "position")]
+        public LatLng Position { get; set; }
+
+        [JsonProperty(PropertyName = "poi")]
+        public PoiInfo Poi { get; set; }
+
+        [JsonProperty(PropertyName = "viewport")]
+        public Viewport Viewport { get; set; }
+
+        public Location ToLocation()
+        {
+            var c = new GeocodePoint()
+            {
+                Coordinates = new System.Collections.Generic.List<double>
+                {
+                    Position.Latitude, Position.Longitude,
+                },
+            };
+
+            return new Location()
+            {
+                Address = (Address != null) ? Address.ToBingAddress() : null,
+                Point = c,
+                BoundaryBox = (Viewport != null) ? new System.Collections.Generic.List<double>()
+                {
+                    Viewport.BtmRightPoint.Latitude, Viewport.TopLeftPoint.Longitude, Viewport.TopLeftPoint.Latitude, Viewport.BtmRightPoint.Longitude,
+                }
+                : null,
+                GeocodePoints = new System.Collections.Generic.List<GeocodePoint>()
+                {
+                    c,
+                },
+                Name = (Poi != null && !string.IsNullOrEmpty(Poi.Name)) ? Poi.Name : Address.FreeformAddress,
+                EntityType = ResultType,
+            };
+        }
+    }
+}
---
+++ solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Models/Foursquare/Venuechain.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointOfInterestSkill.Models.Foursquare
+{
+    public class Venuechain
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+}
