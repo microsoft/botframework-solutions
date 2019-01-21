52fd89d7306d0847e3fe92fc763c0099e780a2eb
Ryan Lengel Isgrig <rylengel@microsoft.com>
2019-01-21 14:26:52 -0800

    Cleaned up Foursquare models

--- solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Models/Foursquare/VenueResponse.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Models/Foursquare/VenueResponse.cs
@@ -1,6 +1,8 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PointOfInterestSkill.Models.Foursquare
 {
