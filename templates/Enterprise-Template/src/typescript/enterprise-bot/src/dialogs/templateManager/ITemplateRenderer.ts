7eeacbf7f2fef7107e3400284d3f3de638d6bb0b
Martin Battaglino <martinbatta32@gmail.com>
2019-01-22 13:10:40 -0300

    Fix templateManager tslint errors

--- templates/Enterprise-Template/src/typescript/enterprise-bot/src/dialogs/templateManager/ITemplateRenderer.ts
+++ templates/Enterprise-Template/src/typescript/enterprise-bot/src/dialogs/templateManager/ITemplateRenderer.ts
@@ -6,7 +6,9 @@ import { TurnContext } from 'botbuilder-core';
 /// <summary>
 /// Defines interface for data binding to template and rendering a string
 /// </summary>
+
 export interface ITemplateRenderer {
+
     /// <summary>
     /// render a template to an activity or string
     /// </summary>
@@ -15,5 +17,6 @@ export interface ITemplateRenderer {
     /// <param name="templateId">tenmplate to render</param>
     /// <param name="data">data object to use to render</param>
     /// <returns></returns>
+
     renderTemplate(turnContext: TurnContext, language: string, templateId: string, data: any): Promise<any>;
 }
