2a316b719a0d7683fd23560cc3a032df34ab0c2d
Ryan Lengel Isgrig <rylengel@microsoft.com>
2019-02-03 20:36:20 -0800

    Addressed Stylecop comment warnings.

--- solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Models/Foursquare/VenueResponse.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Models/Foursquare/VenueResponse.cs
@@ -1,8 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using Newtonsoft.Json;
 
 namespace PointOfInterestSkill.Models.Foursquare
 {
