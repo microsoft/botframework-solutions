b6953ac5ce97d5fe50e44eb6ee5891c6a67793d4
Martin Battaglino <martinbatta32@gmail.com>
2019-02-06 12:34:35 -0300

    Update structure and add new tests for botbuilder-enterprise dialog subgenerator

--- templates/Enterprise-Template/src/typescript/generator-botbuilder-enterprise/test/generator-botbuilder-enterprise.dialog.test.suite.js
+++ templates/Enterprise-Template/src/typescript/generator-botbuilder-enterprise/test/generator-botbuilder-enterprise.dialog.test.suite.js
@@ -6,92 +6,158 @@ const rimraf = require("rimraf");
 const _camelCase = require("lodash/camelCase");
 const _upperFirst = require("lodash/upperFirst");
 
-describe("The generator-botbuilder-enterprise dialog tests ", () => {
-  var dialogName = "customDialog";
-  var responsesName = "customResponses";
-  const dialogNameCamelCase = _camelCase(dialogName);
-  const dialogNamePascalCase = _upperFirst(_camelCase(dialogName));
-  const responsesNameCamelCase = _camelCase(responsesName);
-  const responsesNamePascalCase = _upperFirst(_camelCase(responsesName));
-  const dialogGenerationPath = path.join("tmp", dialogName);
+describe("The generator-botbuilder-enterprise dialog tests", function() {
+  var dialogName;
+  var responsesName;
+  var dialogNameCamelCase;
+  var dialogNamePascalCase;
+  var responsesNameCamelCase;
+  var responsesNamePascalCase;
+  var dialogGenerationPath;
+  var confirmationPath;
+  var finalConfirmation;
 
-  before(() => {
-    return helpers
-      .run(path.join(__dirname, "../generators/dialog"))
-      .inDir(path.join(__dirname, "tmp"))
-      .withPrompts({
-        dialogName: dialogName,
-        confirmationPath: true,
-        dialogPath: process.cwd(),
-        finalConfirmation: true
+  describe("should create", function() {
+    dialogName = "customDialog";
+    responsesName = "customResponses";
+    dialogNameCamelCase = _camelCase(dialogName);
+    dialogNamePascalCase = _upperFirst(_camelCase(dialogName));
+    responsesNameCamelCase = _camelCase(responsesName);
+    responsesNamePascalCase = _upperFirst(_camelCase(responsesName));
+    dialogGenerationPath = path.join("tmp", dialogName);
+    confirmationPath = true;
+    finalConfirmation = true;
+
+    before(function() {
+      return helpers
+        .run(path.join(__dirname, "../generators/dialog"))
+        .inDir(path.join(__dirname, "tmp"))
+        .withPrompts({
+          dialogName: dialogName,
+          confirmationPath: confirmationPath,
+          dialogGenerationPath: dialogGenerationPath,
+          finalConfirmation: finalConfirmation
+        });
+    });
+
+    after(function() {
+      rimraf.sync(path.join(__dirname, "tmp/*"));
+    });
+
+    describe("the files", function() {
+      const files = [
+        dialogNameCamelCase + ".ts",
+        responsesNameCamelCase + ".ts"
+      ];
+
+      files.forEach(fileName =>
+        it(fileName + " file", function(done) {
+          assert.file(path.join(__dirname, dialogGenerationPath, fileName));
+          done();
+        })
+      );
+    });
+
+    describe("and have in the dialog file with the given name", function() {
+      it("an import component containing the given name", function(done) {
+        assert.fileContent(
+          path.join(
+            __dirname,
+            dialogGenerationPath,
+            dialogNameCamelCase + ".ts"
+          ),
+          `import { ${responsesNamePascalCase} } from './${responsesNameCamelCase}'`
+        );
+        done();
       });
+
+      it("an export component with the given name", function(done) {
+        assert.fileContent(
+          path.join(
+            __dirname,
+            dialogGenerationPath,
+            dialogNameCamelCase + ".ts"
+          ),
+          `export class ${dialogNamePascalCase}`
+        );
+        done();
+      });
+
+      it("an initialized attribute with the given name", function(done) {
+        assert.fileContent(
+          path.join(
+            __dirname,
+            dialogGenerationPath,
+            dialogNameCamelCase + ".ts"
+          ),
+          `RESPONDER: ${responsesNamePascalCase} = new ${responsesNamePascalCase}()`
+        );
+        done();
+      });
+
+      it("a super method with the given name as parameter", function(done) {
+        assert.fileContent(
+          path.join(
+            __dirname,
+            dialogGenerationPath,
+            dialogNameCamelCase + ".ts"
+          ),
+          `super(${dialogNamePascalCase}.name)`
+        );
+        done();
+      });
+    });
+
+    describe("and have in the responses file with the given name", function() {
+      it("an export component with the given name", function(done) {
+        assert.fileContent(
+          path.join(
+            __dirname,
+            dialogGenerationPath,
+            responsesNameCamelCase + ".ts"
+          ),
+          `export class ${responsesNamePascalCase}`
+        );
+        done();
+      });
+
+      it("a parameter with the given name", function(done) {
+        assert.fileContent(
+          path.join(
+            __dirname,
+            dialogGenerationPath,
+            responsesNameCamelCase + ".ts"
+          ),
+          `new DictionaryRenderer(${responsesNamePascalCase}.RESPONSE_TEMPLATES)`
+        );
+        done();
+      });
+    });
   });
 
-  after(() => {
-    rimraf.sync(path.join(__dirname, "tmp/*"));
-  });
-
-  describe("should create", () => {
-    const files = [dialogNameCamelCase + ".ts", responsesNameCamelCase + ".ts"];
-
-    files.forEach(fileName =>
-      it(fileName + " file", () => {
-        assert.file(path.join(__dirname, dialogGenerationPath, fileName));
-      })
-    );
-  });
-
-  describe("should have in the dialog file with the given name", () => {
-    it("an import component containing the given name", () => {
-      assert.fileContent(
-        path.join(__dirname, dialogGenerationPath, dialogNameCamelCase + ".ts"),
-        `import { ${responsesNamePascalCase} } from './${responsesNameCamelCase}'`
-      );
+  describe("should not create", function() {
+    before(function() {
+      finalConfirmation = false;
+      return helpers
+        .run(path.join(__dirname, "../generators/dialog"))
+        .inDir(path.join(__dirname, "tmp"))
+        .withPrompts({
+          dialogName: dialogName,
+          confirmationPath: confirmationPath,
+          dialogGenerationPath: dialogGenerationPath,
+          finalConfirmation: finalConfirmation
+        });
     });
 
-    it("an export component with the given name", () => {
-      assert.fileContent(
-        path.join(__dirname, dialogGenerationPath, dialogNameCamelCase + ".ts"),
-        `export class ${dialogNamePascalCase}`
-      );
+    after(function() {
+      rimraf.sync(path.join(__dirname, "tmp/*"));
     });
 
-    it("an initialized attribute with the given name", () => {
-      assert.fileContent(
-        path.join(__dirname, dialogGenerationPath, dialogNameCamelCase + ".ts"),
-        `RESPONDER: ${responsesNamePascalCase} = new ${responsesNamePascalCase}()`
-      );
-    });
-
-    it("a super method with the given name as parameter", () => {
-      assert.fileContent(
-        path.join(__dirname, dialogGenerationPath, dialogNameCamelCase + ".ts"),
-        `super(${dialogNamePascalCase}.name)`
-      );
-    });
-  });
-
-  describe("should have in the responses file with the given name", () => {
-    it("an export component with the given name", () => {
-      assert.fileContent(
-        path.join(
-          __dirname,
-          dialogGenerationPath,
-          responsesNameCamelCase + ".ts"
-        ),
-        `export class ${responsesNamePascalCase}`
-      );
-    });
-
-    it("a parameter with the given name", () => {
-      assert.fileContent(
-        path.join(
-          __dirname,
-          dialogGenerationPath,
-          responsesNameCamelCase + ".ts"
-        ),
-        `new DictionaryRenderer(${responsesNamePascalCase}.RESPONSE_TEMPLATES)`
-      );
+    describe("the base", function() {
+      it(dialogName + " folder when the final confirmation is deny", function(done) {
+        assert.noFile(path.join(__dirname, dialogGenerationPath));
+        done();
+      });
     });
   });
 });
