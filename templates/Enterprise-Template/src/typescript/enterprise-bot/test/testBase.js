bedf9a13ac3da25addeaa429c19ea2bfe7a49e1f
Enzo Cano <enzo.cano@southworks.com>
2019-02-12 14:55:32 -0300

    Sanitize test mocks

--- templates/Enterprise-Template/src/typescript/enterprise-bot/test/testBase.js
+++ templates/Enterprise-Template/src/typescript/enterprise-bot/test/testBase.js
@@ -6,6 +6,17 @@ const nockBack = require('nock').back;
 nockBack.setMode(TEST_MODE);
 nockBack.fixtures = __dirname + '/nockFixtures';
 
+const uuidRegex = /[a-f\d-]{8}-[a-f\d-]{4}-[a-f\d-]{4}-[a-f\d-]{4}-[a-f\d-]{12}/;
+const qnaRegex = /\/\/[^.]+\.azurewebsites/;
+
+const replaceUUID = function (value) {
+    return value.replace(uuidRegex, 'f7c2ee78-8679-4a3e-b384-0cd10c67e554');
+}
+
+const replaceScope = function (value) {
+    return value.replace(qnaRegex, '//sample-hostname-qnahost.azurewebsites');
+}
+
 const beforeNock = function (scope) {
     scope.filteringRequestBody = function (body, rBody) {
         if (body === JSON.stringify(rBody)) {
@@ -14,18 +25,31 @@ const beforeNock = function (scope) {
 
         return body;
     }
+
+    scope.filteringPath = replaceUUID;
+
+    scope.filteringScope = replaceScope;
 };
 
+const afterRecordNock = function (scopes) {
+    return scopes.map(function (scope) {
+        scope.path = replaceUUID(scope.path)
+        scope.scope = replaceScope(scope.scope);
+
+        return scope;
+    });
+}
+
 const resolveWithMocks = function (testName, done, testFlow) {
-    nockBack(snakeCase(testName) + '.json', { before: beforeNock }, function (nockDone) {
+    nockBack(snakeCase(testName) + '.json', { before: beforeNock, afterRecord: afterRecordNock }, function (nockDone) {
         testFlow
-        .then(function () {
-            nockDone();
-            done();
-        })
-        .catch(function (err) {
-            done(err);
-        });
+            .then(function () {
+                nockDone();
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
     });
 }
 
