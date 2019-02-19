451efbabc2c2fe55a9418bb6d5043b6f2de64bc7
Qiong Wu (qiowu) <qiowu@microsoft.com>
2019-02-19 11:46:19 +0800

    fix crash issue (#825)

--- solutions/Virtual-Assistant/src/csharp/skills/todoskill/todoskill/ToDoSkill.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/todoskill/todoskill/ToDoSkill.cs
@@ -85,7 +85,7 @@ namespace ToDoSkill
         /// <returns>A <see cref="TaskItem"/> representing the asynchronous operation.</returns>
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken)
         {
-            turnContext.TurnState.Add(TelemetryLoggerMiddleware.AppInsightsServiceKey, _telemetryClient);
+            turnContext.TurnState.TryAdd(TelemetryLoggerMiddleware.AppInsightsServiceKey, _telemetryClient);
 
             var dc = await _dialogs.CreateContextAsync(turnContext);
 
