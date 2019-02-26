2f0c07dddb9ae02e89c97a9f1cb47b1b046cdbf2
lauren-mills <lamil@microsoft.com>
2019-02-26 14:08:40 -0800

    Enterprise Bot Tests (#868)
    * added tests to sample
    
    * added test project to ebt and ebt sample
    
    * updated telemetry
    
    * updates to telemetry and packages

---
+++ templates/Enterprise-Template/src/csharp/EnterpriseBotSample/EnterpriseBotSample/Extensions/ActivityExtensions.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+using System.Linq;
+
+namespace EnterpriseBotSample.Extensions
+{
+    public static class ActivityExtensions
+    {
+        public static bool IsStartActivity(this Activity activity)
+        {
+            switch (activity.ChannelId)
+            {
+                case Channels.Skype:
+                    {
+                        if (activity.Type == ActivityTypes.ContactRelationUpdate && activity.Action == "add")
+                        {
+                            return true;
+                        }
+
+                        return false;
+                    }
+
+                case Channels.Directline:
+                case Channels.Emulator:
+                case Channels.Webchat:
+                case Channels.Msteams:
+                case "test":
+                    {
+                        if (activity.Type == ActivityTypes.ConversationUpdate)
+                        {
+                            // When bot is added to the conversation (triggers start only once per conversation)
+                            if (activity.MembersAdded.Any(m => m.Id == activity.Recipient.Id))
+                            {
+                                return true;
+                            }
+                        }
+
+                        return false;
+                    }
+
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+    }
+}
