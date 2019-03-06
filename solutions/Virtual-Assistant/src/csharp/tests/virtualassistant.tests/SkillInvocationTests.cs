3d2ce25b423c2681f939aa754906ed3221369e8b
Lauren Mills <lamil@microsoft.com>
2019-03-06 11:20:40 -0800

    updated va and skills

--- solutions/Virtual-Assistant/src/csharp/tests/virtualassistant.tests/SkillInvocationTests.cs
+++ solutions/Virtual-Assistant/src/csharp/tests/virtualassistant.tests/SkillInvocationTests.cs
@@ -32,57 +32,69 @@ namespace VirtualAssistant.Tests.SkillInvocationTests
         }
 
         /// <summary>
-        /// Test that we can invoke the Calendar Skill. If we are able to do this we will at this time get an Exception saying that the
-        /// Test Adapter doesn't support GetUserToken which signals the skill has been invoked and the GetAuth step has started
-        /// signalling the Skill has completed.
+        /// Test that we can invoke the Calendar Skill. If we are able to do this we will get an OAuth card.
         /// </summary>
         /// <returns>Task.</returns>
         [TestMethod]
-        [ExpectedExceptionAndMessage(typeof(InvalidOperationException), "OAuthPrompt.GetUserToken(): not supported by the current adapter")]
         public async Task CalendarSkillInvocation()
         {
             this.SkillDialogOptions = new SkillDialogOptions();
             this.SkillDialogOptions.SkillDefinition = this.skillDefinitions["calendarSkill"];
 
             await this.GetTestFlow()
-            .Send(CalendarUtterances.BookMeeting)
-            .StartTestAsync();
+                .Send(CalendarUtterances.BookMeeting)
+                .AssertReply((activity) => Assert.AreEqual(ActivityTypes.Trace, activity.Type))
+                .AssertReply((activity) =>
+                {
+                    var messageActivity = activity.AsMessageActivity();
+                    Assert.AreEqual(1, messageActivity.Attachments.Count);
+                    Assert.AreEqual("application/vnd.microsoft.card.oauth", messageActivity.Attachments[0].ContentType);
+                })
+                .StartTestAsync();
         }
 
         /// <summary>
-        /// Test that we can invoke the Email Skill. If we are able to do this we will at this time get an Exception saying that the
-        /// Test Adapter doesn't support GetUserToken which signals the skill has been invoked and the GetAuth step has started
-        /// signalling the Skill has completed.
+        /// Test that we can invoke the Email Skill. If we are able to do this we will get an OAuth card.
         /// </summary>
         /// <returns>Task.</returns>
         [TestMethod]
-        [ExpectedExceptionAndMessage(typeof(InvalidOperationException), "OAuthPrompt.GetUserToken(): not supported by the current adapter")]
         public async Task EmailSkillInvocation()
         {
             this.SkillDialogOptions = new SkillDialogOptions();
             this.SkillDialogOptions.SkillDefinition = this.skillDefinitions["emailSkill"];
 
             await this.GetTestFlow()
-            .Send(EmailUtterances.SendEmail)
-            .StartTestAsync();
+                .Send(EmailUtterances.SendEmail)
+                .AssertReply((activity) => Assert.AreEqual(ActivityTypes.Trace, activity.Type))
+                .AssertReply((activity) =>
+                {
+                    var messageActivity = activity.AsMessageActivity();
+                    Assert.AreEqual(1, messageActivity.Attachments.Count);
+                    Assert.AreEqual("application/vnd.microsoft.card.oauth", messageActivity.Attachments[0].ContentType);
+                })
+                .StartTestAsync();
         }
 
         /// <summary>
-        /// Test that we can invoke the ToDo Skill. If we are able to do this we will at this time get an Exception saying that the
-        /// Test Adapter doesn't support GetUserToken which signals the skill has been invoked and the GetAuth step has started
-        /// signalling the Skill has completed.
+        /// Test that we can invoke the ToDo Skill. If we are able to do this we will get an OAuth card.
         /// </summary>
         /// <returns>Task.</returns>
         [TestMethod]
-        [ExpectedExceptionAndMessage(typeof(InvalidOperationException), "OAuthPrompt.GetUserToken(): not supported by the current adapter")]
         public async Task ToDoSkillInvocation()
         {
             this.SkillDialogOptions = new SkillDialogOptions();
             this.SkillDialogOptions.SkillDefinition = this.skillDefinitions["toDoSkill"];
 
             await this.GetTestFlow()
-            .Send(ToDoUtterances.AddToDo)
-            .StartTestAsync();
+                .Send(ToDoUtterances.AddToDo)
+                .AssertReply((activity) => Assert.AreEqual(ActivityTypes.Trace, activity.Type))
+                .AssertReply((activity) =>
+                {
+                    var messageActivity = activity.AsMessageActivity();
+                    Assert.AreEqual(1, messageActivity.Attachments.Count);
+                    Assert.AreEqual("application/vnd.microsoft.card.oauth", messageActivity.Attachments[0].ContentType);
+                })
+                .StartTestAsync();
         }
 
         /// <summary>
