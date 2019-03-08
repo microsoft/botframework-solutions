70d74380a1af197bf8a4c828299ef964881aa439
lauren-mills <lamil@microsoft.com>
2019-03-08 11:04:10 -0800

    Added microsoft.bot.builder.solutions project to lib folder (#952)

---
+++ lib/microsoft.bot.builder.solutions/Authentication/ProviderTokenResponse.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Builder.Solutions.Authentication
+{
+    public class ProviderTokenResponse
+    {
+        public OAuthProvider AuthenticationProvider { get; set; }
+
+        public TokenResponse TokenResponse { get; set; }
+    }
+}
