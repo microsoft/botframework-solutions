c43b9fdc64d9be5737927af4773ecc131b318493
David Favretto <david.favretto@southworks.com>
2019-03-12 18:12:25 -0300

    Migrate responses

--- solutions/Virtual-Assistant/src/typescript/assistant/src/dialogs/onboarding/onboardingResponses.ts
+++ solutions/Virtual-Assistant/src/typescript/assistant/src/dialogs/onboarding/onboardingResponses.ts
@@ -1,32 +1,70 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License
 
-import * as i18n from 'i18n';
+import {
+    Activity,
+    Attachment,
+    CardFactory,
+    CardImage,
+    InputHints,
+    MessageFactory,
+    TurnContext } from 'botbuilder';
+import { __ } from 'i18n';
 import {
     DictionaryRenderer,
     LanguageTemplateDictionary,
-    TemplateFunction } from '../templateManager/dictionaryRenderer';
+    TemplateFunction,
+    TemplateIdMap} from '../templateManager/dictionaryRenderer';
 import { TemplateManager } from '../templateManager/templateManager';
 
 export class OnboardingResponses extends TemplateManager {
-
-    // Declare here the type of properties and the prompts
-    public static RESPONSE_IDS: {
+    public static responseIds: {
+        namePrompt: string;
+        locationPrompt: string;
+        haveLocation: string;
+        addLinkedAccountsMessage: string;
+    } = {
+        namePrompt: 'namePrompt',
+        locationPrompt: 'locationPrompt',
+        haveLocation: 'haveLocation',
+        addLinkedAccountsMessage: 'linkedAccountsInfo'
     };
 
-    // Declare the responses map prompts
-    private static readonly RESPONSE_TEMPLATES: LanguageTemplateDictionary = new Map([
-        ['default', new Map([
+    private static readonly responseTemplates: LanguageTemplateDictionary = new Map([
+        ['default', <TemplateIdMap> new Map([
+            [OnboardingResponses.responseIds.namePrompt, OnboardingResponses.fromResources('onboarding.namePrompt')],
+            [OnboardingResponses.responseIds.locationPrompt, OnboardingResponses.fromResources('onboarding.locationPrompt')],
+            [OnboardingResponses.responseIds.haveLocation, OnboardingResponses.fromResources('onboarding.haveLocation')],
+            [OnboardingResponses.responseIds.addLinkedAccountsMessage,
+                // tslint:disable-next-line:no-any
+                (context: TurnContext, data: any): Promise<Activity> => OnboardingResponses.buildLinkedAccountsCard(context, data)]
         ])]
    ]);
 
-   // Initialize the responses class properties
+   // Constructor
     constructor() {
         super();
-        this.register(new DictionaryRenderer(OnboardingResponses.RESPONSE_TEMPLATES));
+        this.register(new DictionaryRenderer(OnboardingResponses.responseTemplates));
+    }
+
+    // tslint:disable-next-line:no-any
+    public static async buildLinkedAccountsCard(turnContext: TurnContext, data: any): Promise<Activity> {
+        const title: string = __('onboarding.linkedAccountsInfoTitle');
+        const text: string = __('onboarding.linkedAccountsInfoBody');
+        const images: (CardImage | string)[] = [
+            {
+                url: __('onboarding.linkedAccountsInfoUrl'),
+                alt: __('onboarding.linkedAccountsInfoAlt')
+            }
+        ];
+        const attachment: Attachment = CardFactory.heroCard(title, text, images);
+        const response: Partial<Activity> = MessageFactory.attachment(attachment, title, InputHints.AcceptingInput);
+
+        return Promise.resolve(<Activity> response);
     }
 
     private static fromResources(name: string): TemplateFunction {
-        return (): Promise<string> => Promise.resolve(i18n.__(name));
+        return (): Promise<string> => Promise.resolve(__(name));
     }
+
 }
