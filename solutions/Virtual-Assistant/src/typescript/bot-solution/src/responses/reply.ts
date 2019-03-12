a0acc9cc6e4f3b1fc36bf39e5bb08243bb0330c9
David Favretto <david.favretto@southworks.com>
2019-03-12 18:17:49 -0300

    Migrate needed classes from microsoft.bot.solution

---
+++ solutions/Virtual-Assistant/src/typescript/bot-solution/src/responses/reply.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export class Reply {
+    public cardText: string = '';
+    public speak: string = '';
+    public text: string = '';
+}
