e7d391f07c5c1c5fa6900887b1e37c4c12d01eb3
David Favretto <david.favretto@southworks.com>
2019-03-12 18:16:11 -0300

    Migrate main classes

--- solutions/Virtual-Assistant/src/typescript/assistant/src/virtualAssistant.ts
+++ solutions/Virtual-Assistant/src/typescript/assistant/src/virtualAssistant.ts
@@ -1,16 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {
-    ConversationState,
-    TurnContext,
-    UserState } from 'botbuilder';
-import {
-    DialogContext,
-    DialogSet,
-    DialogState,
-    DialogTurnResult,
-    DialogTurnStatus } from 'botbuilder-dialogs';
+import { BotTelemetryClient, ConversationState, EndOfConversationCodes, TurnContext, UserState } from 'botbuilder';
+import { DialogContext, DialogSet, DialogState, DialogTurnResult } from 'botbuilder-dialogs';
+import { IEndpointService } from 'botframework-config';
 import { BotServices } from './botServices';
 import { MainDialog } from './dialogs/main/mainDialog';
 
@@ -18,37 +11,55 @@ import { MainDialog } from './dialogs/main/mainDialog';
  * Main entry point and orchestration for bot.
  */
 export class VirtualAssistant {
-    private readonly BOT_SERVICES: BotServices;
-    private readonly CONVERSATION_STATE: ConversationState;
-    private readonly USER_STATE: UserState;
-    private readonly DIALOGS: DialogSet;
+    private readonly services: BotServices;
+    private readonly conversationState: ConversationState;
+    private readonly userState: UserState;
+    private readonly endpointService: IEndpointService;
+    private readonly telemetryClient: BotTelemetryClient;
+    private readonly dialogs: DialogSet;
 
     /**
      * Constructs the three pieces necessary for this bot to operate.
      */
-    constructor(botServices: BotServices, conversationState: ConversationState, userState: UserState) {
-        if (!botServices) { throw new Error(('Missing parameter.  botServices is required')); }
+    constructor(botServices: BotServices,
+                conversationState: ConversationState,
+                userState: UserState,
+                endpointService: IEndpointService,
+                telemetryClient: BotTelemetryClient) {
         if (!conversationState) { throw new Error(('Missing parameter.  conversationState is required')); }
         if (!userState) { throw new Error(('Missing parameter.  userState is required')); }
+        if (!botServices) { throw new Error(('Missing parameter.  botServices is required')); }
+        if (!endpointService) { throw new Error(('Missing parameter.  endpointService is required')); }
+        if (!telemetryClient) { throw new Error(('Missing parameter.  telemetryClient is required')); }
 
-        this.BOT_SERVICES = botServices;
-        this.CONVERSATION_STATE = conversationState;
-        this.USER_STATE = userState;
+        this.services = botServices;
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.endpointService = endpointService;
+        this.telemetryClient = telemetryClient;
 
-        this.DIALOGS = new DialogSet(this.CONVERSATION_STATE.createProperty<DialogState>('VirtualAssistant'));
-        this.DIALOGS.add(new MainDialog());
+        this.dialogs = new DialogSet(this.conversationState.createProperty<DialogState>(VirtualAssistant.name));
+        this.dialogs.add(new MainDialog(this.services, this.conversationState, this.userState, this.endpointService, this.telemetryClient));
     }
 
     /**
      * Run every turn of the conversation. Handles orchestration of messages.
      */
     public async onTurn(turnContext: TurnContext): Promise<void> {
-        const dc: DialogContext = await this.DIALOGS.createContext(turnContext);
-        // tslint:disable-next-line:no-any
-        const result: DialogTurnResult<any> = await dc.continueDialog();
+        // Client notifying this bot took to long to respond (timed out)
+        if (turnContext.activity.code === EndOfConversationCodes.BotTimedOut) {
+            const message: string = `Timeout in ${turnContext.activity.channelId} channel: Bot took too long to respond.`;
+            this.services.telemetryClient.trackTrace({ message: message });
 
-        if (result.status === DialogTurnStatus.empty) {
-            await dc.beginDialog('MainDialog');
+            return;
+        }
+
+        const dc: DialogContext = await this.dialogs.createContext(turnContext);
+
+        if (!!dc.activeDialog) {
+            const result: DialogTurnResult = await dc.continueDialog();
+        } else {
+            await dc.beginDialog(MainDialog.name);
         }
     }
 }
