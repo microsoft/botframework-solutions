8ca5baa42871731f2228b198241509f2ae34142f
Darren Jefford <33519520+darrenj@users.noreply.github.com>
2019-03-12 15:30:31 +0000

    Update Experimental Skills and tidy of Skills Readme/Launch pages (#951)
    * Tidy up skills to have new landing pages (not default enterprise) and link to new documentation locations
    Update experimental skills to work with recent SkillDialog/Proactive and Response changes
    Overhaul Restaurant Booking to be a better showcase of timex, validation, etc.
    
    * updated to latest skill template, working tests
    
    * updates

--- solutions/Virtual-Assistant/src/csharp/experimental/skills/restaurantbooking/Dialogs/BookingDialog/Resources/Cards/CusineChoiceCardData.cs
+++ solutions/Virtual-Assistant/src/csharp/experimental/skills/restaurantbooking/Dialogs/BookingDialog/Resources/Cards/CusineChoiceCardData.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AdaptiveCards;
+using Microsoft.Bot.Solutions.Responses;
 
-namespace RestaurantBooking.Models
+namespace RestaurantBooking.Dialogs.Resources.Cards
 {
-    public class ImageHeaderTableFooterCardData : HeaderTableFooterCardData
+    public class CusineChoiceCardData : ICardData
     {
         public string ImageUrl { get; set; }
 
         public AdaptiveImageSize ImageSize { get; set; }
 
         public AdaptiveHorizontalAlignment ImageAlign { get; set; }
+
+        public string Cusine { get; set; }
     }
 }
