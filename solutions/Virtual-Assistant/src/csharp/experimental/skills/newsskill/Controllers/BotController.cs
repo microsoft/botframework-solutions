cb38ddce268bd8eb65bb019f6a5bea94c12f0b7b
Ted <tedlee@microsoft.com>
2019-03-16 13:40:28 -0700

    Refactor to MVC bot (#984)
    * add botcontroller
    
    * use botframeworkhttpadapter
    
    * refactor all bots to MVC and add controller

---
+++ solutions/Virtual-Assistant/src/csharp/experimental/skills/newsskill/Controllers/BotController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace NewsSkill.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly IBot _bot;
+
+        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        {
+            _adapter = adapter;
+            _bot = bot;
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+            await _adapter.ProcessAsync(Request, Response, _bot);
+        }
+    }
+}
