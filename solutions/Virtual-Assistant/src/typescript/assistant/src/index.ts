4f65c540b9cdce4fbf58a9d95a0f02db3df60773
David Favretto <david.favretto@southworks.com>
2019-03-19 18:13:13 -0300

    Update remaining 'assistant' files

--- solutions/Virtual-Assistant/src/typescript/assistant/src/index.ts
+++ solutions/Virtual-Assistant/src/typescript/assistant/src/index.ts
@@ -1,12 +1,24 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
 
 import { TelemetryClient } from 'applicationinsights';
-import { EventDebuggerMiddleware, SetLocaleMiddleware, SkillDefinition, TelemetryExtensions } from 'bot-solution';
+import {
+    BackgroundTaskQueue,
+    EventDebuggerMiddleware,
+    IBackgroundTaskQueue,
+    ProactiveState,
+    ProactiveStateMiddleware,
+    SetLocaleMiddleware,
+    SkillDefinition,
+    SkillEvent,
+    TelemetryExtensions } from 'bot-solution';
 import {
     AutoSaveStateMiddleware,
     BotFrameworkAdapter,
     ConversationState,
+    ShowTypingMiddleware,
     TranscriptLoggerMiddleware,
     TurnContext,
     UserState
@@ -35,6 +47,7 @@ import * as restify from 'restify';
 import { BotServices } from './botServices';
 import { MainResponses } from './dialogs/main/mainResponses';
 import { default as languageModelsRaw } from './languageModels.json';
+import { default as skillEventsRaw } from './skillEvents.json';
 import { default as skillsRaw } from './skills.json';
 import { VirtualAssistant } from './virtualAssistant';
 
@@ -73,6 +86,9 @@ function searchService(botConfiguration: IBotConfiguration, serviceType?: Servic
     return service;
 }
 
+// add background task queue
+const backgroundTaskQueue: IBackgroundTaskQueue = new BackgroundTaskQueue();
+
 // Initializes your bot language models and skills definitions
 const languageModels: Map<string, { botFilePath: string; botFileSecret: string }> = new Map(Object.entries(languageModelsRaw));
 
@@ -81,6 +97,10 @@ const skills: SkillDefinition[] = skillsRaw.map((skill: { [key: string]: Object|
     result.configuration = new Map<string, string>(Object.entries(skill.configuration || {}));
 
     return result;
+});
+
+const skillEvents: SkillEvent[] = skillEventsRaw.map((skillEvent: { [key: string]: Object|undefined }) => {
+    return Object.assign(new SkillEvent(), skillEvent);
 });
 
 try {
@@ -133,6 +153,7 @@ if (!cosmosConfig) {
 // create conversation and user state
 const conversationState: ConversationState = new ConversationState(storage);
 const userState: UserState = new UserState(storage);
+const proactiveState: ProactiveState = new ProactiveState(storage);
 
 // Use the AutoSaveStateMiddleware middleware to automatically read and write conversation and user state.
 adapter.use(new AutoSaveStateMiddleware(conversationState, userState));
@@ -152,11 +173,12 @@ const transcriptStore: AzureBlobTranscriptStore = new AzureBlobTranscriptStore({
 adapter.use(new TranscriptLoggerMiddleware(transcriptStore));
 
 /* Typing Middleware
-(automatically shows typing when the bot is responding/working)
-(not implemented https://github.com/Microsoft/botbuilder-js/issues/470)
-adapter.use(new ShowTypingMiddleware());*/
+(automatically shows typing when the bot is responding/working)*/
+adapter.use(new ShowTypingMiddleware());
 adapter.use(new SetLocaleMiddleware(DEFAULT_LOCALE));
 adapter.use(new EventDebuggerMiddleware());
+adapter.use(new ProactiveStateMiddleware(proactiveState));
+
 
 adapter.onTurnError = async (context: TurnContext, error: Error): Promise<void> => {
     // tslint:disable-next-line:no-console
@@ -173,8 +195,15 @@ adapter.onTurnError = async (context: TurnContext, error: Error): Promise<void> 
 
 let bot: VirtualAssistant;
 try {
-    const botServices: BotServices = new BotServices(botConfig, languageModels, skills);
-    bot = new VirtualAssistant(botServices, conversationState, userState, endpointService, telemetryClient);
+    const botServices: BotServices = new BotServices(botConfig, languageModels, skills, skillEvents);
+    bot = new VirtualAssistant(
+        botServices,
+        conversationState,
+        userState,
+        proactiveState,
+        endpointService,
+        telemetryClient,
+        backgroundTaskQueue);
 } catch (err) {
     throw err;
 }
