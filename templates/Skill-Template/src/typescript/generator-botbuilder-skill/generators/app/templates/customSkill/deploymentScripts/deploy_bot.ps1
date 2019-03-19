7570d90093b9e2f1277e7c9a2c224b717dc07f4c
Martin Battaglino <martinbatta32@gmail.com>
2019-03-19 09:49:32 -0300

    [Skill Template][TypeScript] Adapt PowerShell Deployment Scripts (#990)
    * Update cognitiveModel references in bot recipe
    
    * Adapt PS deployment scripts to TypeScript

--- templates/Skill-Template/src/typescript/generator-botbuilder-skill/generators/app/templates/customSkill/deploymentScripts/deploy_bot.ps1
+++ templates/Skill-Template/src/typescript/generator-botbuilder-skill/generators/app/templates/customSkill/deploymentScripts/deploy_bot.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 6
+
 # all msbot clone parameters and locales
 Param(
 	[string] [Parameter(Mandatory=$true)]$name,
@@ -9,23 +11,33 @@ Param(
 	[string] $luisPublishRegion,
 	[string] $subscriptionId,
 	[string] $insightsRegion,
-	[string] $groupName,
+	[string] $groupName = $name,
 	[string] $sdkLanguage,
 	[string] $sdkVersion,
 	[string] $prefix,
 	[string] $appId,        
-	[string] $appSecret
+    [string] $appSecret,
+    [switch] $showLogs
 )
 
 if (!$languagesOnly)
 {
 	# Change to project directory for .bot file
-	Write-Host "Changing to project directory ..."
-	cd "$($PSScriptRoot)\..\"
+    Write-Host "Changing to project directory ..."
+    Set-Location "$(Join-Path $PSScriptRoot ..)"
 
-	# Deploy the common resources (Azure Bot Service, App Insights, Azure Storage, Cosmos DB, etc)
+    # Deploy the common resources (Azure Bot Service, App Insights, Azure Storage, Cosmos DB, etc)
 	Write-Host "Deploying common resources..."
-	msbot clone services -n $name -l $location --luisAuthoringKey $luisAuthoringKey --folder "$($PSScriptRoot)" --appId $appId --appSecret $appSecret --force --quiet
+    $msbotScript = "msbot clone services -n " + $name + " -l " + $location + " --luisAuthoringKey " + $luisAuthoringKey + " --groupName " + $groupName + " --folder $($PSScriptRoot) --appId " + $appId + " --appSecret " + $appSecret + " --force  " 
+    if($showLogs)
+    {
+        $msbotScript += "--verbose"
+    }
+    else
+    {
+        $msbotScript += "--quiet"
+    }
+	Invoke-Expression $msbotScript
 }
 
 $localeArr = $locales.Split(',')
@@ -33,18 +45,18 @@ $localeArr = $locales.Split(',')
 foreach ($locale in $localeArr)
 {	
 	# Update deployment scripts for the locale
-	Invoke-Expression "$($PSScriptRoot)\generate_deployment_scripts.ps1 -locale $($locale)"
+	Invoke-Expression "$(Join-Path $PSScriptRoot generate_deployment_scripts.ps1) -locale $($locale)"
 
 	# Get language code from locale (first two characters, i.e. "en")
 	$langCode = ($locale -split "-")[0]
 
 	# Create LocaleConfigurations folder and change directory
-	mkdir -Force "$($PSScriptRoot)\..\LocaleConfigurations" > $null
-	Set-Location "$($PSScriptRoot)\..\LocaleConfigurations" > $null
+	New-Item -ItemType directory -Force -Path "$(Join-Path $PSScriptRoot .. LocaleConfigurations)" > $null
+	cd "$(Join-Path $PSScriptRoot .. LocaleConfigurations)" > $null
 
 	# Deploy Dispatch, LUIS (calendar, email, todo, and general), and QnA Maker for the locale
     Write-Host "Deploying $($locale) resources..."
-    msbot clone services -n "$($name)$($langCode)" -l $location --luisAuthoringKey $luisAuthoringKey --groupName $name --force --quiet --folder "$($PSScriptRoot)\$($langCode)" | Out-Null
+    msbot clone services -n "$($name)$($langCode)" -l $location --luisAuthoringKey $luisAuthoringKey --groupName $groupName --force --quiet --folder "$(Join-Path $PSScriptRoot $langCode)" | Out-Null
 }
 
 Write-Host "Done."
