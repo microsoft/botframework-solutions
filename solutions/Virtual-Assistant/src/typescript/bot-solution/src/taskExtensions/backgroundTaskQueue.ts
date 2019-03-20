82fdd493d9ae78c3dd33ba768117746cb614aeea
Enzo Cano <enzo.cano@southworks.com>
2019-03-20 07:52:17 -0300

    Add backgroundTaskQueue

--- solutions/Virtual-Assistant/src/typescript/bot-solution/src/taskExtensions/backgroundTaskQueue.ts
+++ solutions/Virtual-Assistant/src/typescript/bot-solution/src/taskExtensions/backgroundTaskQueue.ts
@@ -2,34 +2,27 @@
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
+import { default as PQueue } from 'p-queue';
 
 export type WorkItemFunc = () => Promise<void>;
 
 export interface IBackgroundTaskQueue {
     queueBackgroundWorkItem(workItem: WorkItemFunc): void;
-    dequeue(): Promise<WorkItemFunc>;
 }
 
 export class BackgroundTaskQueue implements IBackgroundTaskQueue {
-    private readonly workItems: WorkItemFunc[];
+    private readonly queueExecutor: PQueue;
 
     constructor() {
-        this.workItems = [];
+        this.queueExecutor = new PQueue({
+            concurrency: 1,
+            autoStart: true
+        });
     }
 
     public queueBackgroundWorkItem(workItem: WorkItemFunc): void {
         if (!workItem) { throw new Error('Missing parameter.  workItem is required'); }
 
-        this.workItems.push(workItem);
-    }
-
-    public dequeue(): Promise<WorkItemFunc> {
-        const workItem: WorkItemFunc|undefined = this.workItems.pop();
-
-        if (workItem) {
-            return Promise.resolve(workItem);
-        }
-
-        return Promise.resolve(() => Promise.resolve());
+        this.queueExecutor.add(workItem);
     }
 }
