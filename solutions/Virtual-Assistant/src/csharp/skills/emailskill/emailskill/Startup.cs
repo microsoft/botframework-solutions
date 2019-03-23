a489442e740f15a0b8b87d3f9b7a0df8ee254a93
Ted <tedlee@microsoft.com>
2019-03-23 16:40:37 -0700

    make adapters transient (#1011)

--- solutions/Virtual-Assistant/src/csharp/skills/emailskill/emailskill/Startup.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/emailskill/emailskill/Startup.cs
@@ -130,7 +130,7 @@ namespace EmailSkill
             services.AddSingleton<IBot, EmailSkill>();
 
             // Add the http adapter to enable MVC style bot API
-            services.AddSingleton<IBotFrameworkHttpAdapter>(sp =>
+            services.AddTransient<IBotFrameworkHttpAdapter>(sp =>
             {
                 var credentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
 
