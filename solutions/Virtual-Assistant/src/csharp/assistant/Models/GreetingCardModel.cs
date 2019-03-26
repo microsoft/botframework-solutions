6d9f753c571d887977669e18c18c08246ebdbc79
Ryan Lengel <rylengel@microsoft.com>
2019-03-25 18:02:02 -0700

    Added MainDialogResponses for new intro card text, GreetingCardModel for card properties, image assets for card.

---
+++ solutions/Virtual-Assistant/src/csharp/assistant/Models/GreetingCardModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder.Solutions.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualAssistant.Models
+{
+    public class GreetingCardModel : ICardData
+    {
+        public string HeaderUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
