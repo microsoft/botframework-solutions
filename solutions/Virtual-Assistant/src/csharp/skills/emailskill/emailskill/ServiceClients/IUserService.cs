584fb41e26cae4822cf4e84798d7af7c7094de2e
Qiong Wu <qiowu@microsoft.com>
2019-03-27 20:47:11 +0800

    modify icon size to fit bg image

--- solutions/Virtual-Assistant/src/csharp/skills/emailskill/emailskill/ServiceClients/IUserService.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/emailskill/emailskill/ServiceClients/IUserService.cs
@@ -30,8 +30,17 @@ namespace EmailSkill.ServiceClients
         /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         Task<List<Contact>> GetContactsAsync(string name);
 
+        /// <summary>
+        /// Get me.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         Task<User> GetMeAsync();
 
+        /// <summary>
+        /// Get user photo from your organization.
+        /// </summary>
+        /// <param name="id">The contact's id.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         Task<string> GetUserPhotoAsync(string id);
     }
 }
