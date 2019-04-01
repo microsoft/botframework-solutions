8c2473c6faf7c9db2ea8ce5f0b9b1bf6e725cdb1
Lauren Mills <lamil@microsoft.com>
2019-04-01 16:47:45 -0700

    folder cleanup

---
+++ templates/Virtual-Assistant-Template/VirtualAssistantTemplate/VirtualAssistantTemplate/Dialogs/EscalateDialog.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder;
+using VirtualAssistantTemplate.Responses.Escalate;
+
+namespace VirtualAssistantTemplate.Dialogs
+{
+    public class EscalateDialog : EnterpriseDialog
+    {
+        private EscalateResponses _responder = new EscalateResponses();
+
+        public EscalateDialog(BotServices botServices, IBotTelemetryClient telemetryClient)
+            : base(nameof(EscalateDialog), botServices, telemetryClient)
+        {
+            InitialDialogId = nameof(EscalateDialog);
+
+            var escalate = new WaterfallStep[]
+            {
+                SendPhone,
+            };
+
+            AddDialog(new WaterfallDialog(InitialDialogId, escalate));
+        }
+
+        private async Task<DialogTurnResult> SendPhone(WaterfallStepContext sc, CancellationToken cancellationToken)
+        {
+            await _responder.ReplyWith(sc.Context, EscalateResponses.ResponseIds.SendPhoneMessage);
+            return await sc.EndDialogAsync();
+        }
+    }
+}
---
+++ templates/Virtual-Assistant-Template/VirtualAssistantTemplate/VirtualAssistantTemplate/Dialogs/MainDialog.cs
@@ -0,0 +1,193 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VirtualAssistantTemplate.Dialogs.Onboarding;
+using Luis;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Globalization;
+using Microsoft.Bot.Builder.Skills;
+using Microsoft.Bot.Builder.Solutions.Dialogs;
+using VirtualAssistantTemplate.Responses.Main;
+
+namespace VirtualAssistantTemplate.Dialogs
+{
+    public class MainDialog : RouterDialog
+    {
+        private BotServices _services;
+        private UserState _userState;
+        private ConversationState _conversationState;
+        private MainResponses _responder = new MainResponses();
+
+        public MainDialog(
+            BotServices services,
+            ConversationState conversationState,
+            UserState userState,
+            IBotTelemetryClient telemetryClient)
+            : base(nameof(MainDialog), telemetryClient)
+        {
+            _services = services;
+            _conversationState = conversationState;
+            _userState = userState;
+            TelemetryClient = telemetryClient;
+
+            AddDialog(new OnboardingDialog(_services, _userState.CreateProperty<OnboardingState>(nameof(OnboardingState)), telemetryClient));
+            AddDialog(new EscalateDialog(_services, telemetryClient));
+
+            foreach (var skill in services.SkillDefinitions)
+            {
+                AddDialog(new SkillDialog(skill, telemetryClient));
+            }
+        }
+
+        protected override async Task OnStartAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var view = new MainResponses();
+            await view.ReplyWith(dc.Context, MainResponses.ResponseIds.Intro);
+        }
+
+        protected override async Task RouteAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Get cognitive models for locale
+            var locale = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var cognitiveModels = _services.CognitiveModelSets[locale];
+
+            // Check dispatch result
+            var dispatchResult = await cognitiveModels.DispatchService.RecognizeAsync<DispatchLUIS>(dc.Context, CancellationToken.None);
+            var intent = dispatchResult.TopIntent().intent;
+
+            if (_services.SkillDefinitions.Any(s => s.DispatchIntent == intent.ToString()))
+            {
+                var skill = _services.SkillDefinitions.Where(s => s.DispatchIntent == intent.ToString()).First();
+
+                // Initialize the skill connection
+                await dc.BeginDialogAsync(skill.Name);
+
+                // Pass the activity we have
+                var result = await dc.ContinueDialogAsync();
+
+                if (result.Status == DialogTurnStatus.Complete)
+                {
+                    await CompleteAsync(dc);
+                }
+            }
+            else if (intent == DispatchLUIS.Intent.l_general)
+            {
+                // If dispatch result is general luis model
+                cognitiveModels.LuisServices.TryGetValue("general", out var luisService);
+
+                if (luisService == null)
+                {
+                    throw new Exception("The specified LUIS Model could not be found in your Bot Services configuration.");
+                }
+                else
+                {
+                    var result = await luisService.RecognizeAsync<General>(dc.Context, CancellationToken.None);
+
+                    var generalIntent = result?.TopIntent().intent;
+
+                    // switch on general intents
+                    switch (generalIntent)
+                    {
+                        case General.Intent.Cancel:
+                            {
+                                // send cancelled response
+                                await _responder.ReplyWith(dc.Context, MainResponses.ResponseIds.Cancelled);
+
+                                // Cancel any active dialogs on the stack
+                                await dc.CancelAllDialogsAsync();
+                                break;
+                            }
+
+                        case General.Intent.Escalate:
+                            {
+                                // start escalate dialog
+                                await dc.BeginDialogAsync(nameof(EscalateDialog));
+                                break;
+                            }
+
+                        case General.Intent.Help:
+                            {
+                                // send help response
+                                await _responder.ReplyWith(dc.Context, MainResponses.ResponseIds.Help);
+                                break;
+                            }
+
+                        case General.Intent.None:
+                        default:
+                            {
+                                // No intent was identified, send confused message
+                                await _responder.ReplyWith(dc.Context, MainResponses.ResponseIds.Confused);
+                                break;
+                            }
+                    }
+                }
+            }
+            else if (intent == DispatchLUIS.Intent.q_faq)
+            {
+                cognitiveModels.QnAServices.TryGetValue("faq", out var qnaService);
+
+                if (qnaService == null)
+                {
+                    throw new Exception("The specified QnA Maker Service could not be found in your Bot Services configuration.");
+                }
+                else
+                {
+                    var answers = await qnaService.GetAnswersAsync(dc.Context);
+
+                    if (answers != null && answers.Count() > 0)
+                    {
+                        await dc.Context.SendActivityAsync(answers[0].Answer);
+                    }
+                }
+            }
+            else if (intent == DispatchLUIS.Intent.q_chitchat)
+            {
+                cognitiveModels.QnAServices.TryGetValue("chitchat", out var qnaService);
+
+                if (qnaService == null)
+                {
+                    throw new Exception("The specified QnA Maker Service could not be found in your Bot Services configuration.");
+                }
+                else
+                {
+                    var answers = await qnaService.GetAnswersAsync(dc.Context);
+
+                    if (answers != null && answers.Count() > 0)
+                    {
+                        await dc.Context.SendActivityAsync(answers[0].Answer);
+                    }
+                }
+            }
+            else
+            {
+                // If dispatch intent does not map to configured models, send "confused" response.
+                await _responder.ReplyWith(dc.Context, MainResponses.ResponseIds.Confused);
+            }
+        }
+
+        protected override async Task OnEventAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Check if there was an action submitted from intro card
+            if (dc.Context.Activity.Value != null)
+            {
+                dynamic value = dc.Context.Activity.Value;
+                if (value.action == "startOnboarding")
+                {
+                    await dc.BeginDialogAsync(nameof(OnboardingDialog));
+                    return;
+                }
+            }
+        }
+
+        protected override async Task CompleteAsync(DialogContext dc, DialogTurnResult result = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // The active dialog's stack ended with a complete status
+            await _responder.ReplyWith(dc.Context, MainResponses.ResponseIds.Completed);
+        }
+    }
+}
