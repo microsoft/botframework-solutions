aede4d9adc67a24a704a1adbc1eb30d41fba4ced
Lauren Mills <lamil@microsoft.com>
2019-04-03 11:26:09 -0700

    add cognitiveModelSet

---
+++ lib/csharp/microsoft.bot.builder.solutions/microsoft.bot.builder.solutions/CognitiveModelSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.AI.QnA;
+
+namespace Microsoft.Bot.Builder.Solutions.Configuration
+{
+    public class CognitiveModelSet
+    {
+        public IRecognizer DispatchService { get; set; }
+
+        public Dictionary<string, IRecognizer> LuisServices { get; set; } = new Dictionary<string, IRecognizer>();
+
+        public Dictionary<string, QnAMaker> QnAServices { get; set; } = new Dictionary<string, QnAMaker>();
+    }
+}
