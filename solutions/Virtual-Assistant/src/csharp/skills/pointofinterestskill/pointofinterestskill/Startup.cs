f30960eeb46ab08b61ce0214c77f74221277ccac
ted Li <tedlee@microsoft.com>
2019-04-04 12:48:12 -0700

    startup leftover

--- solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Startup.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Startup.cs
@@ -2,11 +2,8 @@
 // Licensed under the MIT License.
 
 using System.Linq;
-<<<<<<< HEAD
 using System.Threading.Tasks;
-=======
 using Microsoft.ApplicationInsights;
->>>>>>> origin/4.4
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,10 +16,6 @@ using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.Skills;
 using Microsoft.Bot.Builder.Skills.Auth;
-<<<<<<< HEAD
-using Microsoft.Bot.Builder.Solutions.Middleware;
-=======
->>>>>>> origin/4.4
 using Microsoft.Bot.Builder.Solutions.Proactive;
 using Microsoft.Bot.Builder.Solutions.Responses;
 using Microsoft.Bot.Builder.Solutions.TaskExtensions;
@@ -30,14 +23,6 @@ using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-<<<<<<< HEAD
-using PointOfInterestSkill.Dialogs.CancelRoute.Resources;
-using PointOfInterestSkill.Dialogs.FindPointOfInterest.Resources;
-using PointOfInterestSkill.Dialogs.Main.Resources;
-using PointOfInterestSkill.Dialogs.Route.Resources;
-using PointOfInterestSkill.Dialogs.Shared.Resources;
-using PointOfInterestSkill.ServiceClients;
-=======
 using PointOfInterestSkill.Adapters;
 using PointOfInterestSkill.Bots;
 using PointOfInterestSkill.Dialogs;
@@ -47,7 +32,6 @@ using PointOfInterestSkill.Responses.Main;
 using PointOfInterestSkill.Responses.Route;
 using PointOfInterestSkill.Responses.Shared;
 using PointOfInterestSkill.Services;
->>>>>>> origin/4.4
 
 namespace PointOfInterestSkill
 {
@@ -94,58 +78,11 @@ namespace PointOfInterestSkill
             services.AddSingleton<ProactiveState>();
             services.AddSingleton(sp =>
             {
-<<<<<<< HEAD
-                CosmosDBEndpoint = new Uri(cosmosDb.Endpoint),
-                AuthKey = cosmosDb.Key,
-                CollectionId = cosmosDb.Collection,
-                DatabaseId = cosmosDb.Database,
-            };
-            var dataStore = new CosmosDbStorage(cosmosOptions);
-            var userState = new UserState(dataStore);
-            var conversationState = new ConversationState(dataStore);
-            var proactiveState = new ProactiveState(dataStore);
-
-            services.AddSingleton(dataStore);
-            services.AddSingleton(userState);
-            services.AddSingleton(conversationState);
-            services.AddSingleton(proactiveState);
-            services.AddSingleton(new BotStateSet(userState, conversationState));
-
-            var environment = _isProduction ? "production" : "development";
-            var service = botConfig.Services.FirstOrDefault(s => s.Type == ServiceTypes.Endpoint && s.Name == environment);
-            if (!(service is EndpointService endpointService))
-            {
-                throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
-            }
-
-            services.AddSingleton(endpointService);
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.Authority = "https://login.microsoftonline.com/microsoft.com";
-                options.Audience = endpointService.AppId;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidIssuer = "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/v2.0",
-                };
-            });
-
-            // comment out for now to disable whitelist checking
-            //services.AddSingleton<ISkillAuthProvider, JwtClaimAuthProvider>();
-            //services.AddSingleton<ISkillWhitelist, SkillWhitelist>();
-
-            services.AddSingleton<IServiceManager, ServiceManager>();
-
-            // HttpContext required for path resolution
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-=======
                 var userState = sp.GetService<UserState>();
                 var conversationState = sp.GetService<ConversationState>();
                 var proactiveState = sp.GetService<ProactiveState>();
                 return new BotStateSet(userState, conversationState);
             });
->>>>>>> origin/4.4
 
             // Configure telemetry
             var telemetryClient = new BotTelemetryClient(new TelemetryClient(settings.AppInsights));
@@ -183,8 +120,9 @@ namespace PointOfInterestSkill
                 };
             });
 
-            services.AddSingleton<ISkillAuthProvider, JwtClaimAuthProvider>();
-            services.AddSingleton<ISkillWhitelist, SkillWhitelist>();
+            // comment out for now to disable whitelist checking
+            //services.AddSingleton<ISkillAuthProvider, JwtClaimAuthProvider>();
+            //services.AddSingleton<ISkillWhitelist, SkillWhitelist>();
 
             // Configure adapters
             services.AddTransient<IBotFrameworkHttpAdapter, DefaultAdapter>();
