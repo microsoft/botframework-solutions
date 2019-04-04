89c3ec60ad6da8e9c85f409f2b6793b8b8d6ebd2
Ryan Lengel Isgrig <rylengel@microsoft.com>
2019-04-04 14:06:12 -0700

    Added first set of speech utility tests

---
+++ lib/csharp/microsoft.bot.builder.solutions/microsoft.bot.builder.solutions.tests/Responses/SpeechUtilityTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Bot.Schema;
+using Microsoft.Bot.Builder.Solutions.Responses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdaptiveCards;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace Microsoft.Bot.Builder.Solutions.Tests
+{
+    [TestClass]
+    public class SpeechUtilityTests
+    {
+
+        private Activity _activity;
+
+        private PromptOptions _promptOptions;
+
+        private string parentSpeakProperty = "Parent speak property";
+
+        private string listItemSpeakProperty ="List item speak property";
+
+        [TestInitialize]
+        public void Setup()
+        {            
+            _activity = new Activity() { Speak = parentSpeakProperty };
+            _promptOptions = new PromptOptions() { Prompt = new Activity() { Speak = parentSpeakProperty } };
+        }
+
+        [TestMethod]
+        public void GetSpeechReadyStringFromPromptOptions()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetSpeechReadyStringFromActivityWithAttachments()
+        {
+            _activity.Attachments = new List<Attachment> { new Attachment(content: new AdaptiveCard() { Speak = listItemSpeakProperty }) };
+            var response = SpeechUtility.ListToSpeechReadyString(_activity);
+
+            Assert.AreEqual(response, string.Format($"{parentSpeakProperty}<break/>{listItemSpeakProperty}"));
+        }
+    }
+}
