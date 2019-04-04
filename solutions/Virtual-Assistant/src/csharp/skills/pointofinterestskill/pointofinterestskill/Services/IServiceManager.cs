5d2c1dfc496d190cb9c54bd50984259d0540bd9f
lauren-mills <lamil@microsoft.com>
2019-04-04 11:44:29 -0700

    [4.4] Updates POI skill to new folder structure + config (#1054)
    * updated poi skill folders + config
    
    * fixed merge issues

---
+++ solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Services/IServiceManager.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Bot.Builder.Solutions.Skills;
+
+namespace PointOfInterestSkill.Services
+{
+    public interface IServiceManager
+    {
+        IGeoSpatialService InitMapsService(BotSettings services, string locale = "en-us");
+
+        IGeoSpatialService InitAddressMapsService(BotSettings services, string locale = "en-us");
+
+        IGeoSpatialService InitRoutingMapsService(BotSettings services, string locale = "en-us");
+    }
+}
