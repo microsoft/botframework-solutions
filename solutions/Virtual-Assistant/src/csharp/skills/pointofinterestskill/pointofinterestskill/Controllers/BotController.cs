efd25b9f43472fbc7d202fcdacf57356febc1a8e
ted Li <tedlee@microsoft.com>
2019-04-04 12:49:31 -0700

    bot controller leftover

--- solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Controllers/BotController.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/pointofinterestskill/pointofinterestskill/Controllers/BotController.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder.Skills;
-using Microsoft.Bot.Builder.Skills.Auth;
 
 namespace PointOfInterestSkill.Controllers
 {
     [ApiController]
     public class BotController : SkillController
     {
-<<<<<<< HEAD
         public BotController(IServiceProvider serviceProvider)
             : base(serviceProvider)
-=======
-        public BotController(IBotFrameworkHttpAdapter botFrameworkHttpAdapter, SkillAdapter skillAdapter, ISkillAuthProvider skillAuthProvider, IBot bot)
-            : base(botFrameworkHttpAdapter, skillAdapter, skillAuthProvider, bot)
->>>>>>> origin/4.4
         {
         }
     }
