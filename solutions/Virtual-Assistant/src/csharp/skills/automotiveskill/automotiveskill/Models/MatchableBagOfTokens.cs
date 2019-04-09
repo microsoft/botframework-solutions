41c6ed6697168e7aa3adc02774a3410e46c0b93f
Lauren Mills <lamil@microsoft.com>
2019-04-09 09:24:47 -0700

    added botsettingsbase.cs

--- solutions/Virtual-Assistant/src/csharp/skills/automotiveskill/automotiveskill/Models/MatchableBagOfTokens.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/automotiveskill/automotiveskill/Models/MatchableBagOfTokens.cs
@@ -4,7 +4,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace AutomotiveSkill.Utilities
+namespace AutomotiveSkill.Models
 {
     /// <summary>
     /// Precomputed information about a bag of tokens.
--- templates/Virtual-Assistant-Template/VirtualAssistantTemplate/VirtualAssistantTemplate/Services/BotSettings.cs
+++ templates/Virtual-Assistant-Template/VirtualAssistantTemplate/VirtualAssistantTemplate/Services/BotSettings.cs
@@ -1,21 +1,15 @@
 ﻿using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.Skills;
+using Microsoft.Bot.Builder.Solutions.Shared;
 using Microsoft.Bot.Builder.Solutions.Shared.Authentication;
-using Microsoft.Bot.Configuration;
 using System.Collections.Generic;
 
 namespace VirtualAssistantTemplate.Services
 {
-    public class BotSettings
+    public class BotSettings : BotSettingsBase
     {
-        public string MicrosoftAppId { get; set; }
-
-        public string MicrosoftAppPassword { get; set; }
-
         public List<OAuthConnection> OAuthConnections { get; set; }
-
-        public string DefaultLocale { get; set; }
 
         public CosmosDbStorageOptions CosmosDb { get; set; }
 
@@ -24,29 +18,10 @@ namespace VirtualAssistantTemplate.Services
         public BlobStorageConfiguration BlobStorage { get; set; }
 
         public ContentModeratorConfiguration ContentModerator { get; set; }
+        public string DefaultLocale { get; set; }
 
         public Dictionary<string, CognitiveModelConfiguration> CognitiveModels { get; set; }
 
         public List<SkillDefinition> Skills { get; set; }
-
-        public class BlobStorageConfiguration
-        {
-            public string ConnectionString { get; set; }
-            public string Container { get; set; }
-        }
-
-        public class ContentModeratorConfiguration
-        {
-            public string Key { get; set; }
-        }
-
-        public class CognitiveModelConfiguration
-        {
-            public DispatchService DispatchModel { get; set; }
-
-            public List<LuisService> LanguageModels { get; set; }
-
-            public List<QnAMakerService> Knowledgebases { get; set; }
-        }
     }
 }
