ae61a7d23bfadd0207cf32326eaeb36ebe5bcf6e
Ryan Lengel Isgrig <rylengel@microsoft.com>
2019-04-09 13:35:53 -0700

    Fixed solution refs

--- lib/csharp/microsoft.bot.builder.solutions/microsoft.bot.builder.solutions/Resources/CommonStrings.Designer.cs
+++ lib/csharp/microsoft.bot.builder.solutions/microsoft.bot.builder.solutions/Resources/CommonStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Bot.Builder.Solutions.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class CommonStrings {
@@ -201,15 +201,6 @@ namespace Microsoft.Bot.Builder.Solutions.Resources {
         public static string LoginDescription {
             get {
                 return ResourceManager.GetString("LoginDescription", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to more.
-        /// </summary>
-        public static string More {
-            get {
-                return ResourceManager.GetString("More", resourceCulture);
             }
         }
         
