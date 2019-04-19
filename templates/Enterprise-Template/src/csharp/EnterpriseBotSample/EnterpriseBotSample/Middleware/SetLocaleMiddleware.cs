5dd902fa27486d35d90c41c90408d9ba44367139
Tiago Noronha <27889458+tiagodenoronha@users.noreply.github.com>
2019-04-19 14:22:08 +0100

    Fixed bug with locale
    The local returned empty and not null, thus rendering the cultureInfo as empty.

--- templates/Enterprise-Template/src/csharp/EnterpriseBotSample/EnterpriseBotSample/Middleware/SetLocaleMiddleware.cs
+++ templates/Enterprise-Template/src/csharp/EnterpriseBotSample/EnterpriseBotSample/Middleware/SetLocaleMiddleware.cs
@@ -16,7 +16,7 @@ namespace EnterpriseBotSample.Middleware
 
         public async Task OnTurnAsync(ITurnContext context, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var cultureInfo = context.Activity.Locale != null ? new CultureInfo(context.Activity.Locale) : new CultureInfo(defaultLocale);
+            var cultureInfo = !string.IsNullOrWhiteSpace(context.Activity.Locale) ? new CultureInfo(context.Activity.Locale) : new CultureInfo(defaultLocale);
 
             CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture = cultureInfo;
 
