1331a6d6968e692097c2d3bf607e9ae66223397a
Lauren Mills <lamil@microsoft.com>
2019-04-30 14:10:21 -0700

    va template updates

--- templates/Virtual-Assistant-Template/csharp/Template/VA/Responses/Main/MainStrings.Designer.cs
+++ templates/Virtual-Assistant-Template/csharp/Template/VA/Responses/Main/MainStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace $safeprojectname$.Responses.Main {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class MainStrings {
@@ -187,11 +187,20 @@ namespace $safeprojectname$.Responses.Main {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ./Content/Intro.json.
+        ///   Looks up a localized string similar to ./Content/NewUserGreeting.json.
         /// </summary>
         public static string INTRO_PATH {
             get {
                 return ResourceManager.GetString("INTRO_PATH", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ./Content/ReturningUserGreeting.json.
+        /// </summary>
+        public static string INTRO_RETURNING {
+            get {
+                return ResourceManager.GetString("INTRO_RETURNING", resourceCulture);
             }
         }
         
