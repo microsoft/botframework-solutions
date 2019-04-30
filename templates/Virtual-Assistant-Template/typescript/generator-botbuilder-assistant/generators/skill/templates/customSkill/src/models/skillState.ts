c7bc406762dcecc76cbffa24c68a9f618ab105f1
batta32 <martinbatta32@gmail.com>
2019-04-30 17:53:56 -0300

    Add models and bots folder

---
+++ templates/Virtual-Assistant-Template/typescript/generator-botbuilder-assistant/generators/skill/templates/customSkill/src/models/skillState.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export interface ISkillState {
+    readonly token: string;
+    clear(): void;
+}
