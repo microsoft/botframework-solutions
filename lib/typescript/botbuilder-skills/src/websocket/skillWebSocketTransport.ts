8a69af04bef233231f25b92eaa3af36b3c1823dc
Enzo Cano <enzo.cano@southworks.com>
2019-05-02 10:57:34 -0300

    Fix issue with skillDialog

--- lib/typescript/botbuilder-skills/src/websocket/skillWebSocketTransport.ts
+++ lib/typescript/botbuilder-skills/src/websocket/skillWebSocketTransport.ts
@@ -1,8 +1,17 @@
 import { TurnContext } from 'botbuilder';
+import { MicrosoftAppCredentials } from 'botframework-connector';
 import { Activity } from 'botframework-schema';
 import { ISkillTransport, TokenRequestHandler } from '../skillTransport';
+import { ISkillManifest } from '../models';
 
 export class SkillWebSocketTransport implements ISkillTransport {
+    constructor(
+        skillManifest: ISkillManifest,
+        appCredentials: MicrosoftAppCredentials
+    ) {
+        
+    }
+
     public forwardToSkill(
         turnContext: TurnContext,
         activity: Partial<Activity>,
