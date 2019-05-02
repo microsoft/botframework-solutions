e4b01b7d37a2708a65c22b9a3437cc438e80bc88
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-05-01 17:59:06 -0700

    [4.4] Experimental weather skill (#1217)
    * Add base WeatherSkill (experimental) with LUIS model using a manual sample of the prebuilt weather intent and geography entity. Parsing luis result into skill state.
    
    * Added service calls for location & 1 day forecast information
    
    * Added card response and formatting into daily/six hour forecast.
    Added settings property for api key.
    Added note in experimental skills doc.
    
    * Refactore ApiKey to apiKey and removed custom deployment info

---
+++ skills/src/csharp/experimental/weatherskill/Models/Accuweather/Day.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherSkill.Models
+{
+    public class Day
+    {
+        public int Icon { get; set; }
+        public string IconPhrase { get; set; }
+        public string ShortPhrase { get; set; }
+        public string LongPhrase { get; set; }
+
+        public Wind Wind { get; set; }
+    }
+}
---
+++ skills/src/csharp/experimental/weatherskill/Startup.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Bot.Builder.ApplicationInsights;
+using Microsoft.ApplicationInsights;
+using Microsoft.Bot.Builder.BotFramework;
+using WeatherSkill.Bots;
+using Microsoft.AspNetCore.Mvc;
+using WeatherSkill.Dialogs;
+using WeatherSkill.Services;
+using Microsoft.Bot.Builder.Solutions.TaskExtensions;
+using Microsoft.Bot.Builder.Solutions.Responses;
+using System.Linq;
+using WeatherSkill.Responses.Main;
+using WeatherSkill.Responses.Shared;
+using WeatherSkill.Responses.Sample;
+using Microsoft.Bot.Builder.Skills;
+using WeatherSkill.Adapters;
+using Microsoft.Bot.Builder.Solutions;
+
+namespace WeatherSkill
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("cognitivemodels.json", optional: true)
+                .AddJsonFile($"cognitivemodels.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("skills.json", optional: true)
+                .AddJsonFile($"skills.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Load settings
+            var settings = new BotSettings();
+            Configuration.Bind(settings);
+            services.AddSingleton(settings);
+            services.AddSingleton<BotSettingsBase>(settings);
+
+            // Configure bot services
+            services.AddSingleton<BotServices>();
+
+            // Configure credentials
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Configure storage
+            services.AddSingleton<IStorage>(new CosmosDbStorage(settings.CosmosDb));
+            services.AddSingleton<UserState>();
+            services.AddSingleton<ConversationState>();
+            services.AddSingleton(sp =>
+            {
+                var userState = sp.GetService<UserState>();
+                var conversationState = sp.GetService<ConversationState>();
+                return new BotStateSet(userState, conversationState);
+            });
+
+            // Configure telemetry
+            var telemetryClient = new BotTelemetryClient(new TelemetryClient(settings.AppInsights));
+            services.AddSingleton<IBotTelemetryClient>(telemetryClient);
+            services.AddBotApplicationInsights(telemetryClient);
+
+            // Configure proactive
+            services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+            services.AddHostedService<QueuedHostedService>();
+
+            // Configure responses
+            services.AddSingleton(sp => new ResponseManager(
+                settings.CognitiveModels.Select(l => l.Key).ToArray(),
+                new MainResponses(),
+                new SharedResponses(),
+                new SampleResponses()));
+
+            // Register dialogs
+            services.AddTransient<SampleDialog>();
+            services.AddTransient<MainDialog>();
+
+            // Configure adapters
+            services.AddTransient<IBotFrameworkHttpAdapter, DefaultAdapter>();
+            services.AddTransient<SkillWebSocketBotAdapter, CustomSkillAdapter>();
+            services.AddTransient<SkillWebSocketAdapter>();
+
+            // Configure bot
+            services.AddTransient<MainDialog>();
+            services.AddTransient<IBot, DialogBot<MainDialog>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseBotApplicationInsights()
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSockets()
+                .UseMvc();
+        }
+    }
+}
