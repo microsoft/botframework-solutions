0ac6d0aae69e6885eb5e4e84bb413e866cf8c408
Enzo Cano <enzo.cano@southworks.com>
2019-05-03 08:43:02 -0300

    Add script to update version and build all typescript projects

---
+++ build.ts.ps1
@@ -0,0 +1,58 @@
+﻿Param(
+	[string] $version
+)
+
+if (-not $version) {
+    Write-Host "Version required!.  Please use the param -version" -ForegroundColor DarkRed
+	Break
+}
+
+pushd .\lib\typescript
+
+rush update
+
+pushd .\botskills
+
+npm version $($version) --allow-same-version
+npm run build
+
+popd
+
+pushd .\botbuilder-solutions
+
+npm version $($version) --allow-same-version
+npm run build
+
+popd
+
+pushd .\botbuilder-skills
+
+npm version $($version) --allow-same-version
+npm run build
+
+popd
+
+popd
+
+pushd .\templates\Virtual-Assistant-Template\typescript\generator-botbuilder-assistant
+
+npm version $($version) --allow-same-version
+npm install
+
+popd
+
+if (-not(test-path ".\outputpackages"))
+{
+      New-Item -ItemType directory -Path ".\outputpackages"
+}
+
+pushd .\outputpackages
+
+npm pack ..\lib\typescript\botskills
+npm pack ..\lib\typescript\botbuilder-solutions
+npm pack ..\lib\typescript\botbuilder-skills
+npm pack ..\templates\Virtual-Assistant-Template\typescript\generator-botbuilder-assistant
+
+dir
+
+popd
