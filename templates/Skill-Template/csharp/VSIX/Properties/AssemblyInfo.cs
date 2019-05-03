624080bcd99a45b25e6970ca6c7b8e34e2504861
Darren Jefford <33519520+darrenj@users.noreply.github.com>
2019-05-02 20:10:25 -0700

    Fix skill template (#1240)
    * fix vsix asset type
    
    * more changes
    
    * skill template fixes

--- templates/Skill-Template/csharp/VSIX/Properties/AssemblyInfo.cs
+++ templates/Skill-Template/csharp/VSIX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SkillVSIX")]
+[assembly: AssemblyTitle("SkillTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SkillVSIX")]
+[assembly: AssemblyProduct("SkillTemplate")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
