d30e414d3119a4684a372ad79f8a08281f3b051d
Darren Jefford <darrenj@microsoft.com>
2019-05-02 19:56:54 -0700

    more changes

--- templates/Virtual-Assistant-Template/csharp/VSIX/Properties/AssemblyInfo.cs
+++ templates/Virtual-Assistant-Template/csharp/VSIX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSIX")]
+[assembly: AssemblyTitle("VirtualAssistantTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSIX")]
+[assembly: AssemblyProduct("VirtualAssistantTemplate")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
