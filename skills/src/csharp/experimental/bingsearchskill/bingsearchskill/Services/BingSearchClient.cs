fa4d35fb4e2bec24d0f99fb7ef897b61aa640f13
yuesu <yuesu@microsoft.com>
2019-05-05 10:44:27 +0800

    add bing search client

---
+++ skills/src/csharp/experimental/bingsearchskill/bingsearchskill/Services/BingSearchClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Search.EntitySearch;
+using Microsoft.Azure.CognitiveServices.Search.EntitySearch.Models;
+using Newtonsoft.Json;
+
+namespace BingSearchSkill.Services
+{
+    public class BingSearchClient
+    {
+        private EntitySearchClient _client;
+
+        public BingSearchClient(string key)
+        {
+            _client = new EntitySearchClient(new ApiKeyServiceClientCredentials(key));
+        }
+
+        public async Task<Entities> GetSearchResult(string query)
+        {
+            var entityData = await _client.Entities.SearchAsync(query);
+            return entityData.Entities;
+        }
+    }
+}
