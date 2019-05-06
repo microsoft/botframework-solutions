1fb3b570671f1861459911a13e06b620559163d6
Enzo Cano <enzo.cano@southworks.com>
2019-05-06 10:54:55 -0300

    [TypeScript] Fix issue with installed dependencies (#1294)
    * Fix NPM install error
    
    * Update deployment scripts
    
    * Fix NPM install error for sample assistant
    
    * Add skill port to be communicated with the assistant
    
    * Update generator-botbuilder-assistant version

--- templates/Virtual-Assistant-Template/typescript/generator-botbuilder-assistant/generators/app/templates/customAssistant/deployment/scripts/deploy.ps1
+++ templates/Virtual-Assistant-Template/typescript/generator-botbuilder-assistant/generators/app/templates/customAssistant/deployment/scripts/deploy.ps1
@@ -100,7 +100,7 @@ if ($parametersFile) {
     (az group deployment create `
         --name $timestamp `
         --resource-group $resourceGroup `
-        --template-file "$(Join-Path $PSScriptRoot '..' 'Resources' 'template.json')" `
+        --template-file "$(Join-Path $PSScriptRoot '..' 'resources' 'template.json')" `
         --parameters "@$($parametersFile)" `
         --parameters microsoftAppId=$appId microsoftAppPassword="`"$($appPassword)`"") 2>> $logFile | Out-Null
 }
@@ -108,7 +108,7 @@ else {
     (az group deployment create `
         --name $timestamp `
         --resource-group $resourceGroup `
-        --template-file "$(Join-Path $PSScriptRoot '..' 'Resources' 'template.json')" `
+        --template-file "$(Join-Path $PSScriptRoot '..' 'resources' 'template.json')" `
         --parameters microsoftAppId=$appId microsoftAppPassword="`"$($appPassword)`"") 2>> $logFile | Out-Null
 }
 
