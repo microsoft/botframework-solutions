4afe143191f86943cb4a8c9aef513726470a5d6e
David Favretto <david.favretto@southworks.com>
2019-05-07 19:58:35 -0300

    Refactor commands to use string arrays instead of string concatenations (#1310)

--- lib/typescript/botskills/src/utils/index.ts
+++ lib/typescript/botskills/src/utils/index.ts
@@ -3,6 +3,6 @@
  * Licensed under the MIT License.
  */
 
-export { execute, extractArgs, spawn, tryExecute } from './childProcessUtils';
+export { execute, spawn, tryExecute } from './childProcessUtils';
 export { authenticate } from './authenticationUtils';
 export { validatePairOfArgs } from './validationUtils';
