2343dbb8cb199f6923d4319eb957220b9df7f613
Enzo Cano <enzo.cano@southworks.com>
2019-05-09 16:07:35 -0300

    Update VA template

--- templates/Virtual-Assistant-Template/typescript/generator-botbuilder-assistant/generators/app/templates/customAssistant/src/adapters/defaultAdapter.ts
+++ templates/Virtual-Assistant-Template/typescript/generator-botbuilder-assistant/generators/app/templates/customAssistant/src/adapters/defaultAdapter.ts
@@ -3,14 +3,18 @@
  * Licensed under the MIT License.
  */
 
-import { TelemetryClient } from 'applicationinsights';
 import {
+    ActivityTypes,
     AutoSaveStateMiddleware,
     BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
+    BotTelemetryClient,
     ConversationState,
     ShowTypingMiddleware,
+    TelemetryLoggerMiddleware,
     TranscriptLoggerMiddleware,
+    TranscriptStore,
+    TurnContext,
     UserState
 } from 'botbuilder';
 import {
@@ -19,27 +23,35 @@ import {
     CosmosDbStorageSettings
 } from 'botbuilder-azure';
 import { ISkillManifest } from 'botbuilder-skills';
-import {
-    EventDebuggerMiddleware,
-    ProactiveState,
-    ProactiveStateMiddleware,
-    SetLocaleMiddleware } from 'botbuilder-solutions';
+import { EventDebuggerMiddleware, SetLocaleMiddleware } from 'botbuilder-solutions';
+import i18next from 'i18next';
 import { IBotSettings } from '../services/botSettings.js';
 
 export class DefaultAdapter extends BotFrameworkAdapter {
-    private readonly transcriptStore: AzureBlobTranscriptStore;
-    private readonly proactiveState: ProactiveState;
     public readonly conversationState: ConversationState;
     public readonly cosmosDbStorageSettings: CosmosDbStorageSettings;
     public readonly skills: ISkillManifest[] = [];
-    public readonly telemetryClient: TelemetryClient;
     public readonly userState: UserState;
 
     constructor(
         settings: Partial<IBotSettings>,
-        adapterSettings: Partial<BotFrameworkAdapterSettings>
+        adapterSettings: Partial<BotFrameworkAdapterSettings>,
+        telemetryClient: BotTelemetryClient
     ) {
         super(adapterSettings);
+
+        this.onTurnError = async (context: TurnContext, error: Error): Promise<void> => {
+            await context.sendActivity({
+                type: ActivityTypes.Trace,
+                text: error.message
+            });
+            await context.sendActivity({
+                type: ActivityTypes.Trace,
+                text: error.stack
+            });
+            await context.sendActivity(i18next.t('main.error'));
+            telemetryClient.trackException({ exception: error });
+        };
 
         if (settings.cosmosDb === undefined) {
             throw new Error('There is no cosmosDb value in appsettings file');
@@ -57,34 +69,21 @@ export class DefaultAdapter extends BotFrameworkAdapter {
         // create conversation and user state
         this.conversationState = new ConversationState(storage);
         this.userState = new UserState(storage);
-        this.proactiveState = new ProactiveState(storage);
 
         if (settings.blobStorage === undefined) {
             throw new Error('There is no blobStorage value in appsettings file');
         }
 
-        this.transcriptStore = new AzureBlobTranscriptStore({
+        const transcriptStore: TranscriptStore = new AzureBlobTranscriptStore({
             containerName: settings.blobStorage.container,
             storageAccountOrConnectionString: settings.blobStorage.connectionString
         });
 
-        if (settings.appInsights === undefined) {
-            throw new Error('There is no appInsights value in appsettings file');
-        }
-        this.telemetryClient = new TelemetryClient(settings.appInsights.instrumentationKey);
-
-        // Use the AutoSaveStateMiddleware middleware to automatically read and write conversation and user state.
-        this.use(new AutoSaveStateMiddleware(this.conversationState, this.userState));
-        // Currently not working https://github.com/Microsoft/botbuilder-js/issues/853#issuecomment-481416004
-        // this.use(new TranscriptLoggerMiddleware(this.transcriptStore));
-
-        // Typing Middleware (automatically shows typing when the bot is responding/working)
+        this.use(new TranscriptLoggerMiddleware(transcriptStore));
+        this.use(new TelemetryLoggerMiddleware(telemetryClient, true));
         this.use(new ShowTypingMiddleware());
-        if (settings.defaultLocale === undefined) {
-            throw new Error('There is no defaultLocale value in appsettings file');
-        }
-        this.use(new SetLocaleMiddleware(settings.defaultLocale));
+        this.use(new SetLocaleMiddleware(settings.defaultLocale || 'en-us'));
         this.use(new EventDebuggerMiddleware());
-        this.use(new ProactiveStateMiddleware(this.proactiveState));
+        this.use(new AutoSaveStateMiddleware(this.conversationState, this.userState));
     }
 }
