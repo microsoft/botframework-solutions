a628e2dc712a31e285432881df986046fe7fc47f
Diego Cardozo <42946572+DiegoCardozo94@users.noreply.github.com>
2019-05-08 21:26:50 -0300

    Add new Sample-Skill (#1318)

---
+++ templates/Virtual-Assistant-Template/typescript/samples/sample-skill/src/services/botSettings.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { IBotSettingsBase } from 'botbuilder-solutions';
+
+// tslint:disable-next-line: no-empty-interface
+export interface IBotSettings extends IBotSettingsBase {
+
+}
