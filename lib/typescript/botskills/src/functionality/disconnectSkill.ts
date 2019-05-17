d1bc3566e0e8951f947efef71c612ee8a796f01e
David Favretto <david.favretto@southworks.com>
2019-05-17 10:07:19 -0300

    Add disconnect tests

--- lib/typescript/botskills/src/functionality/disconnectSkill.ts
+++ lib/typescript/botskills/src/functionality/disconnectSkill.ts
@@ -23,7 +23,7 @@ async function runCommand(command: string[], description: string): Promise<strin
     }
 }
 
-async function updateDispatch(configuration: IDisconnectConfiguration): Promise<void> {
+async function updateDispatch(configuration: IDisconnectConfiguration): Promise<boolean> {
     try {
         // Initializing variables for the updateDispatch scope
         const dispatchFile: string = `${configuration.dispatchName}.dispatch`;
@@ -36,7 +36,8 @@ async function updateDispatch(configuration: IDisconnectConfiguration): Promise<
         // dispatch remove(?)
         if (!existsSync(dispatchFilePath)) {
             logger.error(`Could not find file ${dispatchFile}. Please provide the 'dispatchName' and 'dispatchFolder' parameters.`);
-            process.exit(1);
+
+            return false;
         }
         // tslint:disable-next-line:no-var-require non-literal-require
         const dispatchData: IDispatchFile = JSON.parse(
@@ -44,12 +45,16 @@ async function updateDispatch(configuration: IDisconnectConfiguration): Promise<
             .toString());
         const serviceToRemove: IDispatchService | undefined = dispatchData.services.find((service: IDispatchService) =>
             service.name === configuration.skillId);
-        if (serviceToRemove) {
-            dispatchData.serviceIds.splice(dispatchData.serviceIds.findIndex((serviceId: string) => serviceId === serviceToRemove.id));
-            dispatchData.services.splice(dispatchData.services.findIndex((service: IDispatchService) =>
-                service.name === configuration.skillId));
-            writeFileSync(dispatchFilePath, JSON.stringify(dispatchData, undefined, 4));
+        if (!serviceToRemove) {
+            logger.warning(`The skill ${configuration.skillId} is not present in the Dispatch model.
+Run 'botskills list --assistantSkills "<YOUR-ASSISTANT-SKILLS-FILE-PATH>"' in order to list all the skills connected to your assistant`);
+
+            return false;
         }
+        dispatchData.serviceIds.splice(dispatchData.serviceIds.findIndex((serviceId: string) => serviceId === serviceToRemove.id));
+        dispatchData.services.splice(dispatchData.services.findIndex((service: IDispatchService) =>
+            service.name === configuration.skillId));
+        writeFileSync(dispatchFilePath, JSON.stringify(dispatchData, undefined, 4));
 
         logger.message('Running Dispatch refresh');
 
@@ -70,15 +75,25 @@ async function updateDispatch(configuration: IDisconnectConfiguration): Promise<
         luisgenCommand.push(...[`-${configuration.lgLanguage}`, '"DispatchLuis"']);
         luisgenCommand.push(...['-o', configuration.lgOutFolder]);
         await runCommand(luisgenCommand, `Executing luisgen for the ${configuration.dispatchName} file`);
+
+        return true;
     } catch (err) {
         throw new Error(`An error ocurred while updating the Dispatch model:\n${err}`);
     }
 }
 
-export async function disconnectSkill(configuration: IDisconnectConfiguration): Promise<void> {
+export async function disconnectSkill(configuration: IDisconnectConfiguration): Promise<boolean> {
     try {
         if (configuration.logger) {
             logger = configuration.logger;
+        }
+
+        // Validate configuration.skillsFile
+        if (!existsSync(configuration.skillsFile)) {
+            logger.error(`The 'skillsFile' argument is absent or leads to a non-existing file.
+Please make sure to provide a valid path to your Assistant Skills configuration file.`);
+
+            return false;
         }
 
         // Take VA Skills configurations
@@ -94,9 +109,13 @@ export async function disconnectSkill(configuration: IDisconnectConfiguration): 
         if (!skillToRemove) {
             logger.warning(`The skill '${configuration.skillId}' is not present in the assistant Skills configuration file.
 Run 'botskills list --assistantSkills "<YOUR-ASSISTANT-SKILLS-FILE-PATH>"' in order to list all the skills connected to your assistant`);
-            process.exit(1);
+
+            return false;
         } else {
-            await updateDispatch(configuration);
+            if (!await updateDispatch(configuration)) {
+
+                return false;
+            }
             // Removing the skill manifest from the assistant skills array
             logger.warning(`Removing the '${configuration.skillId}' skill from your assistant's skills configuration file.`);
             assistantSkills.splice(assistantSkills.indexOf(skillToRemove), 1);
@@ -107,9 +126,13 @@ Run 'botskills list --assistantSkills "<YOUR-ASSISTANT-SKILLS-FILE-PATH>"' in or
             // Writing (and overriding) the assistant skills file
             writeFileSync(configuration.skillsFile, JSON.stringify(assistantSkillsFile, undefined, 4));
             logger.success(`Successfully removed '${configuration.skillId}' skill from your assistant's skills configuration file.`);
+
+            return true;
         }
     } catch (err) {
         logger.error(`There was an error while disconnecting the Skill ${configuration.skillId} from the Assistant:\n${err}`);
+
+        return false;
     }
 }
 
