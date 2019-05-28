f3a7344f7421f849c6791d764a015c8f826ccb65
Martin Battaglino <martinbatta32@gmail.com>
2019-05-28 15:09:08 -0300

    [BotBuilder Solutions][TypeScript] Implement tests (#1436)
    * Replace dateformat with dayjs library, update npm shrinkwrap
    
    * Add testResponses in helpers
    
    * Update locales
    
    * Fixes in extensions and utility
    
    * Implement speechUtilityTest
    
    * Implement responseGenerationTest
    
    * Add someComplexType class, implement listExtensionsTest
    
    * Add dateTimeTestData class, create customizeLocale (zh), implement dateTimeExtensionsTest

--- lib/typescript/botbuilder-solutions/src/extensions/dateTimeExtensions.ts
+++ lib/typescript/botbuilder-solutions/src/extensions/dateTimeExtensions.ts
@@ -3,43 +3,64 @@
  * Licensed under the MIT License.
  */
 
-import * as dateFormat from 'dateformat';
+import * as dayjs from 'dayjs';
+// tslint:disable
+import 'dayjs/locale/de';
+import 'dayjs/locale/es';
+import 'dayjs/locale/it';
+import 'dayjs/locale/fr';
+import '../resources/customizeLocale/zh';
 import i18next from 'i18next';
+// tslint:enable
 
 export namespace DateTimeExtensions {
-    export function toSpeechDateString(date: Date, includePrefix: boolean = false): string {
-        const utcDate: Date = new Date(date.toUTCString());
 
-        if (date.toDateString() === utcDate.toDateString()) {
+    export function toSpeechDateString(date: Date, includePrefix: boolean = false): string {
+        const locale: string = i18next.language;
+        const utcDate: Date = new Date();
+        utcDate.setHours(date.getHours());
+        utcDate.setMinutes(date.getMinutes());
+        utcDate.setSeconds(date.getSeconds());
+        if (date.toUTCString() === utcDate.toUTCString()) {
             return i18next.t('common:today');
         }
 
-        const nextDate: Date = new Date(date.toString());
-        nextDate.setDate(date.getDate() + 1);
-
-        if (nextDate.toDateString() === utcDate.toDateString()) {
+        const nextDate: Date = new Date();
+        nextDate.setDate(nextDate.getDate() + 1);
+        nextDate.setHours(date.getHours());
+        nextDate.setMinutes(date.getMinutes());
+        nextDate.setSeconds(date.getSeconds());
+        if (date.toUTCString() === nextDate.toUTCString()) {
             return i18next.t('common:tomorrow');
         }
 
         const prefix: string = i18next.t('common:spokenDatePrefix');
         if (includePrefix && prefix) {
-            return `${prefix} ${dateFormat(date, i18next.t('common:spokenDateFormat'))}`;
+            return `${prefix} ${dayjs(date)
+                .locale(locale)
+                .format(
+                    i18next.t('common:spokenDateFormat')
+                )}`;
         }
 
-        return dateFormat(date, i18next.t('common:spokenDateFormat'));
+        return dayjs(date)
+            .locale(locale)
+            .format(
+                i18next.t('common:spokenDateFormat')
+            );
     }
 
     export function toSpeechTimeString(date: Date, includePrefix: boolean = false): string {
         if (includePrefix) {
             const prefix: string = date.getHours() === 1
-                ? i18next.t('common:spokenTimePrefix_One')
-                : i18next.t('common:spokenTimePrefix_MoreThanOne');
+                ? i18next.t('common:spokenTimePrefixOne')
+                : i18next.t('common:spokenTimePrefixMoreThanOne');
 
             if (prefix) {
-                return `${prefix} ${date.toTimeString()}`;
+                return `${prefix} ${date.toLocaleTimeString()}`;
             }
         }
 
-        return date.toTimeString();
+        return date.toLocaleTimeString();
     }
 }
