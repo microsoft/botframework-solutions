d5305b0af140ca1d0679979cf51dd87e5596bdc3
Ted <lzc850612@gmail.com>
2019-06-13 12:47:17 -0700

    [Skills] Add error handling for skills websocket transport (#1518)
    * complete failure handling for websocket transport
    
    * remove redundant code
    
    * fix bug
    
    * fix indentation

---
+++ lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills/SkillWebSocketCallbackException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Bot.Builder.Skills
+{
+	public class SkillWebSocketCallbackException : Exception
+	{
+		public SkillWebSocketCallbackException(string message, Exception ex)
+			: base(message, ex)
+		{
+		}
+	}
+}
