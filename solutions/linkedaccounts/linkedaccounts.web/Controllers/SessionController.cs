9b811f97af735986c637515cff6b45d4962a33ee
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-07-03 13:49:28 -0700

    [Linked Accounts] Fix sign-in/sign-out flow (#1723)
    * Clean up appsettings values to fix builds
    
    * Resolved some simple formatting errors across the project
    Fixed sign-out & sign-in by passing AzureAD as asp-area. Web app will now ask user to sign-in and can sign-out successfully.
    
    * Update linked accounts doc with app settings comments
    
    * Update Microsoft.Identity.Web with latest references

--- solutions/linkedaccounts/linkedaccounts.web/Controllers/SessionController.cs
+++ solutions/linkedaccounts/linkedaccounts.web/Controllers/SessionController.cs
@@ -16,13 +16,13 @@ namespace LinkedAccounts.Web.Controllers
     public class SessionController : Controller
     {
         public static Dictionary<string, string> Sessions = new Dictionary<string, string>();
-        
+
         // POST api/values
         [HttpPost]
         public void Post([FromQuery]string id)
         {
             // Passed the SessionID from the View which we store against the UserId for later use.
-            var userId = UserId.GetUserId(HttpContext, this.User);
+            var userId = UserId.GetUserId(this.HttpContext, this.User);
             Sessions[userId] = id;
         }
     }
