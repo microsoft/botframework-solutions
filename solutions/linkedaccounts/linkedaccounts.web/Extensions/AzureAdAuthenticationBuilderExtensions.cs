d69c92d45bfe09185839c3851bc11c0b0c913271
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-07-03 13:49:28 -0700

    [Linked Accounts] Fix sign-in/sign-out flow (#1723)
    * Clean up appsettings values to fix builds
    
    * Resolved some simple formatting errors across the project
    Fixed sign-out & sign-in by passing AzureAD as asp-area. Web app will now ask user to sign-in and can sign-out successfully.
    
    * Update linked accounts doc with app settings comments
    
    * Update Microsoft.Identity.Web with latest references

--- solutions/linkedaccounts/linkedaccounts.web/Extensions/AzureAdAuthenticationBuilderExtensions.cs
+++ solutions/linkedaccounts/linkedaccounts.web/Extensions/AzureAdAuthenticationBuilderExtensions.cs
@@ -7,8 +7,9 @@ namespace Microsoft.AspNetCore.Authentication
     using Microsoft.AspNetCore.Authentication.OpenIdConnect;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Options;
+
     public static class AzureAdAuthenticationBuilderExtensions
-    {        
+    {
         public static AuthenticationBuilder AddAzureAd(this AuthenticationBuilder builder)
             => builder.AddAzureAd(_ => { });
 
@@ -20,27 +21,27 @@ namespace Microsoft.AspNetCore.Authentication
             return builder;
         }
 
-        private class ConfigureAzureOptions: IConfigureNamedOptions<OpenIdConnectOptions>
+        private class ConfigureAzureOptions : IConfigureNamedOptions<OpenIdConnectOptions>
         {
             private readonly AzureAdOptions _azureOptions;
 
             public ConfigureAzureOptions(IOptions<AzureAdOptions> azureOptions)
             {
-                _azureOptions = azureOptions.Value;
+                this._azureOptions = azureOptions.Value;
             }
 
             public void Configure(string name, OpenIdConnectOptions options)
             {
-                options.ClientId = _azureOptions.ClientId;
-                options.Authority = $"{_azureOptions.Instance}{_azureOptions.TenantId}";
+                options.ClientId = this._azureOptions.ClientId;
+                options.Authority = $"{this._azureOptions.Instance}{this._azureOptions.TenantId}";
                 options.UseTokenLifetime = true;
-                options.CallbackPath = _azureOptions.CallbackPath;
+                options.CallbackPath = this._azureOptions.CallbackPath;
                 options.RequireHttpsMetadata = false;
             }
 
             public void Configure(OpenIdConnectOptions options)
             {
-                Configure(Options.DefaultName, options);
+                this.Configure(Options.DefaultName, options);
             }
         }
     }
