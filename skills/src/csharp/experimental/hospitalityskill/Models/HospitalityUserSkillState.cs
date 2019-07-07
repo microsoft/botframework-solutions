36bd05fdfff331dde8933507a4d483d6eeb58ae5
Luna ItoFisher <t-luitof@microsoft.com>
2019-07-07 16:05:26 -0700

    add hospitality skill files

---
+++ skills/src/csharp/experimental/hospitalityskill/Models/HospitalityUserSkillState.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Luis;
+
+namespace HospitalitySkill.Models
+{
+    public class HospitalityUserSkillState
+    {
+        public HospitalityUserSkillState()
+        {
+            CheckedOut = false;
+        }
+
+        public bool CheckedOut { get; set; }
+
+        public void Clear()
+        {
+        }
+    }
+}
---
+++ skills/src/csharp/experimental/hospitalityskill/Responses/Reservation/ReservationResponses.cs
@@ -0,0 +1,16 @@
+﻿// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder.Solutions.Responses;
+
+namespace HospitalitySkill.Responses.Reservation
+{
+
+    public class ReservationResponses : IResponseIdCollection
+    {
+        // Generated accessors
+        public const string ConfirmCheckOut = "ConfirmCheckOut";
+        public const string RetryConfirmCheckOut = "RetryConfirmCheckOut";
+    }
+}
