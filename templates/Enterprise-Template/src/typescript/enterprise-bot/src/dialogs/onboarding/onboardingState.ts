9dc0d3ea2df553d6046ce4e45e57513f2ecad021
Darren Jefford <33519520+darrenj@users.noreply.github.com>
2019-07-16 23:30:18 +0100

    Remove typescript version of Enterprise Template (#1868)
    * remove JS Enterprise Template
    
    * update pointer

--- templates/Enterprise-Template/src/typescript/enterprise-bot/src/dialogs/onboarding/onboardingState.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License
-
-export interface IOnboardingState {
-    name: string;
-    email: string;
-    location: string;
-}
