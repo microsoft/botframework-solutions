e718581efcdaaf65d2fe9723ef4e9d088a802692
lauren-mills <lamil@microsoft.com>
2019-07-23 14:20:45 -0700

    Remove autosave state middleware (#1929)
    * updated va ibot implementation
    
    * updated skill template ibot implementation
    
    * updated skills ibot implementations
    
    * removed autosavestatemiddleware from va
    
    * removed autosavestate middleware from skill template
    
    * removed autosavestate from skills

--- templates/Skill-Template/csharp/Sample/SkillSample/Adapters/DefaultAdapter.cs
+++ templates/Skill-Template/csharp/Sample/SkillSample/Adapters/DefaultAdapter.cs
@@ -19,7 +19,6 @@ namespace SkillSample.Bots
         public DefaultAdapter(
             BotSettings settings,
             ICredentialProvider credentialProvider,
-            BotStateSet botStateSet,
             IBotTelemetryClient telemetryClient,
             ResponseManager responseManager)
             : base(credentialProvider)
@@ -39,7 +38,6 @@ namespace SkillSample.Bots
             Use(new ShowTypingMiddleware());
             Use(new SetLocaleMiddleware(settings.DefaultLocale ?? "en-us"));
             Use(new EventDebuggerMiddleware());
-            Use(new AutoSaveStateMiddleware(botStateSet));
         }
     }
 }
