4ee337fc24ca7ee0f04df6dc6d11725521dc0b01
Martin Battaglino <martinbatta32@gmail.com>
2019-07-24 11:19:23 -0300

    Add a comment for the normalize util

--- tools/botskills/test/helpers/normalizeUtils.js
+++ tools/botskills/test/helpers/normalizeUtils.js
@@ -3,6 +3,9 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * Normalize the line endings of the content received
+ */
 function normalizeContent(content) {
     return content.replace(/\r\n/gm, "\n") //normalize
                   .replace(/\n/gm, "\r\n"); //CR+LF - Windows EOL;
