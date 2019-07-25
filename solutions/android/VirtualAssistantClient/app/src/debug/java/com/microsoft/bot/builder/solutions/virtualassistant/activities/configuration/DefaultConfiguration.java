03b4e209c1b5ce886148fe101943c4c2484274e6
Alexander Biemann <abiemann@hotmail.com>
2019-07-25 10:20:56 -0700

    update default config

--- solutions/android/VirtualAssistantClient/app/src/debug/java/com/microsoft/bot/builder/solutions/virtualassistant/activities/configuration/DefaultConfiguration.java
+++ solutions/android/VirtualAssistantClient/app/src/debug/java/com/microsoft/bot/builder/solutions/virtualassistant/activities/configuration/DefaultConfiguration.java
@@ -3,19 +3,16 @@ package com.microsoft.bot.builder.solutions.virtualassistant.activities.configur
 public class DefaultConfiguration {
 
     // Replace below with your own subscription key
-    public static final String SPEECH_SERVICE_SUBSCRIPTION_KEY = "ccbc14f1bb2a476187a831c1dcc6a3fc";
+    public static final String SPEECH_SERVICE_SUBSCRIPTION_KEY = "YOUR_KEY_HERE";//TODO
 
-    // Ryan's latest Bot : calendar, email, to-do, and point of interest skills
-    public static final String DIRECT_LINE_SPEECH_SECRET_KEY = "aeRqQEhGPaM.cwA.TkU.3pFGkwEK7X8m4IxFujVOhURSByvHEuVVi1Na_u8yBHI";//aeRqQEhGPaM.cwA.TkU.3pFGkwEK7X8m4IxFujVOhURSByvHEuVVi1Na_u8yBHI
+    public static final String DIRECT_LINE_SPEECH_SECRET_KEY = "YOUR_DIRECTLINE_SPEECH_KEY_HERE";//TODO
 
-    // Replace below with your own service region (e.g., "westus").
-    public static final String SPEECH_SERVICE_SUBSCRIPTION_KEY_REGION = "westus2";
+    public static final String SPEECH_SERVICE_SUBSCRIPTION_KEY_REGION = "westus2";//TODO
 
     public static final String USER_NAME = "User";
-    public static final String USER_FROM_ID = "625d6da6-1862-452b-b0ba-f2fa1ea5a7c5";
+    public static final String USER_FROM_ID = "YOUR_USER_FROM_ID_HERE";//TODO
 
     public static final String LOCALE = "en-us";
 
     // please note that the default colors are read from /res/values/colors.xml
-
 }
