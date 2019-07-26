ecdab1f22d01d15e8f8f4b205f4ecde951f53072
Ted <tedlee@microsoft.com>
2019-07-25 19:57:02 -0700

    enterprise notification sample (#1954)
    * add notification sample
    
    * add architecture image and improve the doc
    
    * Documentation updates and testing
    
    * add proactive implementation into VirtualAssistant of the sample
    
    * updates
    
    * add cosmosdbproactive node
    
    * update
    
    * update
    
    * update skills package version
    
    * update

---
+++ samples/EnterpriseNotification/VirtualAssistant/Proactive/ProactiveStateMiddleware.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VirtualAssistant.Proactive
+{
+    /// <summary>
+    /// A Middleware for saving the proactive model data
+    /// This middleware will refresh user's latest conversation reference and save it to state.
+    /// </summary>
+    public class ProactiveStateMiddleware : IMiddleware
+    {
+        private ProactiveState _proactiveState;
+        private IStatePropertyAccessor<ProactiveModel> _proactiveStateAccessor;
+
+        public ProactiveStateMiddleware(ProactiveState proactiveState)
+        {
+            _proactiveState = proactiveState;
+            _proactiveStateAccessor = _proactiveState.CreateProperty<ProactiveModel>(nameof(ProactiveModel));
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var activity = turnContext.Activity;
+
+            if (activity.From.Properties["role"].ToString().Equals("user", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var proactiveState = await _proactiveStateAccessor.GetAsync(turnContext, () => new ProactiveModel()).ConfigureAwait(false);
+                var conversationReference = turnContext.Activity.GetConversationReference();
+                proactiveState.Conversation = conversationReference;
+                await _proactiveStateAccessor.SetAsync(turnContext, proactiveState).ConfigureAwait(false);
+                await _proactiveState.SaveChangesAsync(turnContext).ConfigureAwait(false);
+            }
+
+            await next(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
