a412d157a2bdc81e075c760a90146176ce6db581
lzc850612 <lzc850612@gmail.com>
2019-08-06 14:06:45 -0700

    remove issuer validation and provide way to override openid metadata

--- lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills/Auth/MsJWTAuthenticationProvider.cs
+++ lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills/Auth/MsJWTAuthenticationProvider.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Threading;
 using Microsoft.IdentityModel.Protocols;
@@ -10,25 +9,20 @@ namespace Microsoft.Bot.Builder.Skills.Auth
 {
     public class MsJWTAuthenticationProvider : IAuthenticationProvider
     {
-        private const string OpenIdMetadataUrl = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration";
-
         private static OpenIdConnectConfiguration openIdConfig;
         private readonly string _microsoftAppId;
-        private List<string> _jwtIssuers = new List<string>
-        {
-            "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/v2.0",
-            "https://sts.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47/",
-        };
+        private readonly string _openIdMetadataUrl = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration";
 
-        static MsJWTAuthenticationProvider()
-        {
-            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(OpenIdMetadataUrl, new OpenIdConnectConfigurationRetriever());
-            openIdConfig = configurationManager.GetConfigurationAsync(CancellationToken.None).GetAwaiter().GetResult();
-        }
-
-        public MsJWTAuthenticationProvider(string microsoftAppId)
+        public MsJWTAuthenticationProvider(string microsoftAppId, string openIdMetadataUrl = null)
         {
             _microsoftAppId = !string.IsNullOrWhiteSpace(microsoftAppId) ? microsoftAppId : throw new ArgumentNullException(nameof(microsoftAppId));
+            if (!string.IsNullOrWhiteSpace(openIdMetadataUrl))
+            {
+                _openIdMetadataUrl = openIdMetadataUrl;
+            }
+
+            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(_openIdMetadataUrl, new OpenIdConnectConfigurationRetriever());
+            openIdConfig = configurationManager.GetConfigurationAsync(CancellationToken.None).GetAwaiter().GetResult();
         }
 
         public bool Authenticate(string authHeader)
@@ -38,7 +32,6 @@ namespace Microsoft.Bot.Builder.Skills.Auth
                 var validationParameters =
                     new TokenValidationParameters
                     {
-                        ValidIssuers = _jwtIssuers,
                         ValidAudiences = new[] { _microsoftAppId },
                         IssuerSigningKeys = openIdConfig.SigningKeys,
                     };
