f7ceefda42da7be3c2ab9ac75a0ea438385e113d
Ted <tedlee@microsoft.com>
2019-08-06 14:35:10 -0700

    enable JWTToken auth by default (#2022)
    * enable JWTToken auth by default
    
    * remove usage of skillsetting
    
    * fix build error
    
    * fix build error
    
    * use msappid from botsettings
    
    * remove issuer validation and provide way to override openid metadata

---
+++ lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills/Models/SkillSettings.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Skills.Models
+{
+    // Temporary class to represent settings for skill
+    // Should be part of Manifest
+    public class SkillSettings
+    {
+        [JsonProperty("Skill")]
+        public SkillConfiguration SkillConfig { get; set; }
+
+        public class SkillConfiguration
+        {
+            public bool IsMsJWTAuthenticationEnabled { get; set; }
+        }
+    }
+}
