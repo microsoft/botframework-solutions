da8ee4c537880b609ec932963d39b180ae6c467e
Ted <tedlee@microsoft.com>
2019-08-07 16:57:02 -0700

    add whitelist authentication (#2072)

--- lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills.Tests/Mocks/MockServiceClientCredentials.cs
+++ lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills.Tests/Mocks/MockServiceClientCredentials.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Skills.Auth;
@@ -7,12 +8,14 @@ namespace Microsoft.Bot.Builder.Skills.Tests.Mocks
 {
     public class MockServiceClientCredentials : IServiceClientCredentials
     {
-		public Task<string> GetTokenAsync(bool forceRefresh = false)
-		{
-			return Task.FromResult(string.Empty);
-		}
+        public string MicrosoftAppId { get; set; } = Guid.NewGuid().ToString();
 
-		public Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        public Task<string> GetTokenAsync(bool forceRefresh = false)
+        {
+            return Task.FromResult(string.Empty);
+        }
+
+        public Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
