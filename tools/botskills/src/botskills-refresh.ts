101e0ff506cc51aafa30a822b51d437b61900e92
Martin Battaglino <martinbatta32@gmail.com>
2019-08-15 17:01:11 -0300

    Modularize methods and implement multilanguage in refresh

--- tools/botskills/src/botskills-refresh.ts
+++ tools/botskills/src/botskills-refresh.ts
@@ -4,7 +4,6 @@
  */
 
 import * as program from 'commander';
-import { readFileSync } from 'fs';
 import { join, resolve } from 'path';
 import { RefreshSkill } from './functionality';
 import { ConsoleLogger, ILogger} from './logger';
@@ -12,7 +11,7 @@ import { ICognitiveModel, IRefreshConfiguration } from './models';
 import { sanitizePath, validatePairOfArgs } from './utils';
 
 function showErrorHelp(): void {
-    program.outputHelp((str: string) => {
+    program.outputHelp((str: string): string => {
         logger.error(str);
 
         return '';
@@ -27,20 +26,18 @@ program.Command.prototype.unknownOption = (flag: string): void => {
     showErrorHelp();
 };
 
-// tslint:disable: max-line-length
 program
     .name('botskills refresh')
     .description('Refresh the connected skills.')
     .option('--cs', 'Determine your assistant project structure to be a CSharp-like structure')
     .option('--ts', 'Determine your assistant project structure to be a TypeScript-like structure')
-    .option('--dispatchName [name]', '[OPTIONAL] Name of your assistant\'s \'.dispatch\' file (defaults to the name displayed in your Cognitive Models file)')
-    .option('--language [language]', '[OPTIONAL] Locale used for LUIS culture (defaults to \'en-us\')')
+    .option('--luisFolder [path]', '[OPTIONAL] Path to the folder containing your Skills\' .lu files (defaults to \'./deployment/resources/skills/en\' inside your assistant folder)')
     .option('--dispatchFolder [path]', '[OPTIONAL] Path to the folder containing your assistant\'s \'.dispatch\' file (defaults to \'./deployment/resources/dispatch/en\' inside your assistant folder)')
     .option('--outFolder [path]', '[OPTIONAL] Path for any output file that may be generated (defaults to your assistant\'s root folder)')
     .option('--lgOutFolder [path]', '[OPTIONAL] Path for the LuisGen output (defaults to a \'service\' folder inside your assistant\'s folder)')
     .option('--cognitiveModelsFile [path]', '[OPTIONAL] Path to your Cognitive Models file (defaults to \'cognitivemodels.json\' inside your assistant\'s folder)')
     .option('--verbose', '[OPTIONAL] Output detailed information about the processing of the tool')
-    .action((cmd: program.Command, actions: program.Command) => undefined);
+    .action((cmd: program.Command, actions: program.Command): undefined => undefined);
 
 const args: program.Command = program.parse(process.argv);
 
@@ -49,9 +46,7 @@ if (process.argv.length < 3) {
     process.exit(0);
 }
 
-let dispatchName: string;
 let dispatchFolder: string;
-let language: string;
 let lgLanguage: string;
 let outFolder: string;
 let lgOutFolder: string;
@@ -65,46 +60,30 @@ const csAndTsValidationResult: string = validatePairOfArgs(args.cs, args.ts);
 if (csAndTsValidationResult) {
     logger.error(
         csAndTsValidationResult.replace('{0}', 'cs')
-        .replace('{1}', 'ts')
+            .replace('{1}', 'ts')
     );
     process.exit(1);
 }
 
 lgLanguage = args.cs ? 'cs' : 'ts';
 
-// language validation
-language = args.language || 'en-us';
-const languageCode: string = (language.split('-'))[0];
-
-// outFolder validation -- the var is needed for reassuring 'configuration.outFolder' is not undefined
+// outFolder validation -- the const is needed for reassuring 'configuration.outFolder' is not undefined
 outFolder = args.outFolder ? sanitizePath(args.outFolder) : resolve('./');
 
 // cognitiveModelsFile validation
-const cognitiveModelsFilePath: string = args.cognitiveModelsFile || join(outFolder, (args.ts ? join('src', 'cognitivemodels.json') : 'cognitivemodels.json'));
-cognitiveModelsFile = cognitiveModelsFilePath;
+cognitiveModelsFile = args.cognitiveModelsFile || join(outFolder, (args.ts ? join('src', 'cognitivemodels.json') : 'cognitivemodels.json'));
 
 // dispatchFolder validation
-dispatchFolder = args.dispatchFolder ? sanitizePath(args.dispatchFolder) : join(outFolder, 'Deployment', 'Resources', 'Dispatch', languageCode);
+dispatchFolder = args.dispatchFolder ? sanitizePath(args.dispatchFolder) : join(outFolder, 'Deployment', 'Resources', 'Dispatch');
 
 // lgOutFolder validation
 lgOutFolder = args.lgOutFolder ? sanitizePath(args.lgOutFolder) : join(outFolder, (args.ts ? join('src', 'Services') : 'Services'));
-
-// dispatchName validation
-if (!args.dispatchName) {
-    // try get the dispatch name from the cognitiveModels file
-    const cognitiveModels: ICognitiveModel = JSON.parse(readFileSync(cognitiveModelsFilePath, 'UTF8'));
-    dispatchName = cognitiveModels.cognitiveModels[languageCode].dispatchModel.name;
-} else {
-    dispatchName = args.dispatchName;
-}
 
 // End of arguments validation
 
 // Initialize an instance of IRefreshConfiguration to send the needed arguments to the refreshskill function
 const configuration: IRefreshConfiguration = {
-    dispatchName: dispatchName,
     dispatchFolder: dispatchFolder,
-    language: language,
     lgLanguage: lgLanguage,
     outFolder: outFolder,
     lgOutFolder: lgOutFolder,
@@ -112,4 +91,4 @@ const configuration: IRefreshConfiguration = {
     logger: logger
 };
 
-new RefreshSkill(logger).refreshSkill(configuration);
+new RefreshSkill(<IRefreshConfiguration> configuration, logger).refreshSkill();
--- tools/botskills/src/functionality/refreshSkill.ts
+++ tools/botskills/src/functionality/refreshSkill.ts
@@ -2,21 +2,19 @@
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
-import { existsSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
 import { ConsoleLogger, ILogger } from '../logger';
-import { IRefreshConfiguration } from '../models';
-import { ChildProcessUtils, wrapPathWithQuotes } from '../utils';
+import { ICognitiveModelFile, IRefreshConfiguration } from '../models';
+import { ChildProcessUtils, getDispatchNames, wrapPathWithQuotes } from '../utils';
 
 export class RefreshSkill {
     public logger: ILogger;
-    private childProcessUtils: ChildProcessUtils;
-    private dispatchFile: string = '';
-    private dispatchJsonFile: string = '';
-    private dispatchFilePath: string = '';
-    private dispatchJsonFilePath: string = '';
+    private readonly childProcessUtils: ChildProcessUtils;
+    private readonly configuration: IRefreshConfiguration;
 
-    constructor(logger?: ILogger) {
+    public constructor(configuration: IRefreshConfiguration, logger?: ILogger) {
+        this.configuration = configuration;
         this.logger = logger || new ConsoleLogger();
         this.childProcessUtils = new ChildProcessUtils();
     }
@@ -25,7 +23,7 @@ export class RefreshSkill {
         this.logger.command(description, command.join(' '));
         const cmd: string = command[0];
         const commandArgs: string[] = command.slice(1)
-            .filter((arg: string) => arg);
+            .filter((arg: string): string => arg);
         try {
             return await this.childProcessUtils.execute(cmd, commandArgs);
         } catch (err) {
@@ -33,59 +31,106 @@ export class RefreshSkill {
         }
     }
 
-    private async updateDispatch(configuration: IRefreshConfiguration): Promise<void> {
+    private async executeDispatchRefresh(dispatchName: string, executionModelByCulture: Map<string, string>): Promise<void> {
         const dispatchRefreshCommand: string[] = ['dispatch', 'refresh'];
         try {
-            this.logger.message('Running dispatch refresh...');
-
-            dispatchRefreshCommand.push(...['--dispatch', this.dispatchFilePath]);
-            dispatchRefreshCommand.push(...['--dataFolder', configuration.dispatchFolder]);
-
+            this.logger.message(`Running dispatch refresh for ${dispatchName}...`);
+            // tslint:disable:no-backbone-get-set-outside-model
+            const dispatchJsonFile: string = <string> executionModelByCulture.get('dispatchJsonFile');
+            const dispatchJsonFilePath: string = <string> executionModelByCulture.get('dispatchJsonFilePath');
+            // tslint:enable:no-backbone-get-set-outside-model
+            const dispatchRefreshCommandArguments: string[] = ['--dispatch', '--dataFolder'];
+            dispatchRefreshCommandArguments.forEach((argument: string): void => {
+                const argumentValue: string = <string> executionModelByCulture.get(argument);
+                dispatchRefreshCommand.push(...[argument, argumentValue]);
+            });
             await this.runCommand(
                 dispatchRefreshCommand,
-                `Executing dispatch refresh for the ${configuration.dispatchName} file`);
+                `Executing dispatch refresh for the ${dispatchName} file`);
 
-            if (!existsSync(this.dispatchJsonFilePath)) {
+            if (!existsSync(dispatchJsonFilePath)) {
                 // tslint:disable-next-line: max-line-length
-                throw new Error(`Path to ${this.dispatchJsonFile} (${this.dispatchJsonFilePath}) leads to a nonexistent file. This may be due to a problem with the 'dispatch refresh' command.`);
+                throw new Error(`Path to ${dispatchJsonFile} (${dispatchJsonFilePath}) leads to a nonexistent file. This may be due to a problem with the 'dispatch refresh' command.`);
             }
         } catch (err) {
-            throw new Error(`There was an error in the dispatch refresh command:\nCommand:${dispatchRefreshCommand}\n${err}`);
+            throw new Error(`There was an error in the dispatch refresh command:\nCommand: ${dispatchRefreshCommand.join(' ')}\n${err}`);
         }
     }
 
-    private async runLuisGen(configuration: IRefreshConfiguration): Promise<void> {
+    private async executeLuisGen(dispatchName: string, executionModelByCulture: Map<string, string>): Promise<void> {
         const luisgenCommand: string[] = ['luisgen'];
         try {
-            this.logger.message('Running LuisGen...');
-
-            luisgenCommand.push(wrapPathWithQuotes(this.dispatchJsonFilePath));
-            luisgenCommand.push(...[`-${configuration.lgLanguage}`, `"DispatchLuis"`]);
-            luisgenCommand.push(...['-o', wrapPathWithQuotes(configuration.lgOutFolder)]);
-
-            await this.runCommand(luisgenCommand, `Executing luisgen for the ${configuration.dispatchName} file`);
+            this.logger.message(`Running LuisGen for ${dispatchName}...`);
+            const dispatchJsonFilePath: string = <string> executionModelByCulture.get('dispatchJsonFilePath');
+            const luisgenCommandArguments: string [] = [
+                wrapPathWithQuotes(dispatchJsonFilePath),
+                `-${this.configuration.lgLanguage}`,
+                '-o'
+            ];
+            luisgenCommandArguments.forEach((argument: string): void => {
+                const argumentValue: string = <string> executionModelByCulture.get(argument);
+                luisgenCommand.push(...[argument, argumentValue]);
+            });
+            this.logger.message(luisgenCommand.join(' '));
+            await this.runCommand(luisgenCommand, `Executing luisgen for the ${dispatchName} file`);
         } catch (err) {
             throw new Error(`There was an error in the luisgen command:\nCommand: ${luisgenCommand.join(' ')}\n${err}`);
         }
     }
 
-    public async refreshSkill(configuration: IRefreshConfiguration): Promise<boolean> {
-        try {
-            this.dispatchFile = `${configuration.dispatchName}.dispatch`;
-            this.dispatchJsonFile = `${configuration.dispatchName}.json`;
-            this.dispatchFilePath = join(configuration.dispatchFolder, this.dispatchFile);
-            this.dispatchJsonFilePath = join(configuration.dispatchFolder, this.dispatchJsonFile);
-
-            if (!existsSync(configuration.dispatchFolder)) {
-                throw new Error(`Path to the Dispatch folder (${configuration.dispatchFolder}) leads to a nonexistent folder.
+    private getExecutionModel(culture: string, dispatchName: string): Map<string, string> {
+        const dispatchFile: string = `${dispatchName}.dispatch`;
+        const dispatchJsonFile: string = `${dispatchName}.json`;
+        const dispatchFilePath: string = join(this.configuration.dispatchFolder, culture, dispatchFile);
+        const dispatchJsonFilePath: string = join(this.configuration.dispatchFolder, culture, dispatchJsonFile);
+        const dataFolder: string = join(this.configuration.dispatchFolder, culture);
+        if (!existsSync(this.configuration.dispatchFolder)) {
+            throw new Error(`Path to the Dispatch folder (${this.configuration.dispatchFolder}) leads to a nonexistent folder.
 Remember to use the argument '--dispatchFolder' for your Assistant's Dispatch folder.`);
-            } else if (!existsSync(this.dispatchFilePath)) {
-                throw new Error(`Path to the ${this.dispatchFile} file leads to a nonexistent file.
-Make sure to use the argument '--dispatchName' for your Assistant's Dispatch file name.`);
-            }
+        } else if (!existsSync(dispatchFilePath)) {
+            throw new Error(`Path to the ${dispatchFile} file leads to a nonexistent file.`);
+        }
 
-            await this.updateDispatch(configuration);
-            await this.runLuisGen(configuration);
+        // tslint:disable:no-backbone-get-set-outside-model
+        const executionModelMap: Map<string, string> = new Map();
+        executionModelMap.set('dispatchJsonFile', dispatchJsonFile);
+        executionModelMap.set('dispatchJsonFilePath', dispatchJsonFilePath);
+        executionModelMap.set('--dispatch', dispatchFilePath);
+        executionModelMap.set('--dataFolder', dataFolder);
+        executionModelMap.set(wrapPathWithQuotes(dispatchJsonFilePath), '');
+        executionModelMap.set(`-${this.configuration.lgLanguage}`, wrapPathWithQuotes('DispatchLuis'));
+        executionModelMap.set('-o', wrapPathWithQuotes(this.configuration.lgOutFolder));
+        // tslint:enable:no-backbone-get-set-outside-model
+
+        return executionModelMap;
+    }
+
+    private async updateModel(): Promise<void> {
+        if (!existsSync(this.configuration.cognitiveModelsFile)) {
+            throw new Error(`Could not find the cognitiveModels file (${
+                this.configuration.cognitiveModelsFile}). Please provide the '--cognitiveModelsFile' argument.`);
+        }
+        // eslint-disable-next-line @typescript-eslint/tslint/config
+        const cognitiveModelsFile: ICognitiveModelFile = JSON.parse(readFileSync(this.configuration.cognitiveModelsFile, 'UTF8'));
+        const dispatchNames: Map<string, string> = getDispatchNames(cognitiveModelsFile);
+        const executionsModelMap: Map<string, Map<string, string>> = new Map();
+        dispatchNames.forEach((dispatchName: string, culture: string): void => {
+            executionsModelMap.set(culture, this.getExecutionModel(culture, dispatchName));
+        });
+
+        await Promise.all(Array.from(executionsModelMap.entries())
+            .map(async (item: [string, Map<string, string>]): Promise<void> => {
+                const culture: string = item[0];
+                const executionModelByCulture: Map<string, string> = item[1];
+                const dispatchName: string = <string> dispatchNames.get(culture);
+                await this.executeDispatchRefresh(dispatchName, executionModelByCulture);
+                await this.executeLuisGen(dispatchName, executionModelByCulture);
+            }));
+    }
+
+    public async refreshSkill(): Promise<boolean> {
+        try {
+            await this.updateModel();
             this.logger.success('Successfully refreshed Dispatch model');
             this.logger.warning(
                 'You need to re-publish your Virtual Assistant in order to have these changes available for Azure based testing');
