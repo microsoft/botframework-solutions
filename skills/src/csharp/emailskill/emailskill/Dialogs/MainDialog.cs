f09da3f35ad34b064e4f7a182618de7fea8f5699
Qiong Wu <qiowu@microsoft.com>
2019-08-15 17:40:20 +0800

    fix score issue

--- skills/src/csharp/emailskill/emailskill/Dialogs/MainDialog.cs
+++ skills/src/csharp/emailskill/emailskill/Dialogs/MainDialog.cs
@@ -256,27 +256,30 @@ namespace EmailSkill.Dialogs
                 {
                     var luisResult = await luisService.RecognizeAsync<General>(dc.Context, cancellationToken);
                     state.GeneralLuisResult = luisResult;
-                    var topIntent = luisResult.TopIntent().intent;
+                    var topIntent = luisResult.TopIntent();
 
-                    switch (topIntent)
+                    if (topIntent.score > 0.5)
                     {
-                        case General.Intent.Cancel:
-                            {
-                                result = await OnCancel(dc);
-                                break;
-                            }
+                        switch (topIntent.intent)
+                        {
+                            case General.Intent.Cancel:
+                                {
+                                    result = await OnCancel(dc);
+                                    break;
+                                }
 
-                        case General.Intent.Help:
-                            {
-                                // result = await OnHelp(dc);
-                                break;
-                            }
+                            case General.Intent.Help:
+                                {
+                                    // result = await OnHelp(dc);
+                                    break;
+                                }
 
-                        case General.Intent.Logout:
-                            {
-                                result = await OnLogout(dc);
-                                break;
-                            }
+                            case General.Intent.Logout:
+                                {
+                                    result = await OnLogout(dc);
+                                    break;
+                                }
+                        }
                     }
                 }
             }
