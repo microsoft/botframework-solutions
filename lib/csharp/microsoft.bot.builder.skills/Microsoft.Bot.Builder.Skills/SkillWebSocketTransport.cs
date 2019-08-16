4b7bc5f45d3e9583f9e17386e51a5dc9835c7a78
ted Li <tedlee@microsoft.com>
2019-08-16 11:12:46 -0700

    add perf on SkillWebSocketTransport side

--- lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills/SkillWebSocketTransport.cs
+++ lib/csharp/microsoft.bot.builder.skills/Microsoft.Bot.Builder.Skills/SkillWebSocketTransport.cs
@@ -65,7 +65,22 @@ namespace Microsoft.Bot.Builder.Skills
             // set back recipient id to make things consistent
             activity.Recipient.Id = recipientId;
 
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
             await _streamingTransportClient.SendAsync(request);
+            stopWatch.Stop();
+
+            _botTelemetryClient.TrackEvent(
+                "SkillWebSocketTurnLatency",
+                new Dictionary<string, string>
+                {
+                    { "SkillName", skillManifest.Name },
+                    { "SkillEndpoint", skillManifest.Endpoint.ToString() },
+                },
+                new Dictionary<string, double>
+                {
+                    { "Latency", stopWatch.ElapsedMilliseconds },
+                });
 
             return endOfConversationActivity;
         }
