49203eefa29c5a46ce3fb4e35e50cb19189d6686
yuesu <yuesu@microsoft.com>
2019-08-16 12:22:15 +0800

    fix stylecop

--- lib/csharp/microsoft.bot.builder.solutions/microsoft.bot.builder.solutions/Testing/DefaultTestAdapter.cs
+++ lib/csharp/microsoft.bot.builder.solutions/microsoft.bot.builder.solutions/Testing/DefaultTestAdapter.cs
@@ -7,16 +7,16 @@ namespace Microsoft.Bot.Builder.Solutions.Testing
 {
     public class DefaultTestAdapter : TestAdapter
     {
-        private const string userAccountId = "user";
-        private const string userAccountName = "User";
-        private const string botAccountId = "bot";
-        private const string botAccountName = "Bot";
-        private const string conversationId = "conversation";
-        private const string conversationName = "Conversation";
-        private const string serviceUrl = "https://test.com";
-        private const string oauthConnection = "Azure Active Directory";
+        private const string UserAccountId = "user";
+        private const string UserAccountName = "User";
+        private const string BotAccountId = "bot";
+        private const string BotAccountName = "Bot";
+        private const string ConversationId = "conversation";
+        private const string ConversationName = "Conversation";
+        private const string ServiceUrl = "https://test.com";
+        private const string OauthConnection = "Azure Active Directory";
 
-        public DefaultTestAdapter(BotStateSet botStateSet, string connectionName = oauthConnection, string token = oauthConnection)
+        public DefaultTestAdapter(BotStateSet botStateSet, string connectionName = OauthConnection, string token = OauthConnection)
             : base(sendTraceActivity: false)
         {
             Use(new EventDebuggerMiddleware());
@@ -24,13 +24,13 @@ namespace Microsoft.Bot.Builder.Solutions.Testing
             this.Conversation = new ConversationReference
             {
                 ChannelId = Channels.Test,
-                ServiceUrl = serviceUrl,
-                User = new ChannelAccount(userAccountId, userAccountName),
-                Bot = new ChannelAccount(botAccountId, botAccountName),
-                Conversation = new ConversationAccount(false, conversationId, conversationName),
+                ServiceUrl = ServiceUrl,
+                User = new ChannelAccount(UserAccountId, UserAccountName),
+                Bot = new ChannelAccount(BotAccountId, BotAccountName),
+                Conversation = new ConversationAccount(false, ConversationId, ConversationName),
             };
 
-            this.AddUserToken(connectionName, Channels.Test, userAccountId, token);
+            this.AddUserToken(connectionName, Channels.Test, UserAccountId, token);
         }
     }
 }
