702819e81b35d8da29af38fee553c66d2ca6589d
Martin Battaglino <martinbatta32@gmail.com>
2019-09-05 12:47:55 -0300

    Separate initializacion, validation, assignment for refresh

--- tools/botskills/src/botskills-refresh.ts
+++ tools/botskills/src/botskills-refresh.ts
@@ -8,7 +8,7 @@ import { readFileSync } from 'fs';
 import { join, resolve } from 'path';
 import { RefreshSkill } from './functionality';
 import { ConsoleLogger, ILogger} from './logger';
-import { ICognitiveModelFile, IRefreshConfiguration } from './models';
+import { ICognitiveModel, IRefreshConfiguration } from './models';
 import { sanitizePath, validatePairOfArgs } from './utils';
 
 function showErrorHelp(): void {
@@ -35,7 +35,6 @@ program
     .option('--ts', 'Determine your assistant project structure to be a TypeScript-like structure')
     .option('--dispatchName [name]', '[OPTIONAL] Name of your assistant\'s \'.dispatch\' file (defaults to the name displayed in your Cognitive Models file)')
     .option('--language [language]', '[OPTIONAL] Locale used for LUIS culture (defaults to \'en-us\')')
-    .option('--luisFolder [path]', '[OPTIONAL] Path to the folder containing your Skills\' .lu files (defaults to \'./deployment/resources/skills/en\' inside your assistant folder)')
     .option('--dispatchFolder [path]', '[OPTIONAL] Path to the folder containing your assistant\'s \'.dispatch\' file (defaults to \'./deployment/resources/dispatch/en\' inside your assistant folder)')
     .option('--outFolder [path]', '[OPTIONAL] Path for any output file that may be generated (defaults to your assistant\'s root folder)')
     .option('--lgOutFolder [path]', '[OPTIONAL] Path for the LuisGen output (defaults to a \'service\' folder inside your assistant\'s folder)')
@@ -47,7 +46,16 @@ const args: program.Command = program.parse(process.argv);
 
 if (process.argv.length < 3) {
     program.help();
+    process.exit(0);
 }
+
+let dispatchName: string;
+let dispatchFolder: string;
+let language: string;
+let lgLanguage: string;
+let outFolder: string;
+let lgOutFolder: string;
+let cognitiveModelsFile: string;
 
 logger.isVerbose = args.verbose;
 
@@ -62,43 +70,46 @@ if (csAndTsValidationResult) {
     process.exit(1);
 }
 
-const projectLanguage: string = args.cs ? 'cs' : 'ts';
-// Initialize an instance of IConnectConfiguration to send the needed arguments to the connectSkill function
-const configuration: Partial<IRefreshConfiguration> = {
-    lgLanguage: projectLanguage
-};
+lgLanguage = args.cs ? 'cs' : 'ts';
 
 // language validation
-const language: string = args.language || 'en-us';
-configuration.language = language;
+language = args.language || 'en-us';
 const languageCode: string = (language.split('-'))[0];
 
 // outFolder validation -- the const is needed for reassuring 'configuration.outFolder' is not undefined
-const outFolder: string = args.outFolder ? sanitizePath(args.outFolder) : resolve('./');
-configuration.outFolder = outFolder;
+outFolder = args.outFolder ? sanitizePath(args.outFolder) : resolve('./');
 
 // cognitiveModelsFile validation
-const cognitiveModelsFilePath: string = args.cognitiveModelsFile || join(configuration.outFolder, (args.ts ? join('src', 'cognitivemodels.json') : 'cognitivemodels.json'));
-configuration.cognitiveModelsFile = cognitiveModelsFilePath;
-// luisFolder validation
-configuration.luisFolder = args.luisFolder ? sanitizePath(args.luisFolder) : join(configuration.outFolder, 'Deployment', 'Resources', 'Skills', languageCode);
+const cognitiveModelsFilePath: string = args.cognitiveModelsFile || join(outFolder, (args.ts ? join('src', 'cognitivemodels.json') : 'cognitivemodels.json'));
+cognitiveModelsFile = cognitiveModelsFilePath;
 
 // dispatchFolder validation
-configuration.dispatchFolder = args.dispatchFolder ? sanitizePath(args.dispatchFolder) : join(configuration.outFolder, 'Deployment', 'Resources', 'Dispatch', languageCode);
+dispatchFolder = args.dispatchFolder ? sanitizePath(args.dispatchFolder) : join(outFolder, 'Deployment', 'Resources', 'Dispatch', languageCode);
 
 // lgOutFolder validation
-configuration.lgOutFolder = args.lgOutFolder ? sanitizePath(args.lgOutFolder) : join(configuration.outFolder, (args.ts ? join('src', 'Services') : 'Services'));
+lgOutFolder = args.lgOutFolder ? sanitizePath(args.lgOutFolder) : join(outFolder, (args.ts ? join('src', 'Services') : 'Services'));
 
 // dispatchName validation
 if (!args.dispatchName) {
     // try get the dispatch name from the cognitiveModels file
-    const cognitiveModelsFile: ICognitiveModelFile = JSON.parse(readFileSync(cognitiveModelsFilePath, 'UTF8'));
-    configuration.dispatchName = cognitiveModelsFile.cognitiveModels[languageCode].dispatchModel.name;
+    const cognitiveModels: ICognitiveModel = JSON.parse(readFileSync(cognitiveModelsFilePath, 'UTF8'));
+    dispatchName = cognitiveModels.cognitiveModels[languageCode].dispatchModel.name;
 } else {
-    configuration.dispatchName = args.dispatchName;
+    dispatchName = args.dispatchName;
 }
 
-configuration.logger = logger;
 // End of arguments validation
 
-new RefreshSkill(logger).refreshSkill(<IRefreshConfiguration> configuration);
+// Initialize an instance of IRefreshConfiguration to send the needed arguments to the refreshskill function
+const configuration: IRefreshConfiguration = {
+    dispatchName: dispatchName,
+    dispatchFolder: dispatchFolder,
+    language: language,
+    lgLanguage: lgLanguage,
+    outFolder: outFolder,
+    lgOutFolder: lgOutFolder,
+    cognitiveModelsFile: cognitiveModelsFile,
+    logger: logger
+};
+
+new RefreshSkill(logger).refreshSkill(configuration);
