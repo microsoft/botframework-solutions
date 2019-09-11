c7c163fe06e4954c9c3cac16b3d253ff07dd2d7c
Lauren Mills <lamil@microsoft.com>
2019-09-11 12:58:57 -0700

    synced publish files between ts projects

--- templates/Virtual-Assistant-Template/typescript/samples/sample-skill/deployment/scripts/publish.ps1
+++ templates/Virtual-Assistant-Template/typescript/samples/sample-skill/deployment/scripts/publish.ps1
@@ -15,8 +15,6 @@ if (-not $name) {
 if (-not $resourceGroup) {
     $resourceGroup = Read-Host "? Bot Resource Group"
 }
-
-
 # Reset log file
 if (Test-Path $logFile) {
 	Clear-Content $logFile -Force | Out-Null
@@ -25,11 +23,11 @@ else {
 	New-Item -Path $logFile | Out-Null
 }
 
-# Check for existing deployment files
+# Check for existing deployment configuration
 if (-not (Test-Path (Join-Path $projFolder '.deployment'))) {
 
-	# Add needed deployment files for az
-	az bot prepare-deploy --code-dir $projFolder --lang Typescript --output json | Out-Null
+	# Add needed deployment configuration
+	Add-Content -Path $(Join-Path $projFolder ".deployment") -Value @("[config]", "SCM_DO_BUILD_DURING_DEPLOYMENT=true") -Encoding utf8
 }
 
 # Delete src zip, if it exists
@@ -37,9 +35,8 @@ $zipPath = $(Join-Path $projFolder 'code.zip')
 if (Test-Path $zipPath) {
 	Remove-Item $zipPath -Force | Out-Null
 }
-
 if($?) 
-{     
+{  
 	# Compress source code
 	Get-ChildItem -Path "$($projFolder)" -Exclude @("node_modules", "test", "deployment") | Compress-Archive -DestinationPath "$($zipPath)" -Force | Out-Null
 
@@ -49,10 +46,10 @@ if($?)
 		--resource-group $resourceGroup `
 		--name $name `
 		--src $zipPath `
-        --output json) 2>> $logFile | Out-Null
-}
- else
-{
+		--output json) 2>> $logFile | Out-Null
+} 
+else 
+{       
 	Write-Host "! Could not deploy automatically to Azure. Review the log for more information." -ForegroundColor DarkRed
 	Write-Host "! Log: $($logFile)" -ForegroundColor DarkRed    
 }       
