fd58e887bdf522544829044788c655fda92b4800
yuesu <yuesu@microsoft.com>
2019-09-11 12:02:47 +0800

    add OpenDefaultApp event model

---
+++ lib/csharp/microsoft.bot.builder.solutions/microsoft.bot.builder.solutions/Models/OpenDefaultApp.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Builder.Solutions.Models
+{
+    class OpenDefaultApp
+    {
+        public string MeetingUri { get; set; }
+
+        public string TelephoneUri { get; set; }
+    }
+}
