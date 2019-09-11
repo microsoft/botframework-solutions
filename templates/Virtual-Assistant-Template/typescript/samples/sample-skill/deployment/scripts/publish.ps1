7451c796f97932795584166c14d990112c25e8d6
lauren-mills <lamil@microsoft.com>
2019-09-11 14:45:59 -0700

    Updated error handling in publish.ps1 (#2354)
    * added feedback middleware
    
    * fixed skill luis telemetry bug
    
    * Added feedback middleware
    
    * updated to rc1
    
    * updated skill sample from skill template
    
    * updated template
    
    * regenerated va sample from template
    
    * added new sample
    
    * removed unneeded packages
    
    * updated publish script error handling (C#)
    
    * synced publish files between ts projects
    
    * updated error handling in ts projects
    
    * added nuget package srcs

--- templates/Virtual-Assistant-Template/typescript/samples/sample-skill/deployment/scripts/publish.ps1
+++ templates/Virtual-Assistant-Template/typescript/samples/sample-skill/deployment/scripts/publish.ps1
@@ -15,8 +15,6 @@ if (-not $name) {
 if (-not $resourceGroup) {
     $resourceGroup = Read-Host "? Bot Resource Group"
 }
-
-
 # Reset log file
 if (Test-Path $logFile) {
 	Clear-Content $logFile -Force | Out-Null
@@ -25,11 +23,11 @@ else {
 	New-Item -Path $logFile | Out-Null
 }
 
-# Check for existing deployment files
+# Check for existing deployment configuration
 if (-not (Test-Path (Join-Path $projFolder '.deployment'))) {
 
-	# Add needed deployment files for az
-	az bot prepare-deploy --code-dir $projFolder --lang Typescript --output json | Out-Null
+	# Add needed deployment configuration
+	Add-Content -Path $(Join-Path $projFolder ".deployment") -Value @("[config]", "SCM_DO_BUILD_DURING_DEPLOYMENT=true") -Encoding utf8
 }
 
 # Delete src zip, if it exists
@@ -38,21 +36,26 @@ if (Test-Path $zipPath) {
 	Remove-Item $zipPath -Force | Out-Null
 }
 
-if($?) 
-{     
-	# Compress source code
+if($?) {
+    # Compress source code
 	Get-ChildItem -Path "$($projFolder)" -Exclude @("node_modules", "test", "deployment") | Compress-Archive -DestinationPath "$($zipPath)" -Force | Out-Null
 
-	# Publish zip to Azure
-	Write-Host "> Publishing to Azure ..." -ForegroundColor Green
-	(az webapp deployment source config-zip `
-		--resource-group $resourceGroup `
-		--name $name `
-		--src $zipPath `
-        --output json) 2>> $logFile | Out-Null
+    # Publish zip to Azure
+    Write-Host "> Publishing to Azure ..." -ForegroundColor Green
+    Invoke-Expression "az webapp deployment source config-zip --resource-group $($resourceGroup) --name $($name) --src $($zipPath) --output json" -ErrorVariable publishError -OutVariable publishOutput 2>&1 | Out-Null
+    Add-Content $logFile $publishOutput
+    Add-Content $logFile $publishError
+
+    $err = $publishError | Where { $_.Exception.ErrorRecord -like "*ERROR*" }
+
+    if ($err)
+    {
+        Write-Host "! Could not deploy automatically to Azure. Review the log for more information." -ForegroundColor DarkRed
+        Write-Host "! Error: $($err.Exception.ErrorRecord)" -ForegroundColor DarkRed
+	    Write-Host "! Log: $($logFile)" -ForegroundColor DarkRed
+    }
 }
- else
-{
+else {
 	Write-Host "! Could not deploy automatically to Azure. Review the log for more information." -ForegroundColor DarkRed
-	Write-Host "! Log: $($logFile)" -ForegroundColor DarkRed    
-}       
+	Write-Host "! Log: $($logFile)" -ForegroundColor DarkRed
+}
