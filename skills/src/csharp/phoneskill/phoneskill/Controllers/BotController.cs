956861bf19029f6b05ecd038dbdd20a03fa23148
Lauren Mills <lamil@microsoft.com>
2019-09-13 15:42:45 -0700

    updated phone skill to latest packages

--- skills/src/csharp/phoneskill/phoneskill/Controllers/BotController.cs
+++ skills/src/csharp/phoneskill/phoneskill/Controllers/BotController.cs
@@ -5,20 +5,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.Skills;
+using Microsoft.Bot.Builder.Skills.Auth;
 using Microsoft.Bot.Builder.Solutions;
 
 namespace PhoneSkill.Controllers
 {
     [ApiController]
     public class BotController : SkillController
-	{
-		public BotController(
-			IBot bot,
-			BotSettingsBase botSettings,
-			IBotFrameworkHttpAdapter botFrameworkHttpAdapter,
-			SkillWebSocketAdapter skillWebSocketAdapter)
-			: base(bot, botSettings, botFrameworkHttpAdapter, skillWebSocketAdapter)
-		{
-		}
-	}
+    {
+        public BotController(
+            IBot bot,
+            BotSettingsBase botSettings,
+            IBotFrameworkHttpAdapter botFrameworkHttpAdapter,
+            SkillWebSocketAdapter skillWebSocketAdapter,
+            IWhitelistAuthenticationProvider whitelistAuthenticationProvider)
+            : base(bot, botSettings, botFrameworkHttpAdapter, skillWebSocketAdapter, whitelistAuthenticationProvider)
+        {
+        }
+    }
 }
