3149a7e1a9d34e8a88fc72902347db63cb3f5887
Martin Battaglino <martinbatta32@gmail.com>
2019-09-18 16:04:24 -0300

    Implement DefaultWebSocketAdapter to use BotFrameworkStreamingAdapter

---
+++ templates/Virtual-Assistant-Template/typescript/samples/sample-assistant/src/adapters/defaultWebSocketAdapter.ts
@@ -0,0 +1,48 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+    BotTelemetryClient,
+    MemoryTranscriptStore,
+    ShowTypingMiddleware,
+    TelemetryLoggerMiddleware,
+    TranscriptLoggerMiddleware,
+    TranscriptStore } from 'botbuilder';
+import { AzureBlobTranscriptStore } from 'botbuilder-azure';
+import { Dialog } from 'botbuilder-dialogs';
+import {
+    EventDebuggerMiddleware,
+    SetLocaleMiddleware,
+    SetSpeakMiddleware } from 'botbuilder-solutions';
+import { BotFrameworkStreamingAdapter } from 'botbuilder-streaming-extensions';
+import { DialogBot } from '../bots/dialogBot';
+import { IBotSettings } from '../services/botSettings';
+
+export class DefaultWebSocketAdapter extends BotFrameworkStreamingAdapter {
+
+    public constructor(
+        bot: DialogBot<Dialog>,
+        settings: Partial<IBotSettings>,
+        telemetryClient: BotTelemetryClient) {
+        super(bot);
+
+        if (settings.blobStorage === undefined) {
+            throw new Error('There is no blobStorage value in appsettings file');
+        }
+
+        const transcriptStore: TranscriptStore = new AzureBlobTranscriptStore({
+            containerName: settings.blobStorage.container,
+            storageAccountOrConnectionString: settings.blobStorage.connectionString
+        });
+
+        this.use(new TranscriptLoggerMiddleware(new MemoryTranscriptStore()));
+        this.use(new TranscriptLoggerMiddleware(transcriptStore));
+        this.use(new TelemetryLoggerMiddleware(telemetryClient, true));
+        this.use(new ShowTypingMiddleware());
+        this.use(new SetLocaleMiddleware(settings.defaultLocale || 'en-us'));
+        this.use(new EventDebuggerMiddleware());
+        this.use(new SetSpeakMiddleware(settings.defaultLocale || 'en-us'));
+    }
+}
