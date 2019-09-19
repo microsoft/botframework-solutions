bfbe7bcb1574f239df5f9e6e4519dc6822b52bd6
Gabo Gilabert <ggilaber@microsoft.com>
2019-09-18 22:26:21 -0700

    Code reorg: moved VABot tests into their own physcial folder

--- lib/csharp/libraries/Microsoft.Bot.Builder.Skills/SkillTransport.cs
+++ lib/csharp/libraries/Microsoft.Bot.Builder.Skills/SkillTransport.cs
@@ -7,6 +7,7 @@ using Microsoft.Bot.Schema;
 
 namespace Microsoft.Bot.Builder.Skills
 {
+    // TODO: so far this class is very similar to BotFrameworkSkillConnector or SkillConnector, do we need the two?
     public abstract class SkillTransport
     {
         public abstract Task<Activity> ForwardToSkillAsync(ITurnContext turnContext, Activity activity, CancellationToken cancellationToken);
