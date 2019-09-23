cf065ba1ea9f47d9f82c73788d4cfcd28e3fcc66
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-09-23 15:43:15 -0700

    [Maintenance] Realign production & experimental skill directories (#2420)
    * Move phone and automotive skills under experimental directory
    Rename restaurantbooking to restaurantbookingskill
    Move bingsearchskill up one directory to follow same format as other experimental skills
    
    * Fix restaurantbookingskill namespace errors
    
    * Fix additional usings in restaurantbookingskill

---
+++ skills/src/csharp/experimental/restaurantbookingskill/Models/FoodTypeInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantBookingSkill.Models
+{
+    public class FoodTypeInfo
+    {
+        public string TypeName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
