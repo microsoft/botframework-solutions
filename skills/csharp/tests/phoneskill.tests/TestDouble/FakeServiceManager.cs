500f3a3e47a526444a6760419e86b3a6a578e46a
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-09-26 11:43:49 -0700

    Refactor Skill directories (#2435)
    * Skills:
    Remove src directory
    Move all tests to /skills/csharp/tests
    Rename all test projects as {SkillName}Tests
    Move Phone & Automotive Skill projects at same level as other experimentals
    
    * Update test namespaces to {Skill}.Tests
    Update TestBases to {Skill}TestBase
    
    * Fix ruleset reference
    
    * Reorder usings
    
    * Fix tabe spacings
    
    * Rename tests directories to match project names

---
+++ skills/csharp/tests/phoneskill.tests/TestDouble/FakeServiceManager.cs
@@ -0,0 +1,21 @@
+﻿using PhoneSkill.Common;
+using PhoneSkill.Models;
+using PhoneSkill.Services;
+
+namespace PhoneSkill.Tests.TestDouble
+{
+    public class FakeServiceManager : IServiceManager
+    {
+        private IContactProvider contactProvider;
+
+        public FakeServiceManager()
+        {
+            contactProvider = new StubContactProvider();
+        }
+
+        public IContactProvider GetContactProvider(string token, ContactSource source)
+        {
+            return contactProvider;
+        }
+    }
+}
