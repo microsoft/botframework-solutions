4d8655af7dbe91d673dc9a24c1f9ba4844596918
Hualiang Xie <hualxie@microsoft.com>
2019-10-24 11:21:38 +0800

    [VA][Skill] fix event, remove BuildServiceProvider
    BuildServiceProvider and runtime one are not same one

--- templates/Skill-Template/csharp/Sample/SkillSample/Startup.cs
+++ templates/Skill-Template/csharp/Sample/SkillSample/Startup.cs
@@ -55,8 +55,6 @@ namespace SkillSample
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            var provider = services.BuildServiceProvider();
-
             // Load settings
             var settings = new BotSettings();
             Configuration.Bind(settings);
