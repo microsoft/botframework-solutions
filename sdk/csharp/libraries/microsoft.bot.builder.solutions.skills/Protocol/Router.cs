6e2f558b694b4ec9904326b5c3db392a1b1ae2d2
lauren-mills <lamil@microsoft.com>
2019-10-25 10:29:48 -0700

    updated to sdk 4.6.0-rc0 (#2593)

--- sdk/csharp/libraries/microsoft.bot.builder.solutions.skills/Protocol/Router.cs
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions.skills/Protocol/Router.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
-using Microsoft.Bot.StreamingExtensions;
+using Microsoft.Bot.Streaming;
 
 namespace Microsoft.Bot.Builder.Skills.Protocol
 {
--- sdk/csharp/libraries/microsoft.bot.builder.solutions.skills/SlotEqualityComparer.cs
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions.skills/SlotEqualityComparer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Microsoft.Bot.Builder.Skills.Models.Manifest;
 
 namespace Microsoft.Bot.Builder.Skills
