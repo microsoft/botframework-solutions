4db64d78783a17f053ba697b43b27efefa820f2a
Hualiang Xie <hualxie@microsoft.com>
2019-11-04 12:50:18 +0800

    [Sdk] add mit license header

--- sdk/csharp/libraries/microsoft.bot.builder.solutions.skills/Models/SkillEvents.cs
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions.skills/Models/SkillEvents.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Bot.Builder.Skills.Models
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Builder.Skills.Models
 {
     public class SkillEvents
     {
--- sdk/csharp/libraries/microsoft.bot.builder.solutions/Responses/LocaleTemplateEngineManager.cs
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions/Responses/LocaleTemplateEngineManager.cs
@@ -1,5 +1,5 @@
-﻿// Licensed under the MIT License.
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 using System;
 using System.Collections.Generic;
