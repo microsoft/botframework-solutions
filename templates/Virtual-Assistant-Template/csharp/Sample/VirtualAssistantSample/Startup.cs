2e1e74c302a36a40ac3aa8bbc6a3ef7b278a5117
lauren-mills <lamil@microsoft.com>
2019-11-04 16:31:52 -0800

    Update va to .net3.0

--- templates/Virtual-Assistant-Template/csharp/Sample/VirtualAssistantSample/Startup.cs
+++ templates/Virtual-Assistant-Template/csharp/Sample/VirtualAssistantSample/Startup.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.ApplicationInsights;
 using Microsoft.Bot.Builder.Azure;
@@ -23,6 +22,7 @@ using Microsoft.Bot.Builder.Solutions.Responses;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using VirtualAssistantSample.Adapters;
 using VirtualAssistantSample.Bots;
@@ -33,7 +33,7 @@ namespace VirtualAssistantSample
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public Startup(IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -53,7 +53,8 @@ namespace VirtualAssistantSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // Configure MVC
+            services.AddControllers();
 
             // Load settings
             var settings = new BotSettings();
@@ -134,7 +135,7 @@ namespace VirtualAssistantSample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -142,9 +143,16 @@ namespace VirtualAssistantSample
             }
 
             app.UseDefaultFiles()
+                .UseHttpsRedirection()
                 .UseStaticFiles()
                 .UseWebSockets()
-                .UseMvc();
+                .UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
+                });
         }
 
         // This method creates a MultiProviderAuthDialog based on a skill manifest.
