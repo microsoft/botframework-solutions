4e22b457ed3fb9d5fdfc2be2d4e8219363b99ee7
lauren-mills <lamil@microsoft.com>
2019-11-07 11:51:11 -0800

    Added skill switch dialog to lib

---
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions/Dialogs/SkillSwitchDialog.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Solutions.Responses;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Builder.Solutions.Dialogs
+{
+    public class SkillSwitchDialog : ComponentDialog
+    {
+        private LocaleTemplateEngineManager _templateEngine;
+        private IStatePropertyAccessor<string> _skillIdAccessor;
+        private IStatePropertyAccessor<Activity> _lastActivityAccessor;
+
+        public SkillSwitchDialog(
+            ConversationState conversationState,
+            LocaleTemplateEngineManager templateEngine)
+            : base(nameof(SkillSwitchDialog))
+        {
+            _skillIdAccessor = conversationState.CreateProperty<string>(Properties.SkillId);
+            _lastActivityAccessor = conversationState.CreateProperty<Activity>(Properties.LastActivity);
+            _templateEngine = templateEngine;
+
+            var intentSwitch = new WaterfallStep[]
+            {
+                PromptToSwitchAsync,
+                EndAsync,
+            };
+
+            AddDialog(new WaterfallDialog(nameof(SkillSwitchDialog), intentSwitch));
+            AddDialog(new ConfirmPrompt("ConfirmIntentSwitch"));
+        }
+
+        // Runs when this dialog ends. Handles result of prompt to switch skills or resume waiting dialog.
+        protected override async Task<DialogTurnResult> EndComponentAsync(DialogContext outerDc, object result, CancellationToken cancellationToken)
+        {
+            var skillId = await _skillIdAccessor.GetAsync(outerDc.Context, () => null).ConfigureAwait(false);
+            var lastActivity = await _lastActivityAccessor.GetAsync(outerDc.Context, () => null).ConfigureAwait(false);
+            outerDc.Context.Activity.Text = lastActivity.Text;
+
+            // Ends this dialog.
+            await outerDc.EndDialogAsync().ConfigureAwait(false);
+
+            if ((bool)result)
+            {
+                // If user decided to switch, replace current skill dialog with new skill dialog.
+                return await outerDc.ReplaceDialogAsync(skillId).ConfigureAwait(false);
+            }
+            else
+            {
+                // Otherwise, continue the waiting skill dialog with the user's previous utterance.
+                return await outerDc.ContinueDialogAsync().ConfigureAwait(false);
+            }
+        }
+
+        // Prompts user to switch to a new skill.
+        private async Task<DialogTurnResult> PromptToSwitchAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var options = stepContext.Options as SkillSwitchDialogOptions ?? throw new ArgumentException($"You must provide options of type {typeof(SkillSwitchDialogOptions).FullName}.");
+            await _skillIdAccessor.SetAsync(stepContext.Context, options.Skill.Id).ConfigureAwait(false);
+            await _lastActivityAccessor.SetAsync(stepContext.Context, stepContext.Context.Activity).ConfigureAwait(false);
+
+            return await stepContext.PromptAsync("ConfirmIntentSwitch", new PromptOptions() { Prompt = options.Prompt }).ConfigureAwait(false);
+        }
+
+        // Ends this dialog, returning the prompt result.
+        private async Task<DialogTurnResult> EndAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            bool result = (bool)stepContext.Result;
+            return await stepContext.EndDialogAsync(result: result).ConfigureAwait(false);
+        }
+
+        private class Properties
+        {
+            public const string SkillId = "skillSwitchValue";
+            public const string LastActivity = "skillSwitchActivity";
+        }
+    }
+}
---
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions/Dialogs/SkillSwitchDialogOptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Builder.Solutions.Skills.Models.Manifest;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Builder.Solutions.Dialogs
+{
+    public class SkillSwitchDialogOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkillSwitchDialogOptions"/> class.
+        /// </summary>
+        /// <param name="prompt">The <see cref="Activity"/> to display when prompting to switch skills.</param>
+        /// <param name="manifest">The <see cref="SkillManifest"/> for the new skill.</param>
+        public SkillSwitchDialogOptions(Activity prompt, SkillManifest manifest)
+        {
+            Prompt = prompt;
+            Skill = manifest;
+        }
+
+        public SkillManifest Skill { get; set; }
+
+        public Activity Prompt { get; set; }
+    }
+}
