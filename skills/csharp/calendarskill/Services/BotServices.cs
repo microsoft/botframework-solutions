090ad1fbf6001e7369ef72cc7c67eb034e293f45
lauren-mills <lamil@microsoft.com>
2019-11-19 10:12:56 -0800

    fixed spellcheck setting

--- skills/csharp/calendarskill/Services/BotServices.cs
+++ skills/csharp/calendarskill/Services/BotServices.cs
@@ -31,6 +31,8 @@ namespace CalendarSkill.Services
                 {
                     TelemetryClient = telemetryClient,
                     LogPersonalInformation = true,
+                    SpellCheck = string.IsNullOrEmpty(settings.BingSpellCheckSubscriptionKey) ? false : true,
+                    BingSpellCheckSubscriptionKey = settings.BingSpellCheckSubscriptionKey
                 };
 
                 if (config.DispatchModel != null)
