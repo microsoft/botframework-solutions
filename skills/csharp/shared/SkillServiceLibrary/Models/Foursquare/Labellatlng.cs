31469de0efbdd81023d488ac43ebcd5e1ed1843b
xieofxie <xieofxie@126.com>
2019-12-06 11:49:47 +0800

    [POI] simply move Services to SkillServiceLibrary (#2563)
    * Add SkillServiceLibrary.Fakes
    * Move tests to SkillServiceLibrary.Tests

---
+++ skills/csharp/shared/SkillServiceLibrary/Models/Foursquare/Labellatlng.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace SkillServiceLibrary.Models.Foursquare
+{
+    public class LabelLatLng
+    {
+        [JsonProperty(PropertyName = "label")]
+        public string Label { get; set; }
+
+        [JsonProperty(PropertyName = "lat")]
+        public float Lat { get; set; }
+
+        [JsonProperty(PropertyName = "lng")]
+        public float Lng { get; set; }
+    }
+}
---
+++ skills/csharp/shared/SkillServiceLibrary/Models/Foursquare/VenueResponse.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace SkillServiceLibrary.Models.Foursquare
+{
+    public class VenueResponse
+    {
+        [JsonProperty(PropertyName = "response")]
+        public Response Response { get; set; }
+    }
+}
