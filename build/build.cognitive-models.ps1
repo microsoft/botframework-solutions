dab438368018146d0aa27aece4b70a3eff0d14cc
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-12-09 18:54:07 -0800

    Add build script to make an empty multilanguage cognitivemodels.json file

---
+++ build/build.cognitive-models.ps1
@@ -0,0 +1,55 @@
+$jsonfile = '.\templates\Virtual-Assistant-Template\csharp\Sample\VirtualAssistantSample\CognitiveModels.json'
+$config = Get-Content -Raw -Path $jsonfile | ConvertFrom-Json
+$languageArr = "en-us", "de-de", "fr-fr", "it-it", "zh-cn"
+
+$dispatchModel = @{
+    authoringkey = ""
+    authoringRegion = ""
+    appid = ""
+    name = ""
+    region = ""
+    subscriptionkey = ""
+    type = "dispatch"
+}
+
+$languageModel = @{
+    appid = ""
+    authoringkey = ""
+    authoringRegion = ""
+    id = ""
+    name = ""
+    region = ""
+    subscriptionkey = ""
+    version = ""
+}
+
+$knowledgeBase = @{
+    endpointKey = ""
+    id = ""
+    hostname = ""
+    kbId = ""
+    name = ""
+    subscriptionKey = ""
+}
+
+$languageBlock = @{
+    dispatchModel = $dispatchModel
+    languageModels = @($languageModel)
+    knowledgeBases = @(
+        $knowledgeBase
+        $knowledgeBase
+    )
+}
+
+foreach ($language in $languageArr){
+    # If english, add additional knowledge base
+    # If other, add all
+    if ($language -eq "en-us"){
+        $config.cognitiveModels.$language.knowledgebases += $knowledgeBase
+    }
+    else {
+        $config.cognitiveModels | Add-Member -Name $language -Value $languageBlock -MemberType NoteProperty
+    }
+}
+
+$config | ConvertTo-Json -depth 5 | Set-Content $jsonfile
