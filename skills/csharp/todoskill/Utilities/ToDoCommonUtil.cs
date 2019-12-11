8ed99222d627baab16f23daedcacc730a58e767e
shzhen <shzhen@microsoft.com>
2019-12-11 15:47:39 +0800

    fix comments

--- skills/csharp/todoskill/Utilities/ToDoCommonUtil.cs
+++ skills/csharp/todoskill/Utilities/ToDoCommonUtil.cs
@@ -1,11 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Dialogs.Adaptive.Templates;
-using Microsoft.Bot.Schema;
-
 namespace ToDoSkill.Utilities
 {
     public class ToDoCommonUtil
