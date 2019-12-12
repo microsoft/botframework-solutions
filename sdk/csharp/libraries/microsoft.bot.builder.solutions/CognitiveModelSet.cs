248aeeb8a7551750683d9330f19313015730c7dc
Darren Jefford <darrenj@microsoft.com>
2019-12-12 17:02:41 +0000

    Docs and typo change in obsolete tag

--- sdk/csharp/libraries/microsoft.bot.builder.solutions/CognitiveModelSet.cs
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions/CognitiveModelSet.cs
@@ -14,7 +14,7 @@ namespace Microsoft.Bot.Builder.Solutions
 
         public Dictionary<string, LuisRecognizer> LuisServices { get; set; } = new Dictionary<string, LuisRecognizer>();
 
-        [Obsolete("Please updated your Virtual Assistant to use the new QnAMakerDialog with Multi Turn and Active Learning support instead. For more information, refer to https://aka.ms/bfvarqnamakerupdate.", false)]
+        [Obsolete("Please update your Virtual Assistant to use the new QnAMakerDialog with Multi Turn and Active Learning support instead. For more information, refer to https://aka.ms/bfvaqnamakerupdate.", false)]
         public Dictionary<string, QnAMaker> QnAServices { get; set; } = new Dictionary<string, QnAMaker>();
 
         public Dictionary<string, QnAMakerEndpoint> QnAConfiguration { get; set; } = new Dictionary<string, QnAMakerEndpoint>();
