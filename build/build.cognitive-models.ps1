615edb619da7d33cf4b27796381fa142f33f8018
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-12-13 12:15:46 -0800

    Fix casing for knowledgebases when using variable subsitution

--- build/build.cognitive-models.ps1
+++ build/build.cognitive-models.ps1
@@ -44,12 +44,12 @@ if ($useDispatch){
     $languageBlock = [pscustomobject]@{
     dispatchModel = $dispatchModel
     languageModels = @()
-    knowledgeBases = @()
+    knowledgebases = @()
     }
 } else {
     $languageBlock = [pscustomobject]@{
         languageModels = @()
-        knowledgeBases = @()
+        knowledgebases = @()
     }
 }
 
@@ -60,7 +60,7 @@ for($i = 0; $i -lt $languageModels; $i++){
 
 # Add knowledge bases
 for($i = 0; $i -lt $knowledgeBases; $i++){
-    $languageBlock.knowledgeBases += $knowledgeBase
+    $languageBlock.knowledgebases += $knowledgeBase
 }
 
 # Get languages
@@ -68,8 +68,6 @@ $languageArr = $languages -split ","
 
 # Add block of models for each language
 foreach ($language in $languageArr){
-    Write-Host "Adding block for ${language}"
-    Write-Host $languageBlock
     $config.cognitiveModels | Add-Member -Force -Name $language -Value $languageBlock -MemberType NoteProperty
 }
 
