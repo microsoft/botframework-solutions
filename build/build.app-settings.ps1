ab25a4869d09faf5c80997a985f1055474733ae2
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2019-12-13 15:01:37 -0800

    Rename oauth-connections script to app-settings to account for additional properties requires in botskills cli

--- build/build.app-settings.ps1
+++ build/build.app-settings.ps1
@@ -13,4 +13,8 @@ for($i = 0; $i -lt $oauthConnections; $i++){
     $config.oauthConnections += $oauthConnection
 }
 
+# Add botWebAppName and resourceGroupName for connecting skills
+$config.cognitiveModels | Add-Member -Force -Name "botWebAppName" -Value "" -MemberType NoteProperty
+$config.cognitiveModels | Add-Member -Force -Name "resourceGroupName" -Value "" -MemberType NoteProperty
+
 $config | ConvertTo-Json -depth 4 | Set-Content $jsonfile
