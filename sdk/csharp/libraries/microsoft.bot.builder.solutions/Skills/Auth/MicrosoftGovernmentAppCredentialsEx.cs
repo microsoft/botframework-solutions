37ced4a7a82227138e3cf81ce020661a2e7599d0
lauren-mills <lamil@microsoft.com>
2020-01-10 10:09:38 -0800

    updated for gov token service

---
+++ sdk/csharp/libraries/microsoft.bot.builder.solutions/Skills/Auth/MicrosoftGovernmentAppCredentialsEx.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Connector.Authentication;
+
+namespace Microsoft.Bot.Builder.Solutions.Skills.Auth
+{
+    public class MicrosoftGovernmentAppCredentialsEx : MicrosoftGovernmentAppCredentials, IServiceClientCredentials
+    {
+        public MicrosoftGovernmentAppCredentialsEx(string appId, string password)
+            : base(appId, password)
+        {
+        }
+    }
+}
