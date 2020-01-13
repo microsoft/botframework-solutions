4481688ad37b215868051ccd790ce32a479d53d6
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2020-01-13 14:34:29 -0800

    [Linked Accounts] Clean up UI & ability to modify user id (#2864)
    * Remove webchat w/ linked acounts app
    
    * Updated to latest bootstrap css
    Cleaned up UI for mobile responsiveness
    Removed edit userid functionality
    
    * Fix formatting on table

--- samples/csharp/clients/linkedaccounts.web/Controllers/HomeController.cs
+++ samples/csharp/clients/linkedaccounts.web/Controllers/HomeController.cs
@@ -131,19 +131,6 @@ namespace LinkedAccounts.Web.Controllers
             return this.RedirectToAction("LinkedAccounts");
         }
 
-        [HttpPost]
-#pragma warning disable CS1998 // This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await Task.Run(...)' to do CPU-bound work on a background thread.
-        public async Task<IActionResult> ChangeUserId(LinkedAccountsViewModel model)
-#pragma warning restore CS1998 // This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await Task.Run(...)' to do CPU-bound work on a background thread.
-        {
-            if (this.ModelState.IsValid)
-            {
-                this.HttpContext.Session.SetString("ChangedUserId", model.UserId);
-            }
-
-            return this.RedirectToAction("LinkedAccounts");
-        }
-
         public IActionResult About()
         {
             this.ViewData["Message"] = "Your application description page.";
