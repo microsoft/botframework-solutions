e67b5949f4cd46067f1d587ab0a9c2dac681199c
Ted <tedlee@microsoft.com>
2020-01-14 12:59:00 -0800

    Rename solutions lib to Microsoft.Bot.Solutions (#2876)
    * rename lib
    
    * rename the lib solution

---
+++ sdk/csharp/libraries/microsoft.bot.solutions/Skills/Models/EnhancedBotFrameworkSkill.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Bot.Builder.Skills;
+
+namespace Microsoft.Bot.Solutions.Skills.Models
+{
+    // Enhanced version of BotFrameworkSkill that adds additional properties commonly needed by a Skills VA
+    public class EnhancedBotFrameworkSkill : BotFrameworkSkill
+    {
+        // Summary:
+        //     Gets or sets the Name of the skill.
+        public string Name { get; set; }
+
+        // Summary:
+        //     Gets or sets the Description of the skill.
+        public string Description { get; set; }
+    }
+}
---
+++ sdk/csharp/libraries/microsoft.bot.solutions/TaskExtensions/ScheduledProcessor.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NCrontab;
+
+namespace Microsoft.Bot.Solutions.TaskExtensions
+{
+    public abstract class ScheduledProcessor : BackgroundService
+    {
+        private const int DelayBetweenTasks = 100;
+        private const int DelayBetweenRuns = 5000;
+        private IBackgroundTaskQueue _backgroundTaskQueue;
+
+        public ScheduledProcessor(IBackgroundTaskQueue backgroundTaskQueue)
+        {
+            _backgroundTaskQueue = backgroundTaskQueue;
+            Schedules = new List<ScheduledTaskModel>();
+        }
+
+        protected List<ScheduledTaskModel> Schedules { get; }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            do
+            {
+                var now = DateTime.Now;
+
+                foreach (var schedule in Schedules)
+                {
+                    if (!schedule.CancellationToken.IsCancellationRequested)
+                    {
+                        var cronSchedule = CrontabSchedule.Parse(schedule.ScheduleExpression);
+                        var nextrun = cronSchedule.GetNextOccurrence(now);
+                        if (now > nextrun)
+                        {
+                            _backgroundTaskQueue.QueueBackgroundWorkItem(schedule.Task);
+                        }
+
+                        await Task.Delay(DelayBetweenTasks, stoppingToken).ConfigureAwait(false); // 100 milliseconds delay to next task in line
+                    }
+                }
+
+                await Task.Delay(DelayBetweenRuns, stoppingToken).ConfigureAwait(false); // 5 seconds delay to next ScheduledProcessor run
+            }
+            while (!stoppingToken.IsCancellationRequested);
+        }
+    }
+}
