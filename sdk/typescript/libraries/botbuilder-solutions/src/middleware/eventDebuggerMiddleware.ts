e19fbbcd401af2cb803aabf1d08b74077d0ebe5e
Martin Battaglino <martinbatta32@gmail.com>
2020-01-16 12:10:09 -0300

    Update Middleware folder

--- sdk/typescript/libraries/botbuilder-solutions/src/middleware/eventDebuggerMiddleware.ts
+++ sdk/typescript/libraries/botbuilder-solutions/src/middleware/eventDebuggerMiddleware.ts
@@ -10,21 +10,22 @@ export class EventDebuggerMiddleware implements Middleware {
     public async onTurn(turnContext: TurnContext, next: () => Promise<void>): Promise<void> {
         const activity: Activity = turnContext.activity;
 
-        if (activity.type === ActivityTypes.Message) {
+        if (activity.type === ActivityTypes.Message && activity.type !== undefined && activity.type.trim().length > 0) {
             const text: string = activity.text;
             const value: string = JSON.stringify(activity.value);
 
-            if (text && text.startsWith('/event:')) {
+            if (text !== undefined && text.trim().length > 0 && text.startsWith('/event:')) {
                 const json: string = text.substr('/event:'.length);
                 const body: Activity = JSON.parse(json);
 
                 turnContext.activity.type = ActivityTypes.Event;
+
                 turnContext.activity.name = body.name || turnContext.activity.name;
                 turnContext.activity.text = body.text || turnContext.activity.text;
                 turnContext.activity.value = body.value || turnContext.activity.value;
             }
 
-            if (value && value.includes('event')) {
+            if (value !== undefined && value.trim().length > 0 && value.includes('event')) {
                 const body: { event: { name: string; text: string; value: string }} = JSON.parse(value);
 
                 turnContext.activity.type = ActivityTypes.Event;
@@ -32,9 +33,11 @@ export class EventDebuggerMiddleware implements Middleware {
                     if (body.event.name !== undefined) {
                         turnContext.activity.name = body.event.name;
                     }
+
                     if (body.event.text !== undefined) {
                         turnContext.activity.text = body.event.text;
                     }
+
                     if (body.event.value !== undefined) {
                         turnContext.activity.value = body.event.value;
                     }
@@ -42,6 +45,6 @@ export class EventDebuggerMiddleware implements Middleware {
             }
         }
 
-        return next();
+        await next();
     }
 }
