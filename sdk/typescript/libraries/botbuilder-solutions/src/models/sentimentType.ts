9c2c7786e5e56f3b8923ecb717acd2e6e63ffe61
Martin Battaglino <martinbatta32@gmail.com>
2020-01-16 12:11:54 -0300

    Add Models folder

---
+++ sdk/typescript/libraries/botbuilder-solutions/src/models/sentimentType.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export enum SentimentType {
+    /**
+     * None
+     */
+    None,
+
+    /**
+     * Positive Sentiment
+     */
+    Positive,
+
+    /**
+     * Neutral Sentiment
+     */
+    Neutral,
+
+    /**
+     * Negative Sentiment
+     */
+    Negative
+}
