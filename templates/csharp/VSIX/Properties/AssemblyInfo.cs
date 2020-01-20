f2eec81b2268ad3dd9c5771b9b8a648228e01d93
lauren-mills <lamil@microsoft.com>
2020-01-20 09:49:54 -0800

    Combined VA and Skill templates into one VSIX (#2851)
    * moved va and skill samples into samples directory
    
    * combined va and skill vsix into one
    
    * consolidated va and skill samples into one sln
    
    * Update NuGet package references to 4.7.0
    
    * Conslidate solution lib reference
    
    * Add Directory.Build.props to VA sample
    
    * removed extra sln files
    
    * updated gitignore
    
    * updated vsix display name
    
    Co-authored-by: Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
    Co-authored-by: Darren Jefford <33519520+darrenj@users.noreply.github.com>

--- templates/csharp/VSIX/Properties/AssemblyInfo.cs
+++ templates/csharp/VSIX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SkillTemplate")]
+[assembly: AssemblyTitle("BotTemplates")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SkillTemplate")]
+[assembly: AssemblyProduct("BotTemplates")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
