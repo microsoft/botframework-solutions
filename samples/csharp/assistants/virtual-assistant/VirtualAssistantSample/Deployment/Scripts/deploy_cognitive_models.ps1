c4974cc5415438f8351c6678ac5af384b155c842
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2020-01-23 15:10:33 -0800

    Remove parentheses on dispatch cmd

--- samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Deployment/Scripts/deploy_cognitive_models.ps1
+++ samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Deployment/Scripts/deploy_cognitive_models.ps1
@@ -168,9 +168,18 @@ foreach ($language in $languageArr)
         Write-Host "> Initializing $($langCode) dispatch model ..." -NoNewline
 		$dispatchName = "$($name)$($langCode)_Dispatch"
 		$dataFolder = Join-Path $PSScriptRoot .. Resources Dispatch $langCode
-		Write-Host "$($dispatchName)"
-		Write-Host "$($dataFolder)"
-		dispatch init --name $dispatchName --luisAuthoringKey $luisAuthoringKey --luisAuthoringRegion $luisAuthoringRegion --gov $gov --dataFolder $dataFolder
+		Write-Host $dispatchName
+		Write-Host $dataFolder
+		Write-Host $luisAuthoringKey
+		Write-Host $luisAuthoringRegion
+		Write-Host $gov
+		Write-Host $logFile
+		dispatch init `
+			--name $dispatchName `
+			--luisAuthoringKey $luisAuthoringKey `
+			--luisAuthoringRegion $luisAuthoringRegion `
+            --gov $gov `
+			--dataFolder $dataFolder 2>> $logFile | Out-Null
         Write-Host "Done." -ForegroundColor Green
 	}
 
