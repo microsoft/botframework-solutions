f5a0e2784703ead429c01ac88a0bf71ebb0e76ac
lzc850612 <lzc850612@gmail.com>
2020-01-27 13:52:40 -0800

    revert unnecessary change

--- samples/csharp/skill/SkillSample.Tests/Utterances/GeneralUtterances.Designer.cs
+++ samples/csharp/skill/SkillSample.Tests/Utterances/GeneralUtterances.Designer.cs
@@ -8,10 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SkillSample.Tests.Utterances {
+namespace SkillSample.Tests.Utterances
+{
     using System;
-    
-    
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,187 +20,227 @@ namespace SkillSample.Tests.Utterances {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class GeneralUtterances {
-        
+    internal class GeneralUtterances
+    {
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal GeneralUtterances() {
+        internal GeneralUtterances()
+        {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
+        internal static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SkillSample.Tests.Utterances.GeneralUtterances", typeof(GeneralUtterances).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
-            get {
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to cancel it.
         /// </summary>
-        public static string Cancel {
-            get {
+        internal static string Cancel
+        {
+            get
+            {
                 return ResourceManager.GetString("Cancel", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to yes.
         /// </summary>
-        public static string Confirm {
-            get {
+        internal static string Confirm
+        {
+            get
+            {
                 return ResourceManager.GetString("Confirm", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to can i talk to a person.
         /// </summary>
-        public static string Escalate {
-            get {
+        internal static string Escalate
+        {
+            get
+            {
                 return ResourceManager.GetString("Escalate", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to all finished.
         /// </summary>
-        public static string FinishTask {
-            get {
+        internal static string FinishTask
+        {
+            get
+            {
                 return ResourceManager.GetString("FinishTask", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to go back.
         /// </summary>
-        public static string GoBack {
-            get {
+        internal static string GoBack
+        {
+            get
+            {
                 return ResourceManager.GetString("GoBack", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to help.
         /// </summary>
-        public static string Help {
-            get {
+        internal static string Help
+        {
+            get
+            {
                 return ResourceManager.GetString("Help", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to unknown.
         /// </summary>
-        public static string None {
-            get {
+        internal static string None
+        {
+            get
+            {
                 return ResourceManager.GetString("None", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to no.
         /// </summary>
-        public static string Reject {
-            get {
+        internal static string Reject
+        {
+            get
+            {
                 return ResourceManager.GetString("Reject", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to could you say it again.
         /// </summary>
-        public static string Repeat {
-            get {
+        internal static string Repeat
+        {
+            get
+            {
                 return ResourceManager.GetString("Repeat", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to any one is ok.
         /// </summary>
-        public static string SelectAny {
-            get {
+        internal static string SelectAny
+        {
+            get
+            {
                 return ResourceManager.GetString("SelectAny", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to choose the last one.
         /// </summary>
-        public static string SelectItem {
-            get {
+        internal static string SelectItem
+        {
+            get
+            {
                 return ResourceManager.GetString("SelectItem", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to none of them.
         /// </summary>
-        public static string SelectNone {
-            get {
+        internal static string SelectNone
+        {
+            get
+            {
                 return ResourceManager.GetString("SelectNone", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to next.
         /// </summary>
-        public static string ShowNext {
-            get {
+        internal static string ShowNext
+        {
+            get
+            {
                 return ResourceManager.GetString("ShowNext", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to previous.
         /// </summary>
-        public static string ShowPrevious {
-            get {
+        internal static string ShowPrevious
+        {
+            get
+            {
                 return ResourceManager.GetString("ShowPrevious", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to restart.
         /// </summary>
-        public static string StartOver {
-            get {
+        internal static string StartOver
+        {
+            get
+            {
                 return ResourceManager.GetString("StartOver", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to be quiet.
         /// </summary>
-        public static string Stop {
-            get {
+        internal static string Stop
+        {
+            get
+            {
                 return ResourceManager.GetString("Stop", resourceCulture);
             }
         }
