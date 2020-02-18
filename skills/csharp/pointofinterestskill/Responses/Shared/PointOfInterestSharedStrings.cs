d8aeebbf156f64ab7d2f28774698830d5eac5d44
Hualiang Xie <hualxie@microsoft.com>
2020-02-18 18:09:36 +0800

    tool part

---
+++ skills/csharp/pointofinterestskill/Responses/Shared/PointOfInterestSharedStrings.cs
@@ -0,0 +1,36 @@
+// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Solutions.Responses;
+
+namespace PointOfInterestSkill.Responses.Shared
+{
+    /// <summary>
+    /// Contains bot responses.
+    /// </summary>
+    public class PointOfInterestSharedStrings : IResponseIdCollection
+    {
+        // Generated accessors
+        public const string CALL = "CALL";
+        public const string CARD_TITLE = "CARD_TITLE";
+        public const string END = "END";
+        public const string HOUR = "HOUR";
+        public const string HOURS = "HOURS";
+        public const string HOUR_ABBREVIATION = "HOUR_ABBREVIATION";
+        public const string KILOMETERS = "KILOMETERS";
+        public const string KILOMETERS_ABBREVIATION = "KILOMETERS_ABBREVIATION";
+        public const string LESS_THAN_A_MINUTE = "LESS_THAN_A_MINUTE";
+        public const string MILES = "MILES";
+        public const string MILES_ABBREVIATION = "MILES_ABBREVIATION";
+        public const string MINUTE = "MINUTE";
+        public const string MINUTES = "MINUTES";
+        public const string MINUTE_ABBREVIATION = "MINUTE_ABBREVIATION";
+        public const string POWERED_BY = "POWERED_BY";
+        public const string SHOW_DIRECTIONS = "SHOW_DIRECTIONS";
+        public const string START = "START";
+        public const string START_NAVIGATION = "START_NAVIGATION";
+        public const string YOU = "YOU";
+    }
+}
+
