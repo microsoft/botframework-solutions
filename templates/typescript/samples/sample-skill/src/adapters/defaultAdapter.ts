d31aa4000aef719626dc95cc4a8f2bac162e5d8b
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2020-02-27 17:07:28 -0300

    [TypeScript] GA SDK Skill Migration - Update Adapters (#225)
    * Update Adapters folder
    
    * Update adapter after merge
    
    * Apply Feedback

--- templates/typescript/samples/sample-skill/src/adapters/defaultAdapter.ts
+++ templates/typescript/samples/sample-skill/src/adapters/defaultAdapter.ts
@@ -8,27 +8,34 @@ import {
     BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
     BotTelemetryClient,
+    ConversationState,
     ShowTypingMiddleware,
     TelemetryLoggerMiddleware,
     TranscriptLoggerMiddleware,
-    TranscriptStore, 
+    UserState,
     TurnContext } from 'botbuilder';
-import { AzureBlobTranscriptStore } from 'botbuilder-azure';
+import { DialogState } from 'botbuilder-dialogs';
 import {
     EventDebuggerMiddleware,
+    SetLocaleMiddleware,
     LocaleTemplateEngineManager,
-    SetLocaleMiddleware } from 'botbuilder-solutions';
+    SetSpeakMiddleware,
+    SkillMiddleware } from 'botbuilder-solutions';
 import { IBotSettings } from '../services/botSettings';
+import { AzureBlobTranscriptStore } from 'botbuilder-azure';
 import { TelemetryInitializerMiddleware } from 'botbuilder-applicationinsights';
+import { Activity } from 'botbuilder-schema';
 
 export class DefaultAdapter extends BotFrameworkAdapter {
-
     public constructor(
         settings: Partial<IBotSettings>,
+        userState: UserState,
+        conversationState: ConversationState,
+        adapterSettings: Partial<BotFrameworkAdapterSettings>,
         templateEngine: LocaleTemplateEngineManager,
         telemetryMiddleware: TelemetryInitializerMiddleware,
         telemetryClient: BotTelemetryClient,
-        adapterSettings: Partial<BotFrameworkAdapterSettings>) {
+    ) {
         super(adapterSettings);
 
         this.onTurnError = async (context: TurnContext, error: Error): Promise<void> => {
@@ -40,27 +47,34 @@ export class DefaultAdapter extends BotFrameworkAdapter {
                 type: ActivityTypes.Trace,
                 text: error.stack
             });
+            
             await context.sendActivity(templateEngine.generateActivityForLocale('ErrorMessage'));
             telemetryClient.trackException({ exception: error });
+
+            if (context.isSkill()){
+                // Send and EndOfConversation activity to the skill caller with the error to end the conversation
+                // and let the caller decide what to do.
+                const endOfconversation = new Activity(ActivityTypes.EndOfConversation)
+                endOfconversation.code = "SkillError";
+                endOfconversation.text = error.message;
+                await context.sendActivity(endOfconversation);
+            }
         };
         
         if (settings.blobStorage === undefined) {
             throw new Error('There is no blobStorage value in appsettings file');
         }
-        
-        const transcriptStore: TranscriptStore = new AzureBlobTranscriptStore({
-            containerName: settings.blobStorage.container,
-            storageAccountOrConnectionString: settings.blobStorage.connectionString
-        });
-        
-        this.use(telemetryMiddleware);
 
+        this.use(telemetryMiddleware)
+        
         // Uncomment the following line for local development without Azure Storage
         // this.use(new TranscriptLoggerMiddleware(new MemoryTranscriptStore()));
-        this.use(new TranscriptLoggerMiddleware(transcriptStore));
+        this.use(new TranscriptLoggerMiddleware(new AzureBlobTranscriptStore(settings.blobStorage.connectionString, settings.blobStorage.container)));
         this.use(new TelemetryLoggerMiddleware(telemetryClient, true));
         this.use(new ShowTypingMiddleware());
         this.use(new SetLocaleMiddleware(settings.defaultLocale || 'en-us'));
         this.use(new EventDebuggerMiddleware());
+        this.use(new SkillMiddleware(userState, conversationState, conversationState.createProperty(DialogState.name)));
+        this.use(new SetSpeakMiddleware());
     }
 }
--- templates/typescript/samples/sample-skill/src/adapters/index.ts
+++ templates/typescript/samples/sample-skill/src/adapters/index.ts
@@ -3,5 +3,4 @@
  * Licensed under the MIT License.
  */
 
-export * from './customSkillAdapter';
 export * from './defaultAdapter';
