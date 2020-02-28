baaa86102e8edf2e0e39797d7db62f4e4bebf456
Victor Grycuk <victor.grycuk@southworks.com>
2020-02-28 12:20:15 -0300

    Remove files (#223)

--- sdk/typescript/libraries/botbuilder-solutions/src/skills/index.ts
+++ sdk/typescript/libraries/botbuilder-solutions/src/skills/index.ts
@@ -3,20 +3,14 @@
  * Licensed under the MIT License.
  */
 
-export * from './auth';
-export * from './http';
 export * from './dialogs';
 export * from './models';
 export * from './responses/skillResponses';
-export * from './activityHandler';
-export * from './skillContext';
 export * from './skillDialog';
 export * from './skillException';
 export * from './skillManifestGenerator';
 export * from './skillMiddleware';
 export * from './skillRouter';
-export * from './skillTransport';
-export * from './skillConstants';
 export * from './skillDialogOption';
 export * from './skillConversationIdFactory';
 export * from './skillDialogArgs';
