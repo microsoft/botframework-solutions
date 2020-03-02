24c63f9211113b5e12a7f75753cf68fdfc4a73cb
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2020-03-02 12:18:15 -0300

    [TypeScript][0.8] Skill Migration - Update Bots Folder (#240)
    * Update Bots Folder
    
    * Apply PR feedback
    
    * Delete unnecesary eslint comments
    
    Co-authored-by: Martin Battaglino <martinbatta32@gmail.com>

--- templates/typescript/samples/sample-skill/src/bots/defaultActivityHandler.ts
+++ templates/typescript/samples/sample-skill/src/bots/defaultActivityHandler.ts
@@ -13,21 +13,22 @@ import {
     DialogState, 
     DialogContext,
     DialogSet } from 'botbuilder-dialogs';
-import { DialogEx } from 'botbuilder-solutions';
+import { LocaleTemplateEngineManager, DialogEx } from 'botbuilder-solutions';
 
 export class DefaultActivityHandler<T extends Dialog> extends ActivityHandler {
+    private readonly dialog: Dialog;
     private readonly conversationState: BotState;
     private readonly userState: BotState;
     private dialogStateAccessor: StatePropertyAccessor<DialogState>;
-    private readonly dialog: Dialog;
-
+    private templateEngine: LocaleTemplateEngineManager;
     private readonly dialogs: DialogSet;
     private readonly rootDialogId: string;
 
     public constructor(
         conversationState: BotState,
         userState: BotState,
-        dialog: T
+        dialog: T,
+        templateEngine: LocaleTemplateEngineManager
     ) {
         super();
         this.dialog = dialog;
@@ -36,17 +37,15 @@ export class DefaultActivityHandler<T extends Dialog> extends ActivityHandler {
         this.conversationState = conversationState;
         this.userState = userState;
         this.dialogStateAccessor = conversationState.createProperty<DialogState>('DialogState');
+        this.templateEngine = templateEngine
 
         this.dialogs = new DialogSet(this.dialogStateAccessor);
         this.dialogs.add(dialog);
         this.onTurn(this.turn.bind(this));
         this.onMembersAdded(this.membersAdded.bind(this));
-        
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    public async turn(turnContext: TurnContext, next: () => Promise<void>): Promise<any> {
-
+    public async turn(turnContext: TurnContext, next: () => Promise<void>): Promise<void> {
         super.onTurn(next);
 
         const dc: DialogContext = await this.dialogs.createContext(turnContext);
@@ -61,19 +60,16 @@ export class DefaultActivityHandler<T extends Dialog> extends ActivityHandler {
         await this.userState.saveChanges(turnContext, false);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    protected async membersAdded(turnContext: TurnContext, next: () => Promise<void>): Promise<any> {
+    protected async membersAdded(turnContext: TurnContext, next: () => Promise<void>): Promise<void> {
+        await turnContext.sendActivity(this.templateEngine.generateActivityForLocale('IntroMessage'));
+        await DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
+    }
+
+    protected async onMessageActivity(turnContext: TurnContext): Promise<void> {
         return DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    protected async onMessageActivity(turnContext: TurnContext): Promise<any> {
+    protected async onEventActivity(turnContext: TurnContext): Promise<void> {
         return DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    protected async onEventActivity(turnContext: TurnContext): Promise<any> {
-        return DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
-    }
-    
 }
