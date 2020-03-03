3dfacabb3e4d7ede0f1e85073ea4b36377d20d54
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2020-03-03 12:24:23 -0800

    Restore QnAMakerService to keep cognitive models working

--- sdk/csharp/libraries/microsoft.bot.solutions/BotSettingsBase.cs
+++ sdk/csharp/libraries/microsoft.bot.solutions/BotSettingsBase.cs
@@ -166,7 +166,9 @@ namespace Microsoft.Bot.Solutions
             /// <value>
             /// The collection of QnA Maker knowledgebases.
             /// </value>
-            public List<QnAMakerEndpoint> Knowledgebases { get; set; }
+#pragma warning disable CS0618 // Type or member is obsolete
+            public List<QnAMakerService> Knowledgebases { get; set; }
+#pragma warning restore CS0618 // Type or member is obsolete
         }
     }
 }
