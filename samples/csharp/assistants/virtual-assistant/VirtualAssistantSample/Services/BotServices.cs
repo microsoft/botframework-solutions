076dffe248546dede8237f1f2078dd3c2ebe01ed
ted Li <tedlee@microsoft.com>
2020-03-13 09:39:09 -0700

    update to LuisRecognizerOptionsV3 for VA and Skill sample

--- samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Services/BotServices.cs
+++ samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Services/BotServices.cs
@@ -28,20 +28,25 @@ namespace VirtualAssistantSample.Services
 
                 var telemetryClient = client;
 
-                var dispatchApp = new LuisApplication(config.DispatchModel.AppId, config.DispatchModel.SubscriptionKey, config.DispatchModel.GetEndpoint());
-                LuisRecognizerOptionsV2 luisOptions = new LuisRecognizerOptionsV2(dispatchApp)
+                LuisRecognizerOptionsV3 luisOptions;
+
+                if (config.DispatchModel != null)
                 {
-                    TelemetryClient = telemetryClient,
-                    LogPersonalInformation = true,
-                };
-                set.DispatchService = new LuisRecognizer(luisOptions);
+                    var dispatchApp = new LuisApplication(config.DispatchModel.AppId, config.DispatchModel.SubscriptionKey, config.DispatchModel.GetEndpoint());
+                    luisOptions = new LuisRecognizerOptionsV3(dispatchApp)
+                    {
+                        TelemetryClient = telemetryClient,
+                        LogPersonalInformation = true,
+                    };
+                    set.DispatchService = new LuisRecognizer(luisOptions);
+                }
 
                 if (config.LanguageModels != null)
                 {
                     foreach (var model in config.LanguageModels)
                     {
                         var luisApp = new LuisApplication(model.AppId, model.SubscriptionKey, model.GetEndpoint());
-                        luisOptions = new LuisRecognizerOptionsV2(luisApp)
+                        luisOptions = new LuisRecognizerOptionsV3(luisApp)
                         {
                             TelemetryClient = telemetryClient,
                             LogPersonalInformation = true,
