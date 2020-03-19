fa9279541aac95816c95c64d11019aa8cfa5b851
Gabo Gilabert <gabog@users.noreply.github.com>
2020-03-19 18:08:06 -0400

    SkillSample updates (#3218)
    * Refactored DefaultAdapter based on SDK Skill samples (broke down OnTurnError in several discrete methods with error handling and deleted conversation state at the end).
    Added AllowedCallersClaimsValidator sample.
    Moved assignment of MainDialog.TelemetryClient into DefaultActivityHandler to avoid calling a virtual property from the Dialog's constructor.
    Made private fields readonly where possible.
    Removed unused _stateAccessor from MainDialog
    Added missing cancellationToken parameters across the board.
    Added Async suffic to async methods.
    Simplified SDK references by removing redundant packages already included in top packages.
    Made private class DialogIds static
    Updated reference to StyleCop.Analyzers to released version and add the  PrivateAssets="all" property to avoid propagating it to child projects.
    Added a reference toMicrosoft.AspNetCore.Mvc.NewtonsoftJson to the project and updated startup.cs to .AddNewtonsoftJson() to ensure compatibility.
    Updated startup.cs to register the AllowedCallersClaimsValidator.
    
    * Removed additional SDK project that are already referenced by Solutions.

--- samples/csharp/skill/SkillSample/Dialogs/SampleAction.cs
+++ samples/csharp/skill/SkillSample/Dialogs/SampleAction.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Newtonsoft.Json;
 
@@ -25,15 +24,14 @@ namespace SkillSample.Dialogs
     public class SampleAction : SkillDialogBase
     {
         public SampleAction(
-            IServiceProvider serviceProvider,
-            IBotTelemetryClient telemetryClient)
-            : base(nameof(SampleAction), serviceProvider, telemetryClient)
+            IServiceProvider serviceProvider)
+            : base(nameof(SampleAction), serviceProvider)
         {
             var sample = new WaterfallStep[]
             {
-                PromptForName,
-                GreetUser,
-                End,
+                PromptForNameAsync,
+                GreetUserAsync,
+                EndAsync,
             };
 
             AddDialog(new WaterfallDialog(nameof(SampleAction), sample));
@@ -42,42 +40,43 @@ namespace SkillSample.Dialogs
             InitialDialogId = nameof(SampleAction);
         }
 
-        private async Task<DialogTurnResult> PromptForName(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> PromptForNameAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             // If we have been provided a input data structure we pull out provided data as appropriate
             // and make a decision on whether the dialog needs to prompt for anything.
-            var actionInput = stepContext.Options as SampleActionInput;
-            if (actionInput != null && !string.IsNullOrEmpty(actionInput.Name))
+            if (stepContext.Options is SampleActionInput actionInput && !string.IsNullOrEmpty(actionInput.Name))
             {
                 // We have Name provided by the caller so we skip the Name prompt.
-                return await stepContext.NextAsync(actionInput.Name);
+                return await stepContext.NextAsync(actionInput.Name, cancellationToken);
             }
 
             var prompt = TemplateEngine.GenerateActivityForLocale("NamePrompt");
-            return await stepContext.PromptAsync(DialogIds.NamePrompt, new PromptOptions { Prompt = prompt });
+            return await stepContext.PromptAsync(DialogIds.NamePrompt, new PromptOptions { Prompt = prompt }, cancellationToken);
         }
 
-        private async Task<DialogTurnResult> GreetUser(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> GreetUserAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             dynamic data = new { Name = stepContext.Result.ToString() };
             var response = TemplateEngine.GenerateActivityForLocale("HaveNameMessage", data);
             await stepContext.Context.SendActivityAsync(response);
 
             // Pass the response which we'll return to the user onto the next step
-            return await stepContext.NextAsync();
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
         }
 
-        private async Task<DialogTurnResult> End(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> EndAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             // Simulate a response object payload
-            var actionResponse = new SampleActionOutput();
-            actionResponse.CustomerId = new Random().Next();
+            var actionResponse = new SampleActionOutput
+            {
+                CustomerId = new Random().Next()
+            };
 
             // We end the dialog (generating an EndOfConversation event) which will serialize the result object in the Value field of the Activity
-            return await stepContext.EndDialogAsync(actionResponse);
+            return await stepContext.EndDialogAsync(actionResponse, cancellationToken);
         }
 
-        private class DialogIds
+        private static class DialogIds
         {
             public const string NamePrompt = "namePrompt";
         }
