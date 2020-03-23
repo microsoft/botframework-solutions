acdfdda8b1223f602ed279d2b6227197e9105218
xieofxie <xieofxie@126.com>
2020-03-24 07:56:59 +0800

    [EA] fix & update eva (#3216)
    * [EA] fix & update eva
    
    * Update template.json
    
    Co-authored-by: Hualiang Xie <hualxie@microsoft.com>
    Co-authored-by: lauren-mills <lamil@microsoft.com>

--- samples/csharp/assistants/enterprise-assistant/VirtualAssistantSample/Services/BotSettings.cs
+++ samples/csharp/assistants/enterprise-assistant/VirtualAssistantSample/Services/BotSettings.cs
@@ -1,14 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
-using Microsoft.Bot.Builder.Solutions;
-using Microsoft.Bot.Builder.Solutions.Skills.Models.Manifest;
+using Microsoft.Bot.Solutions;
 
 namespace VirtualAssistantSample.Services
 {
     public class BotSettings : BotSettingsBase
     {
-        public List<SkillManifest> Skills { get; set; } = new List<SkillManifest>();
     }
 }
