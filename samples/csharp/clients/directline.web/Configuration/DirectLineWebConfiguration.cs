e07c0ffdf48a2338f514576bd351435b04669bf5
Peter Innes <peinnes@microsoft.com>
2020-03-27 09:12:45 -0700

    Making necessary changes to support Direct Line Enhanced Authentication.

---
+++ samples/csharp/clients/directline.web/Configuration/DirectLineWebConfiguration.cs
@@ -0,0 +1,62 @@
+﻿// Copyright(c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace DirectLine.Web.Configuration
+{
+    public class DirectLineWebConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the name of the bot as it will be rendered on the web chat UI.
+        /// </summary>
+        public string BotName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Direct Line secret that is used to acquire a Direct Line token
+        /// used for authorization against the Direct Line endpoint.
+        /// </summary>
+        /// <remarks>
+        /// A Direct Line secret can be acquired from your bot registration's Direct Line channel
+        /// configuration. Refer to the following article for additional details:
+        /// https://blog.botframework.com/2018/09/01/using-webchat-with-azure-bot-services-authentication/
+        /// 
+        /// If you intend to use the Direct Line Speech channel, you do not need to include this setting.
+        /// </remarks>
+        public string DirectLineSecret { get; set; }
+
+        /// <summary>
+        /// Gets or sets a flag indicating whether Enhanced Authentication should be enabled when
+        /// using the Direct Line channel to communicate with your bot.
+        /// </summary>
+        /// <remarks>
+        /// To utilize Enhanced Authentication, you must first enable it in your bot registration's
+        /// Direct Line channel. Refer to the following article for additional details:
+        /// https://blog.botframework.com/2018/09/01/using-webchat-with-azure-bot-services-authentication/
+        /// </remarks>
+        public bool EnableDirectLineEnhancedAuthentication { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the region to which your Speech Service resource is deployed to
+        /// within your Azure subscription, e.g. 'westus'.
+        /// </summary>
+        /// <remarks>
+        /// A mapping of Region Names to Region Identifiers can be found at the following location:
+        /// https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-speech-to-text#how-to-get-an-access-token
+        ///
+        /// SpeechServiceRegionIdentifier, along with SpeechServiceSubscriptionKey, are both required if you
+        /// intend to integrate using the Direct Line Speech channel. If you intend to use regular Direct Line,
+        /// leave both of these properties as empty or null.
+        /// </remarks>
+        public string SpeechServiceRegionIdentifier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subscription key associated with your Speech Service resource to be used
+        /// for acquiring access tokens.
+        /// </summary>
+        /// <remarks>
+        /// SpeechServiceSubscriptionKey, along with SpeechServiceRegionIdentifier, are both required if you
+        /// intend to integrate using the Direct Line Speech channel. If you intend to use regular Direct Line,
+        /// leave both of these properties as empty or null.
+        /// </remarks>
+        public string SpeechServiceSubscriptionKey { get; set; }
+    }
+}
--- samples/csharp/clients/directline.web/Controllers/HomeController.cs
+++ samples/csharp/clients/directline.web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using DirectLine.Web.Configuration;
 using DirectLine.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -20,19 +21,19 @@ namespace DirectLine.Web.Controllers
         private const string GenerateDirectLineTokenUrl = "https://directline.botframework.com/v3/directline/tokens/generate";
         private const string UnknownMode = "UNKNOWN";
 
-        private string botName;
-        private string directLineSecret;
-        private string mode;
-        private string speechServiceRegionIdentifier;
-        private string speechServiceSubscriptionKey;
+        private readonly DirectLineWebConfiguration configuration;
+        private readonly string mode;
+        private readonly string userId;
 
         public HomeController(IConfiguration configuration)
         {
-            // Retrieve the Bot configuration
-            this.botName = configuration.GetSection("BotName").Value;
-            this.directLineSecret = configuration.GetSection("DirectLineSecret").Value;
-            this.speechServiceRegionIdentifier = configuration.GetSection("SpeechServiceRegionIdentfier").Value;
-            this.speechServiceSubscriptionKey = configuration.GetSection("SpeechServiceSubscriptionKey").Value;
+            // Retrieve the Direct Line Web application configuration
+            this.configuration = configuration.Get<DirectLineWebConfiguration>();
+
+            if (this.configuration.EnableDirectLineEnhancedAuthentication)
+            {
+                this.userId = $"dl_{Guid.NewGuid()}";
+            }
 
             // The method in which authentication token retrieval and renewal is handled, as well as configuration
             // for the web chat component, is dependent upon whether Direct Line or Direct Line Speech is being
@@ -42,12 +43,12 @@ namespace DirectLine.Web.Controllers
             // - Direct Line Speech should be used if a speech service region identifier and key are provided
             // - Direct Line should be used if a DL secret is provided
             // - Default to an unknown state (i.e. invalid app configuration provided)
-            if (!string.IsNullOrEmpty(this.speechServiceRegionIdentifier) &&
-                !string.IsNullOrEmpty(this.speechServiceSubscriptionKey))
+            if (!string.IsNullOrEmpty(this.configuration?.SpeechServiceRegionIdentifier) &&
+                !string.IsNullOrEmpty(this.configuration?.SpeechServiceSubscriptionKey))
             {
                 this.mode = DirectLineSpeechMode;
             }
-            else if (!string.IsNullOrEmpty(this.directLineSecret))
+            else if (!string.IsNullOrEmpty(this.configuration?.DirectLineSecret))
             {
                 this.mode = DirectLineMode;
             }
@@ -59,28 +60,46 @@ namespace DirectLine.Web.Controllers
 
         public IActionResult Index(string locale = "en-us")
         {
+            // Passing the User ID through to the web chat component for Enhanced Authentication
+            // is only supported in Direct Line mode.
+            bool isDirectLineMode = string.Equals(DirectLineMode, this.mode, StringComparison.OrdinalIgnoreCase);
+
             ViewData["Locale"] = locale;
             ViewData["Mode"] = this.mode;
-            ViewData["Title"] = this.botName;
+            ViewData["Title"] = this.configuration.BotName;
+            ViewData["UserId"] = isDirectLineMode ? this.userId : null;
 
             // If operating in DL Speech mode, token retrieval and renewal is handled in Views/Home/Index.cshtml.
             // If operating in Direct Line mode, do a one-time retrieval of the authentication token here. The
             // web chat component will then handle renewal of the presented token periodically.
             if (string.Equals(DirectLineSpeechMode, this.mode, StringComparison.OrdinalIgnoreCase))
             {
-                ViewData["SpeechServiceRegionIdentifier"] = this.speechServiceRegionIdentifier;
-                ViewData["SpeechServiceSubscriptionKey"] = this.speechServiceSubscriptionKey;
+                ViewData["SpeechServiceRegionIdentifier"] = this.configuration.SpeechServiceRegionIdentifier;
+                ViewData["SpeechServiceSubscriptionKey"] = this.configuration.SpeechServiceSubscriptionKey;
             }
-            else if (string.Equals(DirectLineMode, this.mode, StringComparison.OrdinalIgnoreCase))
+            else if (isDirectLineMode)
             {
                 string directLineToken = string.Empty;
 
                 var directLineClient = new HttpClient();
-                directLineClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.directLineSecret);
+                directLineClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                    "Bearer",
+                    this.configuration.DirectLineSecret);
+
+                string content = this.configuration.EnableDirectLineEnhancedAuthentication
+                    ? JsonConvert.SerializeObject(
+                        new
+                        {
+                            User = new
+                            {
+                                Id = this.userId
+                            }
+                        })
+                    : string.Empty;
 
                 HttpResponseMessage response = directLineClient.PostAsync(
                     GenerateDirectLineTokenUrl,
-                    new StringContent(string.Empty, Encoding.UTF8, "application/json")).Result;
+                    new StringContent(content, Encoding.UTF8, "application/json")).Result;
 
                 if (response.IsSuccessStatusCode)
                 {
