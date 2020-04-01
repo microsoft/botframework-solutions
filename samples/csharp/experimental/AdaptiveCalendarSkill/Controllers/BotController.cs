d30ebeb2e03ab6f227d931edd044799ddb202d17
lauren-mills <lamil@microsoft.com>
2020-04-01 09:59:51 -0700

    update composer skill

--- samples/csharp/experimental/AdaptiveCalendarSkill/Controllers/BotController.cs
+++ samples/csharp/experimental/AdaptiveCalendarSkill/Controllers/BotController.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
---
+++ samples/csharp/experimental/AdaptiveCalendarSkill/MSGraphComponentRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using ExtensionsLib.Actions.MSGraph;
+using ExtensionsLib.Input;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Newtonsoft.Json;
+
+namespace BotProject
+{
+    public class MSGraphComponentRegistration : ComponentRegistration, IComponentDeclarativeTypes
+    {
+        public virtual IEnumerable<DeclarativeType> GetDeclarativeTypes()
+        {
+            // Actions
+            yield return new DeclarativeType<CancelEvent>(CancelEvent.DeclarativeType);
+            yield return new DeclarativeType<CreateEvent>(CreateEvent.DeclarativeType);
+            yield return new DeclarativeType<CreateOnlineMeeting>(CreateOnlineMeeting.DeclarativeType);
+            yield return new DeclarativeType<DeclineEvent>(DeclineEvent.DeclarativeType);
+            yield return new DeclarativeType<FindMeetingTimes>(FindMeetingTimes.DeclarativeType);
+            yield return new DeclarativeType<GetContacts>(GetContacts.DeclarativeType);
+            yield return new DeclarativeType<GetEvents>(GetEvents.DeclarativeType);
+            yield return new DeclarativeType<UpdateEvent>(UpdateEvent.DeclarativeType);
+            yield return new DeclarativeType<EventDateTimeInput>(EventDateTimeInput.DeclarativeType);
+        }
+
+        public virtual IEnumerable<JsonConverter> GetConverters(ResourceExplorer resourceExplorer, Stack<string> paths)
+        {
+            yield break;
+        }
+    }
+}
