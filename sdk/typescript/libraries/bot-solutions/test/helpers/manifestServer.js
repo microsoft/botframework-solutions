14e4f17a00c8bf74988b34b933e9d86c0670a63f
DiegoCardozo94 <diego.cardozo@southworks.com>
2020-04-23 17:07:54 -0300

    Update Test folder

--- sdk/typescript/libraries/bot-solutions/test/helpers/manifestServer.js
+++ sdk/typescript/libraries/bot-solutions/test/helpers/manifestServer.js
@@ -1,8 +1,6 @@
 const { join } = require("path");
 const restify = require("restify");
-const { manifestGenerator } = require("../../lib/skills/skillManifestGenerator");
 
-const manifestFile = join(__dirname, '..', 'mocks', 'resources', 'manifestTemplate.json');
 const microsoftAppId = '9afc4045-b3f3-4106-80be-d152d8821879';
 const microsoftAppPassword = 'MockPassword';
 const languageModels = [
@@ -33,8 +31,6 @@ const botSettings = {
 const server = restify.createServer();
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
-
-server.get('/api/manifest', manifestGenerator(manifestFile, botSettings));
 
 exports.server = server;
 exports.botSettings = botSettings;
