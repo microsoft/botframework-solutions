4c622830d9fb81510408529e64a33565d06fc24f
Siddharth More <simore@microsoft.com>
2020-05-19 11:54:09 -0700

    Add TaskModule Handlers in VA

--- samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Bots/DefaultActivityHandler.cs
+++ samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Bots/DefaultActivityHandler.cs
@@ -3,15 +3,21 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Integration.AspNet.Core.Skills;
 using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
 using Microsoft.Bot.Solutions;
 using Microsoft.Bot.Solutions.Responses;
+using Microsoft.Bot.Solutions.Skills;
 using Microsoft.Extensions.DependencyInjection;
+using VirtualAssistantSample.Extensions;
 using VirtualAssistantSample.Models;
 
 namespace VirtualAssistantSample.Bots
@@ -25,6 +31,9 @@ namespace VirtualAssistantSample.Bots
         private IStatePropertyAccessor<DialogState> _dialogStateAccessor;
         private IStatePropertyAccessor<UserProfileState> _userProfileState;
         private LocaleTemplateEngineManager _templateEngine;
+        private readonly SkillHttpClient _skillHttpClient;
+        private readonly SkillsConfiguration _skillsConfig;
+        private readonly string _appId;
 
         public DefaultActivityHandler(IServiceProvider serviceProvider, T dialog)
         {
@@ -34,10 +43,17 @@ namespace VirtualAssistantSample.Bots
             _dialogStateAccessor = _conversationState.CreateProperty<DialogState>(nameof(DialogState));
             _userProfileState = _userState.CreateProperty<UserProfileState>(nameof(UserProfileState));
             _templateEngine = serviceProvider.GetService<LocaleTemplateEngineManager>();
+            _skillHttpClient = serviceProvider.GetService<SkillHttpClient>();
+            _skillsConfig = serviceProvider.GetService<SkillsConfiguration>();
         }
 
         public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
         {
+            if (turnContext.Activity.ChannelId == Channels.Msteams)
+            {
+                turnContext.Activity.RemoveRecipientMention();
+            }
+
             await base.OnTurnAsync(turnContext, cancellationToken);
 
             // Save any state changes that might have occured during the turn.
@@ -92,6 +108,47 @@ namespace VirtualAssistantSample.Bots
                         await turnContext.SendActivityAsync(new Activity(type: ActivityTypes.Trace, text: $"Unknown Event '{ev.Name ?? "undefined"}' was received but not processed."));
                         break;
                     }
+            }
+        }
+
+        // Invoked when a "task/fetch" event is received to invoke task module.
+        protected override async Task<TaskModuleResponse> OnTeamsTaskModuleFetchAsync(ITurnContext<IInvokeActivity> turnContext, TaskModuleRequest taskModuleRequest, CancellationToken cancellationToken)
+        {
+            try
+            {
+                string skillName = (turnContext.Activity as Activity).GetSkillName();
+
+                var skill = _skillsConfig.Skills.Where(s => s.Key == skillName).FirstOrDefault().Value;
+
+                // Forward request to correct skill 
+                var invokeResponse = await _skillHttpClient.PostActivityAsync(_appId, skill, _skillsConfig.SkillHostEndpoint, turnContext.Activity as Activity, cancellationToken);
+                return invokeResponse.GetTaskModuleRespose();
+            }
+            catch (Exception)
+            {
+                await turnContext.SendActivityAsync(_templateEngine.GenerateActivityForLocale("ErrorMessage"));
+                return null;
+            }
+        }
+
+        // Invoked when a 'task/submit' invoke activity is received for task module submit actions. 
+        protected override async Task<TaskModuleResponse> OnTeamsTaskModuleSubmitAsync(ITurnContext<IInvokeActivity> turnContext, TaskModuleRequest taskModuleRequest, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+                string skillName = (turnContext.Activity as Activity).GetSkillName();
+
+                var skill = _skillsConfig.Skills.Where(s => s.Key == skillName).FirstOrDefault().Value;
+
+                // Forward request to correct skill 
+                var invokeResponse = await _skillHttpClient.PostActivityAsync(_appId, skill, _skillsConfig.SkillHostEndpoint, turnContext.Activity as Activity, cancellationToken).ConfigureAwait(false);
+                return invokeResponse.GetTaskModuleRespose();
+            }
+            catch (Exception)
+            {
+                await turnContext.SendActivityAsync(_templateEngine.GenerateActivityForLocale("ErrorMessage"));
+                return null;
             }
         }
 
---
+++ samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Models/SkillCardActionData.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace VirtualAssistantSample.Models
+{
+    public class SkillCardActionData
+    {
+        /// <summary>
+        /// Gets or sets skillName
+        /// </summary>
+        /// <value>
+        /// return SkillName
+        /// </value>
+        [JsonProperty("AppName")]
+        public string SkillName { get; set; }
+    }
+}
