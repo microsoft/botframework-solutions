e16f30f0678b9ff7e90dd4260791966cb3a3f3c7
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2020-06-12 16:28:20 -0300

    add C# solution for the TypeScript functional tests

---
+++ templates/typescript/samples/sample-assistant/functional-tests/VirtualAssistantSample.FunctionalTests/DirectLineClientTests.cs
@@ -0,0 +1,242 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.LanguageGeneration;
+using Microsoft.Bot.Connector.DirectLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirtualAssistantSample.FunctionalTests.Models;
+
+namespace VirtualAssistantSample.FunctionalTests
+{
+    [TestClass]
+    [TestCategory("FunctionalTests")]
+    public class DirectLineClientTests
+    {
+        private static readonly string TestName = "Jane Doe";
+
+        private static string _directLineSecret = string.Empty;
+        private static string _botId = string.Empty;
+        private static DirectLineClient _client;
+
+        protected Templates AllResponsesTemplates
+        {
+            get
+            {
+                var path = CultureInfo.CurrentUICulture.Name.ToLower() == "en-us" ?
+                    Path.Combine(".", "Responses", $"AllResponses.lg") :
+                    Path.Combine(".", "Responses", $"AllResponses.{CultureInfo.CurrentUICulture.Name.ToLower()}.lg");
+                return Templates.ParseFile(path);
+            }
+        }
+
+        [TestInitialize]
+        public void Test_Initialize()
+        {
+            GetEnvironmentVars();
+
+            // Create a new Direct Line client.
+            _client = new DirectLineClient(_directLineSecret);
+        }
+
+        [TestMethod]
+        public async Task Test_Greeting()
+        {
+            string fromUser = Guid.NewGuid().ToString();
+
+            await Assert_New_User_Greeting(fromUser);
+            await Assert_Returning_User_Greeting(fromUser);
+        }
+
+        /// <summary>
+        /// Assert that a new user is greeted with the onboarding prompt.
+        /// </summary>
+        /// <param name="fromUser">User identifier used for the conversation and activities.</param>
+        /// <returns>Task.</returns>
+        public async Task Assert_New_User_Greeting(string fromUser)
+        {
+            var profileState = new UserProfileState { Name = TestName };
+
+            var allNamePromptVariations = AllResponsesTemplates.ExpandTemplate("NamePrompt");
+            var allHaveMessageVariations = AllResponsesTemplates.ExpandTemplate("HaveNameMessage", profileState);
+
+            var conversation = await StartBotConversationAsync();
+
+            var responses = await SendActivityAsync(conversation, CreateStartConversationEvent(fromUser));
+
+            Assert.AreEqual(1, responses[0].Attachments.Count);
+            if (responses.Count == 3)
+            {
+                CollectionAssert.Contains(allNamePromptVariations as ICollection, responses[1].Text);
+            }
+            else if (responses.Count == 4)
+            {
+                CollectionAssert.Contains(allNamePromptVariations as ICollection, responses[3].Text);
+            }
+            else
+            {
+                CollectionAssert.Contains(allNamePromptVariations as ICollection, responses[2].Text);
+            }
+
+            responses = await SendActivityAsync(conversation, CreateMessageActivity(fromUser, TestName));
+
+            if (responses.Count == 5)
+                CollectionAssert.Contains(allHaveMessageVariations as ICollection, responses[3].Text);
+            else
+                CollectionAssert.Contains(allHaveMessageVariations as ICollection, responses[4].Text);
+        }
+
+        /// <summary>
+        /// Assert that a returning user is only greeted with a single card activity and the welcome back prompt.
+        /// </summary>
+        /// <param name="fromUser">User identifier used for the conversation and activities.</param>
+        /// <returns>Task.</returns>
+        public async Task Assert_Returning_User_Greeting(string fromUser)
+        {
+            var conversation = await StartBotConversationAsync();
+
+            var responses = await SendActivityAsync(conversation, CreateStartConversationEvent(fromUser));
+
+            // Both should be message Activities.
+            Assert.AreEqual(ActivityTypes.Message, responses[0].GetActivityType());
+            Assert.AreEqual(ActivityTypes.Message, responses[1].GetActivityType());
+
+            // First Activity should have an adaptive card response.
+            Assert.AreEqual(1, responses[0].Attachments.Count);
+            Assert.AreEqual("application/vnd.microsoft.card.adaptive", responses[0].Attachments[0].ContentType);
+        }
+
+        /// <summary>
+        /// Return a Start Conversation event with a customised UserId and Name enabling independent tests to not be affected by earlier functional test conversations.
+        /// </summary>
+        /// <param name="fromUser">User identifier used for the conversation and activities.</param>
+        private static Activity CreateStartConversationEvent(string fromUser)
+        {
+            // An event activity to trigger the welcome message (method for using custom Web Chat).
+            return new Activity
+            {
+                From = new ChannelAccount(fromUser, TestName),
+                Name = "startConversation",
+                Type = ActivityTypes.Event
+            };
+        }
+
+        /// <summary>
+        /// Return a Message Activity with a customised UserId and Name enabling independent tests to not be affected by earlier functional test conversations.
+        /// </summary>
+        /// <param name="fromUser">User identifier used for the conversation and activities.</param>
+        private static Activity CreateMessageActivity(string fromUser, string activityText)
+        {
+            return new Activity
+            {
+                From = new ChannelAccount(fromUser, TestName),
+                Text = activityText,
+                Type = ActivityTypes.Message
+            };
+        }
+
+        /// <summary>
+        /// Starts a conversation with a bot.
+        /// </summary>
+        /// <returns>Returns the new conversation.</returns>
+        private static async Task<Conversation> StartBotConversationAsync()
+        {
+            // Start the conversation.
+            return await _client.Conversations.StartConversationAsync();
+        }
+
+        /// <summary>
+        /// Sends an activity and waits for the response.
+        /// </summary>
+        /// <returns>Returns the bots answer.</returns>
+        private static async Task<List<Activity>> SendActivityAsync(Conversation conversation, Activity activity)
+        {
+            List<Activity> responses;
+            int attempts = 0;
+
+            // The do while loop is being added as in the Pipelines, the first time a test
+            // is ran, it fails with an error on the response. So, we try again and should respond as expected
+            do
+            {
+                // Send the message activity to the bot.
+                await _client.Conversations.PostActivityAsync(conversation.ConversationId, activity);
+
+                // Read the bot's message.
+                responses = await ReadBotMessagesAsync(_client, conversation.ConversationId);
+
+                attempts++;
+
+                if (attempts == 5)
+                {
+                    throw new NullReferenceException("There was an error reading the response. It might be an issue with DirectLine.");
+                }
+
+                if (responses?.FirstOrDefault()?.Attachments == null)
+                {
+                    // Wait for ten second before polling the bot again.
+                    await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+                }
+            }
+            while (responses?.FirstOrDefault()?.Attachments == null);
+
+            return responses;
+        }
+
+        /// <summary>
+        /// Polls the bot continuously until it gets a response.
+        /// </summary>
+        /// <param name="client">The Direct Line client.</param>
+        /// <param name="conversationId">The conversation ID.</param>
+        /// <returns>Returns the bot's answer.</returns>
+        private static async Task<List<Activity>> ReadBotMessagesAsync(DirectLineClient client, string conversationId)
+        {
+            string watermark = null;
+            List<Activity> botResponses = null;
+
+            // Poll the bot for replies once per second.
+            // Retrieve the activity sent from the bot.
+            var activitySet = await client.Conversations.GetActivitiesAsync(conversationId, watermark);
+            watermark = activitySet?.Watermark;
+
+            // Extract the activities sent from the bot.
+            var activities = from x in activitySet.Activities
+                             where x.From.Id == _botId
+                             select x;
+
+            // Analyze each activity in the activity set.
+            if (activities.Any())
+            {
+                botResponses = activities.ToList();
+            }
+
+            return botResponses;
+        }
+
+        /// <summary>
+        /// Get the values for the environment variables.
+        /// </summary>
+        private void GetEnvironmentVars()
+        {
+            if (string.IsNullOrWhiteSpace(_directLineSecret) || string.IsNullOrWhiteSpace(_botId))
+            {
+                _directLineSecret = Environment.GetEnvironmentVariable("DIRECTLINEKEY");
+                if (string.IsNullOrWhiteSpace(_directLineSecret))
+                {
+                    Assert.Inconclusive("Environment variable 'DIRECTLINEKEY' not found.");
+                }
+
+                _botId = Environment.GetEnvironmentVariable("BOTID");
+                if (string.IsNullOrWhiteSpace(_botId))
+                {
+                    Assert.Inconclusive("Environment variable 'BOTID' not found.");
+                }
+            }
+        }
+    }
+}
---
+++ templates/typescript/samples/sample-assistant/functional-tests/VirtualAssistantSample.FunctionalTests/Models/UserProfileState.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace VirtualAssistantSample.FunctionalTests.Models
+{
+    public class UserProfileState
+    {
+        public string Name { get; set; }
+    }
+}
