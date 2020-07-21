8efa53f6f5ea7af1b3f2a51e5cb8f8a171daa1f3
Siddharth More <simore@microsoft.com>
2020-07-21 12:31:46 -0700

    Fix Build break

---
+++ samples/csharp/assistants/virtual-assistant/VirtualAssistantSample/Extensions/InvokeResponseHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema.Teams;
+using Newtonsoft.Json.Linq;
+
+namespace VirtualAssistantSample.Extensions
+{
+    /// <summary>
+    /// InvokeResposneHandler class for returning TaskModuleResponse from InvokeResponse
+    /// </summary>
+    public static class InvokeResponseHandler
+    {
+        // Converts "InvokeResponse" sent by SkillHttpClient to "TaskModuleResponse"
+        public static TaskModuleResponse GetTaskModuleResponse(this InvokeResponse invokeResponse)
+        {
+            if (invokeResponse == null)
+            {
+                throw new ArgumentNullException(nameof(invokeResponse));
+            }
+
+            if (invokeResponse.Body != null)
+            {
+                return new TaskModuleResponse()
+                {
+                    Task = GetTask(invokeResponse.Body),
+                };
+            }
+
+            return null;
+        }
+
+        private static TaskModuleResponseBase GetTask(object invokeResponseBody)
+        {
+            var resposeBody = JObject.FromObject(invokeResponseBody);
+            var task = resposeBody.GetValue("task");
+            string taskType = task.SelectToken("type")?.Value<string>();
+
+            return taskType switch
+            {
+                "continue" => new TaskModuleContinueResponse()
+                {
+                    Type = taskType,
+                    Value = task.SelectToken("value").ToObject<TaskModuleTaskInfo>(),
+                },
+                "message" => new TaskModuleMessageResponse()
+                {
+                    Type = taskType,
+                    Value = task.SelectToken("value").ToString(),
+                },
+                _ => null,
+            };
+        }
+    }
+}
