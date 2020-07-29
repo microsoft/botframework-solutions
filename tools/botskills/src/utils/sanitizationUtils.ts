62f9c9c5d54b70b028c6f2be8f4be94d84468ab6
Martin Battaglino <martinbatta32@gmail.com>
2020-07-29 18:25:56 -0300

    Abstract logic to sanitizeAppSettingsProperties method in utils folder

--- tools/botskills/src/utils/sanitizationUtils.ts
+++ tools/botskills/src/utils/sanitizationUtils.ts
@@ -3,6 +3,8 @@
  * Licensed under the MIT License.
  */
 
+import { readFileSync } from 'fs';
+
 const regexTrailingBackslash = /.*?(\\)+$/;
 
 /**
@@ -18,10 +20,24 @@ export function sanitizePath(path: string): string {
 }
 
 /**
+ * @param appsettings Path to the appsettings file.
+ * @returns Returns an appsettings which is sanitized
+ */
+export function sanitizeAppSettingsProperties(appsettings: string): string {
+    return readFileSync(appsettings, 'UTF8')
+        .replace(new RegExp("\\bBotFrameworkSkills\\b", "gi"), 'botFrameworkSkills')
+        .replace(new RegExp("\\bAppId\\b", "gi"), 'appId')
+        .replace(new RegExp("\\bId\\b", "gi"), 'id')
+        .replace(new RegExp("\\bSkillEndpoint\\b", "gi"), 'skillEndpoint')
+        .replace(new RegExp("\\bName\\b", "gi"), 'name')
+        .replace(new RegExp("\\bDescription\\b", "gi"), 'description')
+        .replace(new RegExp("\\bSkillHostEndpoint\\b", "gi"), 'skillHostEndpoint');    
+}
+
+/**
  * @param path Path to add quotes around in case it has spaces.
  * @returns Returns a path with quotes
  */
 export function wrapPathWithQuotes(path: string): string {
     return `"${ path }"`;
 }
-
