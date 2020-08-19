18f0a1163a4abc5e36c9810db4a5facf725f9c50
Peter Innes <peinnes@microsoft.com>
2020-08-19 13:40:10 -0700

    Fixing scope of injected DefaultAdapter in Startup.cs for the Skill sample and template to use Singleton scope instead of Transient.

--- samples/csharp/skill/SkillSample/Startup.cs
+++ samples/csharp/skill/SkillSample/Startup.cs
@@ -122,7 +122,7 @@ namespace SkillSample
             services.AddTransient<MainDialog>();
 
             // Configure adapters
-            services.AddTransient<IBotFrameworkHttpAdapter, DefaultAdapter>();
+            services.AddSingleton<IBotFrameworkHttpAdapter, DefaultAdapter>();
 
             // Configure bot
             services.AddTransient<IBot, DefaultActivityHandler<MainDialog>>();
