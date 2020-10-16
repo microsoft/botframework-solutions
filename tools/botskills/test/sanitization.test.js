f187cee915b93f73b91bbe32d482af3019234748
Victor <victor@grycuk.net>
2020-10-16 14:41:15 -0300

    Fix sanitization path util test

--- tools/botskills/test/sanitization.test.js
+++ tools/botskills/test/sanitization.test.js
@@ -5,16 +5,17 @@
 
 const { strictEqual } = require("assert");
 const { sanitizePath } = require("../lib/utils");
+const { join, sep } = require("path");
 
 describe("The sanitization path util", function () {
     describe("should return a path without trailing backslash", function () {
         it("when a path does not contain a trailing backslash", async function() {
-            const path = "this\\is\my\\path";
+            const path = join('this', 'is', 'my', 'path');
             strictEqual(sanitizePath(path), path);
         });
         
         it("when a path contains a trailing backslash", async function() {
-            const path = "this\\is\my\\path\\";
+            const path = join('this', 'is', 'my', 'path', sep);
             strictEqual(sanitizePath(path), path.substring(0, path.length - 1));
         });
     })
